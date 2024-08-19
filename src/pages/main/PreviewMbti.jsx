1439e64dc42181946daa4e422def48cf906163f6
holajm <holajjm@naver.com>
2024-08-19 13:58:40 +0900

    Edit UI & Word

--- src/pages/main/PreviewMbti.jsx
+++ src/pages/main/PreviewMbti.jsx
@@ -8,10 +8,9 @@ function PreviewMbti() {
   };
 
   return (
-    <div className="bg-white flex justify-center items-center flex-col gap-3 border-2">
+    <div className="bg-white border-2 flex-grow">
       <h1 className="font-bold text-xl">🌤️날씨 성격 테스트</h1>
-      <p>나의 성격은 어느 날씨와 어울릴까요?</p>
-      <p>지금 바로 테스트를 통해 알아보세요!</p>
+      <p>나의 성격은 어느 날씨와 어울릴까요? <br/> 날씨 성향 테스트를 통해 알아보세요!</p>
       <button
         className="rounded-lg px-4 py-2 bg-primary text-white font-bold"
         onClick={moveToMbitTest}
