a91c560da3f464729780d1cc7c5057ac9d7f9d6f
holajm <holajjm@naver.com>
2024-08-21 16:14:55 +0900

    Edit UI & Style Change

--- src/pages/main/AllCitiesWeather.jsx
+++ src/pages/main/AllCitiesWeather.jsx
@@ -49,17 +49,20 @@ function AllCitiesWeather() {
     const now = new Date();
     const hours = now.getHours().toString().padStart(2, '0');
     const minutes = now.getMinutes().toString().padStart(2, '0');
-    return `${hours}시${minutes}분`;
+    return `${hours}시 ${minutes}분`;
   }
   console.log('allciteis', data);
 
   return (
-    <div className='flex flex-col mt-16 px-48 lg:px-56 xl:px-60'>
+    <div className='flex flex-col gap-4 mt-16 px-20 sm:px-40 md:px-48 lg:px-56 xl:px-60'>
       <div className="flex items-center">
-        <h1 className="text-2xl font-bold flex-grow">WeathreMate의 전국날씨(<p className='inline text-lg'>현재 시각 : {getCurrentTime()}</p>)</h1>
-        <img src="clothes-m-2.svg" className="w-20" />
+        <div className=" text-xl text-nowrap md:text-2xl font-bold flex-grow flex flex-col items-center lg:flex-row">
+          <h1>WeathreMate의 전국날씨</h1>
+          <p className='text-md md:text-lg'>(현재 시각 : {getCurrentTime()})</p>
+        </div>
+        <img src="clothes-m-2.svg" className="w-20 hidden md:inline" />
       </div>
-      <div className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 ">
+      <div className="grid gric-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 ">
         {data?.length > 0 && (
           <>
             {data.map(item => {
@@ -68,21 +71,23 @@ function AllCitiesWeather() {
               return (
                 <div
                   key={item.id}
-                  className="bg-white md:p-4 rounded-md shadow-md px-7 justify-center items-center border-primary border-2"
+                  className="bg-[#fffdea] p-2 md:p-4 rounded-md shadow-lg shadow-[#b8b4ae] justify-center items-center border-[#ffe3b8] border-2"
                 >
                   <div className="text-center">
-                    <h2 className="text-lg font-bold">{cityName}</h2>
-                    <img src={iconURL} alt="Weather Icon" className="mx-auto" />
-                    <p className="text-base">
-                      날씨: {item.weather[0].description}
-                    </p>
-                    <p className="text-xs">현재 온도: {item.main.temp}°C</p>
-                    <p className="text-xs">
-                      체감 온도: {item.main.feels_like}°C
-                    </p>
-                    <p className="text-sm">시간별</p>
-                    <p className="text-xs">최고 :{item.main.temp_max}°C</p>
-                    <p className="text-xs">최저 :{item.main.temp_min}°C</p>
+                    <h2 className="text-md font-bold">{cityName}</h2>
+                    <p className="text-3xl">{String(item.main.temp).slice(0,2)}°C</p>
+                    <div className='flex gap-2 items-center justify-center'>
+                      <img src={iconURL} alt="Weather Icon" className="" />
+                      <p className="text-sm font-bold">{item.weather[0].description}</p>
+                    </div>
+                    <div className='flex gap-2'>
+                      <div className='rounded-md py-2 border border-b-slate-400 shadow-md shadow-slate-400 flex-grow text-nowrap'>
+                        <p className="text-[14px]">최고 :{String(item.main.temp_max).slice(0,4)}°C</p>
+                      </div>
+                      <div className='rounded-md py-2 border border-b-slate-400 shadow-md shadow-slate-400 flex-grow text-nowrap'>
+                        <p className="text-[14px]">최저 :{String(item.main.temp_min).slice(0,4)}°C</p>
+                      </div>
+                    </div>
                   </div>
                 </div>
               );
