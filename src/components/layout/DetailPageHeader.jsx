67d75e4c674df073f85ce491acd06c865735a834
holajm <holajjm@naver.com>
2024-09-17 23:46:27 +0900

    Refactor - Remove Not Neccessary Property

--- src/components/layout/DetailPageHeader.jsx
+++ src/components/layout/DetailPageHeader.jsx
@@ -1,22 +1,13 @@
-import Button from '@components/layout/Button';
 import PropTypes from 'prop-types';
-import { FaArrowLeft } from 'react-icons/fa';
 
 DetailPageHeader.propTypes = {
   title: PropTypes.string,
 };
 
 function DetailPageHeader({ title }) {
-  const handleGoBack = () => {
-    window.history.back();
-  };
   return (
     <>
-      <div className="xl:mx-60 px-5 p-5 border-y-[1px] border-gray-100 flex items-center justify-between">
-        <Button onClick={handleGoBack} className="flex items-center">
-          <FaArrowLeft className="text-xl" />
-        </Button>
-
+      <div className=" px-5 p-5 flex items-center justify-center md:hidden xl:mx-60">
         <h1 className="font-bold text-xl">{title}</h1>
         <div className="w-5"></div>
       </div>
