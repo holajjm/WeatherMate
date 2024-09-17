dbc052c3e45923999d7f7c88f043aed587c52c37
holajm <holajjm@naver.com>
2024-09-17 23:42:49 +0900

    Edit Style & Refactor Remove useNavigate Page Router

--- src/pages/community/CommunityHeader.jsx
+++ src/pages/community/CommunityHeader.jsx
@@ -1,14 +1,12 @@
 import PropTypes from "prop-types";
-import { useNavigate } from "react-router-dom";
 
 CommunityHeader.propTypes = {
   title: PropTypes.string
 }
 
 function CommunityHeader({title}) {
-  const navigate = useNavigate();
   return (
-    <div className="p-5 text-center grow" onClick={() => navigate('/community')}>
+    <div className="p-4 text-center grow" onClick={() => window.location.reload()}>
       <h1 className="inline-block font-bold text-xl lg:text-xl xl:text-2xl">{title}</h1>
     </div>
   )
