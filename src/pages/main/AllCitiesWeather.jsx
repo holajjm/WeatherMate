3d4d303d179edded4a91428701d2876e7c586dfb
holajm <holajjm@naver.com>
2024-09-27 16:41:07 +0900

    Edit UI - Edit a Responsive UI

--- src/pages/main/AllCitiesWeather.jsx
+++ src/pages/main/AllCitiesWeather.jsx
@@ -1,68 +1,67 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import { gsap } from 'gsap';
 
 const apiKey = import.meta.env.VITE_REACT_APP_WEATHER_API_KEY;
+const citiesMappingData = {
+  'Seoul': '서울',
+  'Gyeonggi-do': '경기도',
+  'Gangwon-do': '강원도',
+  'North Chungcheong': '충청북도',
+  'Chungcheongnam-do': '충청남도',
+  'Jeollabuk-do': '전라북도',
+  'Jeollanam-do': '전라남도',
+  'Gyeongsangbuk-do': '경상북도',
+  'Gyeongsangnam-do': '경상남도',
+  'Jeju-do': '제주도',
+};
 
 function AllCitiesWeather() {
   const [data, setData] = useState(null);
 
-  const weatherData = async () => {
-    const params = {
-      id: '1835847,1841610,1843125,1845106,1845105,1845789,1845788,1841597,1902028,1846265',
-      appid: apiKey,
-      lang: 'kr',
-      units: 'metric',
-    };
-    const res = await axios.get(
-      'https://api.openweathermap.org/data/2.5/group',
-      {
-        params,
-      },
-    );
-    return res;
-  };
-
-  const getWeather = async () => {
-    const res = await weatherData();
-    setData(res.data.list);
-  };
-
   useEffect(() => {
+    const getWeather = async () => {
+      const params = {
+        id: '1835847,1841610,1843125,1845106,1845105,1845789,1845788,1841597,1902028,1846265',
+        appid: apiKey,
+        lang: 'kr',
+        units: 'metric',
+      };
+      const res = await axios.get(
+        'https://api.openweathermap.org/data/2.5/group',
+        {
+          params,
+        },
+      );
+      setData(res.data.list);
+    };
     getWeather();
   }, []);
 
-  const citiesMappingData = {
-    'Seoul': '서울',
-    'Gyeonggi-do': '경기도',
-    'Gangwon-do': '강원도',
-    'North Chungcheong': '충청북도',
-    'Chungcheongnam-do': '충청남도',
-    'Jeollabuk-do': '전라북도',
-    'Jeollanam-do': '전라남도',
-    'Gyeongsangbuk-do': '경상북도',
-    'Gyeongsangnam-do': '경상남도',
-    'Jeju-do': '제주도',
-    // 여기에 더 많은 도시를 추가할 수 있습니다.
-  };
-
-  function getCurrentTime() {
-    const now = new Date();
-    const hours = now.getHours().toString().padStart(2, '0');
-    const minutes = now.getMinutes().toString().padStart(2, '0');
-    return `${hours}시 ${minutes}분`;
-  }
-  console.log('allciteis', data);
+  useEffect(() => {
+    const floatTl = gsap.to('.aniFloat', {
+      y: 20,
+      duration: 1,
+      repeat: -1,
+      yoyo: true,
+    });
+    return () => {
+      floatTl.kill();
+    };
+  })
 
   return (
-    <div className='flex flex-col gap-4 mt-16 px-20 sm:px-40 md:px-48 lg:px-56 xl:px-60'>
+    <div className='flex flex-col gap-4 pt-4 pb-8 mt-16 px-20 sm:px-40 md:px-48 md:mt-0 lg:px-56 xl:px-60'>
       <div className="flex items-center">
-        <div className=" text-xl text-nowrap md:text-2xl font-bold flex-grow flex flex-col items-center lg:flex-row">
-          <h1>WeathreMate의 전국날씨</h1>
-          <p className='text-md md:text-lg'>(현재 시각 : {getCurrentTime()})</p>
+        <div className=" text-xl text-nowrap md:text-2xl font-bold flex-grow flex justify-center items-center flex-row">
+          <div className='text-center text-xl md:text-2xl md:flex '>
+            <p>WeathreMate의 전국날씨</p>
+            <p>({new Date().toLocaleString().slice(13,20)} 기준)</p>
+          </div>
+          <img src="clothes-m-2.svg" className="w-20 mb-2 hidden aniFloat md:inline" />
         </div>
-        <img src="clothes-m-2.svg" className="w-20 hidden md:inline" />
       </div>
-      <div className="grid gric-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 ">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 ">
         {data?.length > 0 && (
           <>
             {data.map(item => {
