77d3cea3bb04a5e043201cbc9e364670d62ca5d1
holajm <holajjm@naver.com>
2024-09-27 18:43:38 +0900

    Edit Style - Change Background Color

--- src/pages/main/MainPage.jsx
+++ src/pages/main/MainPage.jsx
@@ -7,7 +7,7 @@ import ToTheTopButton from '@components/layout/ToTheTopButton';
 
 function MainWeather() {
   return (
-    <div className="flex flex-col gap-4 font-sans overflow-hidden px-20 pt-24 pb-8 md:py-8 md:px-48 lg:px-56  xl:px-60 lg:grid lg:grid-cols-2 min-w-[375px]">
+    <div className="bg-gray-100 flex flex-col gap-4 font-sans overflow-hidden px-20 pt-24 pb-8 md:py-8 md:px-48 lg:px-56  xl:px-60 lg:grid lg:grid-cols-2 min-w-[375px] ">
       <TodaysComent />
       <MyLocationWeather />
       <div className="xl:col-span-2">
