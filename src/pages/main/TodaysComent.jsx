42e7ee73c4aa621f6b896441578e6e3ed72aece8
holajm <holajjm@naver.com>
2024-09-27 16:43:34 +0900

    Refactor - Remove Unneccessary Code

--- src/pages/main/TodaysComent.jsx
+++ src/pages/main/TodaysComent.jsx
@@ -4,33 +4,25 @@ import dummyData from '../../assets/WeatherData';
 import { useEffect, useState } from 'react';
 import { gsap } from 'gsap';
 import { Link } from 'react-router-dom';
-// import { MdOutlineKeyboardArrowRight } from 'react-icons/md';
 
-/** 날씨에 따른 의상 추천 & 이미지 보여주기 */
 function TodaysComent() {
-  const userWeather = useRecoilValue(userWeatherState); // Recoil 상태만 가져오기
+  const userWeather = useRecoilValue(userWeatherState);
   const [recomendClothes, setRecomendClothes] = useState(null);
   const [recommendationImage, setRecommendationImage] = useState(null);
   const [sessionData, setSessionData] = useState(null);
 
   useEffect(() => {
-    // 세션에서 데이터 가져오기
     const sessionString = sessionStorage.getItem('saveUser');
     if (sessionString) {
-      const sessionObj = JSON.parse(sessionString); // JSON 문자열 파싱
+      const sessionObj = JSON.parse(sessionString);
       setSessionData(sessionObj);
     }
-  }, []); // 컴포넌트가 마운트될 때만 실행되도록 빈 배열을 두 번째 매개변수로 전달
+  }, []); 
 
-  // const [mainImgUrl, setMainImgUrl] = useState(null);
-
-  // console.log('session', sessionData);
-
-  // 온도 비즈니스 로직
   useEffect(() => {
     const getRecommendedClothes = () => {
       const userTemperature = userWeather?.main.temp - 273.15;
-      // test할 때 여기 온도 바꿔서 테스 디폴트는 273.15
+      // default 273.15
       // 4도 이하일 땐 298.15
       // 8도 이하일 땐 291.15
       // 12도 이하일 땐 289.15
@@ -39,12 +31,9 @@ function TodaysComent() {
       // 22도 이하일 땐 278.15
       // 27도 이하일 땐 273.15
       // 29도 이하일 땐 271
-
-      // console.log('현재온도', userTemperature);
       let closestTemperature = dummyData[0].temperature; // 가장 가까운 온도로 초기화
       let selectedDummyData = dummyData[0]; // 가장 가까운 온도에 해당하는 데이터로 초기화
 
-      // 더미 데이터를 기반으로 가장 가까운 온도 선택
       for (let i = 1; i < dummyData.length; i++) {
         if (
           Math.abs(userTemperature - dummyData[i].temperature) <
@@ -57,27 +46,23 @@ function TodaysComent() {
       setRecomendClothes(selectedDummyData?.recommendation);
       setRecommendationImage(selectedDummyData?.CLOTHES_IMG);
     };
-
     getRecommendedClothes();
   }, [userWeather]);
 
-  // 메인화면 애니메이션
   useEffect(() => {
     const tl = gsap.timeline({ defaults: { ease: 'power1.out' } });
     tl.from('.fade-in-from-NS', { opacity: 0, y: 200, duration: 1 });
     return () => {
-      // Clean up animations
       tl.kill();
     };
   }, []);
 
   const imagePath = recommendationImage;
-  const fileName = imagePath; // 경로에서 파일 이름 추출
+  const fileName = imagePath; 
 
   return (
     <div className="font-sans p-4 rounded-2xl shadow-[0px_4px_10px_rgba(0,0,0,0.2),inset_0px_4px_10px_rgba(255,255,255,0.5)] shadow-violet-300 h-full text-lg md:text-2xl fade-in-from-NS">
       <div className="flex flex-col items-center gap-2">
-        {/* user name 받아와 저장 */}
         <div className="text-[#80c8ff] font-semibold text-wrap">
           {sessionData && sessionData.useState && sessionData.useState.name ? (
             <h1>
@@ -98,8 +83,7 @@ function TodaysComent() {
               to="/allcity"
               className="flex justify-center items-center text-base font-semibold text-white bg-indigo-500 h-10 text-pretty rounded-lg p-1 hover:bg-indigo-700 hover:shadow-[0_5px_30px_4px] hover:shadow-slate-400 duration-200 transition-all"
             >
-              <p className="">전국날씨 보러가기</p>
-              {/* <MdOutlineKeyboardArrowRight /> */}
+              전국날씨 보러가기
             </Link>
           </div>
           <img src={fileName} alt="main-img" className="w-[200px]" />
