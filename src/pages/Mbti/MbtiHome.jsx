d57954d289062873959a129ab25950eba5f230de
holajm <holajjm@naver.com>
2024-10-10 14:36:11 +0900

    Edit Style - Edit Layout & Style

--- src/pages/Mbti/MbtiHome.jsx
+++ src/pages/Mbti/MbtiHome.jsx
@@ -5,24 +5,18 @@ function MbtiHome() {
   const navigate = useNavigate();
 
   return (
-    <div
-      className="bg-cover bg-center 
-       h-screen   py-60 xl:px-60"
-      style={{ backgroundImage: `url(${mbtiStart})` }}
-    >
-      <div className="flex gap-10 flex-col text-center items-center font-TTLaundryGothicB text-gray_04 ">
-        <h1 className="text-4xl font-bold  text-gray-4">날씨 성격 테스트</h1>
+    <div className="py-12">
+      <div style={{ backgroundImage: `url(${mbtiStart})` }} className="rounded-2xl bg-cover bg-center h-screen py-60 xl:px-60 flex gap-10 flex-col text-center items-center font-TTLaundryGothicB">
+        <p className="text-4xl font-bold ">날씨 성격 테스트</p>
         <div className="text-2xl font-bold ">
-          <p>
-            날씨별 상황에 따라
-            <br />내 행동을 골라보자!
-          </p>
+          <p>날씨별 상황에 따라</p>
+          <p>내 행동을 골라보자!</p>
           <br />
           <p>나는 어떤 날씨와 어울릴까? </p>
         </div>
         <button
           onClick={() => navigate('question')}
-          className="bg-white hover:bg-primary p-6 text-xl rounded-lg text-gray_04 "
+          className="bg-white p-6 text-xl rounded-lg transition-all duration-200 hover:shadow-lg hover:bg-violet-200 hover:border-2 hover:border-violet-300 hover:shadow-slate-500 hover:scale-105"
         >
           테스트 시작하기
         </button>
