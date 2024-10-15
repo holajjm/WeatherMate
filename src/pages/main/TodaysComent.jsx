e54ddc80f7b3107be061bc68e7d820712ad6f627
holajm <holajjm@naver.com>
2024-10-16 01:11:53 +0900

    Edit UI - Complete Refactor UI and Style by adding Responsive Layout & Unity

--- src/pages/main/TodaysComent.jsx
+++ src/pages/main/TodaysComent.jsx
@@ -2,7 +2,6 @@ import { useRecoilValue } from 'recoil';
 import { userWeatherState } from '../../recoil/atom.mjs';
 import dummyData from '../../assets/WeatherData';
 import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
 import PreviewMbti from '@pages/main/PreviewMbti';
 // import { gsap } from 'gsap';
 
@@ -63,31 +62,25 @@ function TodaysComent() {
 
   return (
     <div className="grow bg-white border-4 border-violet-200 font-sans p-4 rounded-2xl shadow-[0px_4px_10px_rgba(0,0,0,0.2),inset_0px_4px_10px_rgba(255,255,255,0.5)] shadow-violet-200 h-full text-xl md:text-2xl fade-in">
-      <div className="flex flex-col items-center gap-2 xl:gap-8">
-        <div className="text-[#80c8ff] font-semibold text-wrap">
+      <div className="h-full flex flex-col items-center gap-2">
+        <div className="w-full text-center text-[#80c8ff] font-semibold text-wrap p-4">
           {sessionData && sessionData.useState && sessionData.useState.name ? (
-            <h1 className='text-center 2xl:text-3xl'>
+            <h1 className='2xl:text-3xl'>
               {sessionData.useState.name} 메이트님, 환영합니다.
               <br /> WeatherMate입니다.
             </h1>
           ) : (
-            '로그인해주세요.'
+            <h1 className='2xl:text-3xl'>로그인해주세요.</h1>
           )}
         </div>
-        <div className="flex">
-          <div className="flex flex-col gap-2">
-            <div className="flex flex-col gap-2 grow rounded-xl text-lg font-semibold border-4 border-blue-200 shadow-[inset_0px_0px_10px_rgba(255,255,255,0.5)] shadow-blue-300 p-2">
+        <div className="flex xl:flex-col 2xl:flex-row items-center gap-4 grow">
+          <div className="flex flex-col gap-4 h-full">
+            <div className="flex flex-col gap-2 grow rounded-xl text-lg font-semibold border-[3px] border-blue-200 shadow-[inset_0px_0px_10px_rgba(255,255,255,0.5)] shadow-blue-300 p-2">
               <p className="text-center text-xl">- 오늘의 Comment -</p>
-              <hr className='border-[1px] border-slate-300 2xl:border-slate-400 2xl:border-[1.5px]'/>
+              <hr className='border-[1px] w-full border-slate-100 2xl:border-slate-300 2xl:border-[1.5px]'/>
               <p>{recomendClothes}</p>
             </div>
             <PreviewMbti />
-            <Link
-              to="/allcity"
-              className="flex justify-center items-center text-base font-semibold text-white bg-indigo-500 h-10 text-pretty rounded-lg p-1 hover:bg-indigo-700 hover:shadow-[0_5px_30px_4px] hover:shadow-slate-400 duration-200 transition-all"
-            >
-              전국날씨 보러가기
-            </Link>
           </div>
           <img src={fileName} alt="main-img" className="hidden sm:inline sm:w-[200px] 2xl:w-60" />
         </div>
