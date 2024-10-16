083e99f1177febe9816a1a2326296d22743c6f95
holajm <holajjm@naver.com>
2024-10-16 16:58:36 +0900

    Handle Error - Modify Error Whether Login or Not

--- src/components/layout/NavigationBarTop.jsx
+++ src/components/layout/NavigationBarTop.jsx
@@ -34,7 +34,7 @@ function NavigationBarTop() {
           </button>
           WeatherMate
         </Link>
-        {user ? (
+        {user.name ? (
             <p className="text-slate-400 cursor-pointer hover:text-red-500 hover:font-semibold" onClick={handleLogout}> 로그아웃</p>
           ) : (
             <Link to="/mainlogin" className="text-black">
@@ -53,7 +53,7 @@ function NavigationBarTop() {
           />
         </Link>
         <div className="flex text-center gap-4 py-2 w-2/3 2xl:w-1/2 3xl:w-1/3 h-full leading-[48px]">
-          {user ? (
+          {user.name ? (
             <p className="text-slate-400 cursor-pointer hover:text-red-500 hover:font-semibold" onClick={handleLogout}> 로그아웃</p>
           ) : (
             <Link to="/mainlogin" className="grow text-black">
