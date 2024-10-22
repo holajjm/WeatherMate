857400a813d75640bff283535c3fb515910d42e9
holajm <holajjm@naver.com>
2024-10-22 17:39:46 +0900

    Edit UI & Style - Edit Whole Style and Fixed Final Components Logic & Responsive Layout

--- src/pages/main/weatherDetail.jsx
+++ src/pages/main/weatherDetail.jsx
@@ -16,33 +16,34 @@ function WeatherDetail() {
 
   return (
     <>
-      <div className="flex flex-col justify-center items-center border-2 w-full p-2 rounded-xl shadow-slate-200 shadow-[0px_4px_10px_rgba(0,0,0,0.2),inset_0px_4px_10px_rgba(255,255,255,0.5)]">
-        <p><strong className="text-xl text-blue-600">웨더메이트</strong>가 알려주는 상세 날씨</p>
-        {detailWeather && (
-          <div className="flex justify-center items-center gap-2 font-sans">
-            <div className="bg-white w-[77px] h-[88px] flex flex-col items-center justify-center shadow-lg shadow-slate-400 rounded-lg">
-              <img src="humidity.svg" className="w-[31px] " />
-              <p className="text-[#00179C] font-medium">습도</p>
-              <p>{detailWeather.main.humidity} %</p>
-            </div>
-            <div className="bg-white w-[77px] h-[88px] flex flex-col items-center justify-center shadow-lg shadow-slate-400 rounded-lg">
-              <img src="feelslike.svg" className="w-[31px] " />
-              <p className="text-[#00179C] font-medium">체감온도</p>
-              <p>{(detailWeather.main.feels_like - 269.15).toFixed(1)}°C</p>
-            </div>
-            <div className="bg-white w-[77px] h-[88px] flex flex-col items-center justify-center shadow-lg shadow-slate-400 rounded-lg">
-              <img src="windspeed.svg" className="w-[31px] " />
-              <p className="text-[#00179C] font-medium">풍속</p>
-              <p>{detailWeather.wind.speed} m/s</p>
-            </div>
-            <div className="bg-white w-[77px] h-[88px] flex flex-col items-center justify-center shadow-lg shadow-slate-400 rounded-lg">
-              <img src="uvi.svg" className="w-[31px] " />
-              <p className="text-[#00179C] font-medium">자외선</p>
-              <p>{getWindStatus(detailWeather.wind.speed)}</p>
-            </div>
+      {detailWeather && (
+        <div className="grow flex md:grid md:grid-cols-2 lg:flex xl:grid xl:grid-cols-2 2xl:flex w-full h-full justify-center items-center gap-2 xl:gap-4 font-sans xl:p-4 xl:box-border">
+          <div className="bg-white w-full h-full text-center text-nowrap flex flex-col gap-1 items-center justify-center shadow-md shadow-slate-400 rounded-lg border-[1.5px]">
+            <img src="humidity.svg" className="w-10" />
+            <p className="text-slate-600">습도</p>
+            <hr className="border-[1px] border-slate-400 w-3/4"/>
+            <p className="font-bold 2xl:text-xl text-slate-700">{detailWeather.main.humidity} %</p>
           </div>
+          <div className="bg-white w-full h-full text-center text-nowrap flex flex-col gap-1 items-center justify-center shadow-md shadow-slate-400 rounded-lg border-[1.5px]">
+            <img src="feelslike.svg" className="w-10" />
+            <p className="text-slate-600">체감온도</p>
+            <hr className="border-[1px] border-slate-400 w-3/4"/>
+            <p className="font-bold 2xl:text-xl text-slate-700">{(detailWeather.main.feels_like - 269.15).toFixed(1)}°C</p>
+          </div>
+          <div className="bg-white w-full h-full text-center text-nowrap flex flex-col gap-1 items-center justify-center shadow-md shadow-slate-400 rounded-lg border-[1.5px]">
+            <img src="windspeed.svg" className="w-10" />
+            <p className="text-slate-600">풍속</p>
+            <hr className="border-[1px] border-slate-400 w-3/4"/>
+            <p className="font-bold 2xl:text-xl text-slate-700">{detailWeather.wind.speed} m/s</p>
+          </div>
+          <div className="bg-white w-full h-full text-center text-nowrap flex flex-col gap-1 items-center justify-center shadow-md shadow-slate-400 rounded-lg border-[1.5px]">
+            <img src="uvi.svg" className="w-10" />
+            <p className="text-slate-600">자외선</p>
+            <hr className="border-[1px] border-slate-400 w-3/4"/>
+            <p className="font-bold 2xl:text-xl text-slate-700">{getWindStatus(detailWeather.wind.speed)}</p>
+          </div>
+        </div>
         )}
-      </div>
     </>
   );
 }
