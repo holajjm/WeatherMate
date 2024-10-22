7ee3df28c338d0201332c82be6f02a8f0bacfa7e
holajm <holajjm@naver.com>
2024-10-22 17:37:46 +0900

    Edit UI & Style - Edit Whole Style and Fixed Final Components Logic & Responsive Layout

--- src/pages/main/MyLocationWeather.jsx
+++ src/pages/main/MyLocationWeather.jsx
@@ -1,21 +1,22 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useRecoilState } from 'recoil';
-import { userWeatherState } from '../../recoil/atom.mjs';
-import Loading from '../../components/layout/Loading';
-import WeatherDetail from '@pages/main/weatherDetail';
+import { userWeatherState } from '@recoil/atom.mjs';
+import Loading from '@components/layout/Loading';
+import WeatherByTimeZone from '@pages/main/WeatherByTimeZone';
+import WeatherSpec from '@pages/main/WeatherSpec';
 // import gsap from 'gsap';
 
 const MyLocationWeather = () => {
   const [myPlace, setMyPlace] = useState('');
-  const [userWeather, setUserWeather] = useRecoilState(userWeatherState);
   const [loading, setLoading] = useState(true);
+  const [userWeather, setUserWeather] = useRecoilState(userWeatherState);
 
   useEffect(() => {
     const sessionData = sessionStorage.getItem('userWeather');
     if (sessionData) {
       setUserWeather(JSON.parse(sessionData));
-      setMyPlace(sessionStorage.getItem('myPlace')); 
+      setMyPlace(sessionStorage.getItem('myPlace'));
       setLoading(false);
     } else {
       getUserWeather();
@@ -28,9 +29,8 @@ const MyLocationWeather = () => {
         const lat = position.coords.latitude;
         const lon = position.coords.longitude;
         try {
-          const apiKey = import.meta.env.VITE_REACT_APP_WEATHER_API_KEY;
           const response = await axios.get(
-            `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&lang=kr`,
+            `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${import.meta.env.VITE_REACT_APP_WEATHER_API_KEY}&lang=kr`,
           );
           setUserWeather(response.data);
           setMyPlace(response.data.name);
@@ -48,7 +48,7 @@ const MyLocationWeather = () => {
     }
   };
   // console.log('mylocationWeather:', userWeather);
-  const defaultImgPath = '/01.svg'; // 디폴트 이미지 경로
+  const defaultImgPath = '/01.svg';
   const weatherImageMapping = {
     Clear: '/uvi.svg',
     Clouds: '/manyClouds.svg',
@@ -67,22 +67,12 @@ const MyLocationWeather = () => {
       !userWeather.weather ||
       userWeather.weather.length === 0
     )
-    return defaultImgPath; // userWeather나 userWeather.weather가 없으면 기본 이미지 반환
+      return defaultImgPath; // userWeather나 userWeather.weather가 없으면 기본 이미지 반환
     const englishDescription = userWeather.weather[0].main; // 영어로된 날씨 설명 가져오기
     // 영어로 된 날씨 설명에 해당하는 이미지 경로 반환
     return weatherImageMapping[englishDescription] || defaultImgPath;
   };
   const imagePath = getImagePathForWeather(); // getImagePathForWeather 함수 호출하여 imagePath 설정
-
-  const unixToHumanTime = unixTimestamp => {
-    const date = new Date(unixTimestamp * 1000);
-    const hours = date.getHours();
-    let minutes = date.getMinutes();
-    const ampm = hours >= 12 ? 'PM' : 'AM'; // 오전과 오후를 판별합니다.
-    const hour = hours % 12 || 12; // 12시간 형식으로 변경합니다.
-    minutes = minutes === 0 ? '' : ':' + (minutes < 10 ? '0' : '') + minutes;
-    return `${hour}${minutes} ${ampm}`;
-  };
 
   // useEffect(() => {
   //   const tl = gsap.timeline({defaults: {ease: 'power2.out'}});
@@ -93,41 +83,27 @@ const MyLocationWeather = () => {
   // },[])
 
   return (
-    <div  className="grow bg-white border-4 border-violet-200 rounded-2xl p-4 shadow-[0px_4px_10px_rgba(0,0,0,0.2),inset_0px_4px_10px_rgba(255,255,255,0.5)] shadow-violet-200  fade-in">
+    <div className="grow bg-white border-4 border-violet-200 rounded-2xl p-4 shadow-[0px_4px_10px_rgba(0,0,0,0.2),inset_0px_4px_10px_rgba(255,255,255,0.5)] shadow-violet-200 fade-in xl:w-1/2">
       {loading ? (
         <Loading />
       ) : (
-        <div style={{backgroundImage: `url(${imagePath})`}} className='flex flex-col items-center justify-between relative h-full bg-contain bg-center bg-no-repeat'>
+        <div
+          style={{ backgroundImage: `url(${imagePath})` }}
+          className="flex flex-col gap-4 items-center justify-center h-full bg-contain bg-center bg-no-repeat"
+        >
           {userWeather && (
             <>
               <div className="text-5xl font-bold text-center">
                 <h2 className="text-xl font-bold">{myPlace}</h2>
                 {(userWeather.main.temp - 273.15).toFixed(1)}°C
               </div>
-              <p className="text-lg font-bold ">
-                {userWeather.weather[0].description}
-              </p>
-              <p className="">기준 : {unixToHumanTime(userWeather.dt)}</p>
-
-              <div className="flex gap-5">
-                <div className="flex flex-col justify-center items-center">
-                  <img src="sunset.svg" className="w-5 h-5" />
-                  <p>일출:{unixToHumanTime(userWeather.sys.sunrise)}</p>
-                  <p>일몰:{unixToHumanTime(userWeather.sys.sunset)}</p>
-                </div>
-                <div className="flex flex-col justify-center items-center">
-                  <img src="dgree.svg" className="w-5 h-5" />
-                  <p>최고:{(userWeather.main.temp_max - 273.15).toFixed(1)}°C</p>
-                  <p>최저:{(userWeather.main.temp_min - 274.15).toFixed(1)}°C</p>
-                </div>
-              </div>
-              <WeatherDetail />
+              <WeatherSpec />
+              <WeatherByTimeZone />
             </>
           )}
-
         </div>
       )}
     </div>
   );
 };
-export default React.memo(MyLocationWeather);
+export default MyLocationWeather;
