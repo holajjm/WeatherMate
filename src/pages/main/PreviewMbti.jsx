f6bb3d0c12775acae136ccc0de50018da99671b5
holajm <holajjm@naver.com>
2024-10-23 16:38:42 +0900

    1. Edit UI & Style - Fixed Final Responsive UI & Button Style because of TimeZone Components UI

--- src/pages/main/PreviewMbti.jsx
+++ src/pages/main/PreviewMbti.jsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 function PreviewMbti() {
   const navigate = useNavigate();
@@ -9,7 +9,7 @@ function PreviewMbti() {
         <p className="font-bold text-center">🌤️날씨 성격 테스트</p>
         <p className="hidden sm:inline 2xl:hidden 3xl:inline">나의 성격은 어느 날씨와 어울릴까요?<br />날씨 성향 테스트를 통해 알아보세요!</p>
         <button
-          className="w-full flex justify-center items-center text-base font-semibold text-white bg-primary h-10 text-pretty rounded-lg p-1 hover:bg-sky-500 hover:shadow-[0_5px_30px_4px] hover:shadow-slate-400 duration-200 transition-all"
+          className="w-full h-full flex justify-center items-center text-base font-semibold text-white text-pretty rounded-lg p-1 duration-200 transition-all bg-primary hover:bg-sky-500 hover:shadow-[0_5px_30px_4px] hover:shadow-slate-400"
           onClick={() => navigate('/mbti')}
         >
           테스트 하러가기
@@ -18,12 +18,12 @@ function PreviewMbti() {
       <div className="flex flex-col gap-2 border-2 rounded-xl grow p-2 box-border ">
         <p className="font-bold text-center">🌤️전국의 날씨</p>
         <p className="hidden sm:inline 2xl:hidden 3xl:inline">나의 성격은 어느 날씨와 어울릴까요?<br />날씨 성향 테스트를 통해 알아보세요!</p>
-        <Link
-          to="/allcity"
-          className="flex justify-center items-center text-base font-semibold text-white bg-indigo-500 h-10 text-pretty rounded-lg p-1 hover:bg-indigo-700 hover:shadow-[0_5px_30px_4px] hover:shadow-slate-400 duration-200 transition-all"
+        <button
+          className="w-full h-full flex justify-center items-center text-base font-semibold text-white text-pretty rounded-lg p-1 duration-200 transition-all bg-indigo-500 hover:bg-indigo-700 hover:shadow-[0_5px_30px_4px] hover:shadow-slate-400"
+          onClick={() => navigate("/allcity")}
         >
           전국날씨 보러가기
-        </Link>
+        </button>
       </div>
     </div>
   );
