71645bd481077a97c6db504e7b9cced325097211
holajm <holajjm@naver.com>
2024-10-25 16:06:28 +0900

    Add a New File - Seperate Component About User Login Page

---
+++ src/pages/user/ValidLogin.jsx
@@ -0,0 +1,49 @@
+import { useNavigate } from 'react-router-dom';
+
+function ValidLogin() {
+  const navigate = useNavigate();
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <div className="p-8 md:px-20 min-w-[375px] w-full flex justify-center drop-shadow-md">
+        <div className="bg-white flex flex-col w-full lg:w-1/2 p-4 md:p-8 rounded-xl gap-4">
+          <div className="flex flex-col justify-center items-center gap-4">
+            <h2 className="text-3xl font-black text-primary_deep font-Ainmom">
+              로그인 후 이용할 수 있어요!
+            </h2>
+            <p className="text-gray-600">
+              원활한 서비스 사용을 위해 로그인을 해주세요!
+            </p>
+            <img className="w-1/3" src="/mainlogin.svg" />
+          </div>
+          <div className="w-full grid grid-cols-2 gap-2 md:gap-4">
+            <button
+              className="p-2 border-2 border-slate-100 rounded-lg text-sm font-semibold transition-all duration-200 text-white bg-sky-400 hover:bg-sky-500 hover:shadow-[0_5px_30px_4px] hover:shadow-slate-400"
+              onClick={() => navigate('/user/Login')}
+            >
+              로그인
+            </button>
+            <button
+              className="p-2 border-2 border-slate-100 rounded-lg text-sm font-semibold transition-all duration-200 text-white bg-indigo-500 hover:bg-indigo-700 hover:shadow-[0_5px_30px_4px] hover:shadow-slate-400"
+              onClick={() => navigate('/user/SignUp')}
+            >
+              회원가입
+            </button>
+            <button
+              className="p-2 border-2 border-[#FEE500] rounded-lg text-sm font-semibold transition-all duration-200 text-gray-500 text-nowrap bg-[#FEE500] hover:bg-yellow-400 hover:shadow-[0_5px_30px_4px] hover:shadow-slate-400 hover:text-white"
+            >
+              카카오로 시작하기
+            </button>
+            <button
+              onClick={() => navigate("/")}
+              className="p-2 border-2 border-slate-100 rounded-lg text-sm font-semibold transition-all duration-200 text-gray-500 text-nowrap bg-slate-50 hover:bg-slate-400 hover:shadow-[0_5px_30px_4px] hover:shadow-slate-400 hover:text-white"
+            >
+              메인으로 돌아가기
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ValidLogin;
