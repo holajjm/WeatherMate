40576b1e1ec50625a7a606e3d7b4b3f43692255d
holajm <holajjm@naver.com>
2024-10-25 16:03:54 +0900

    Edit UI & Style - Fixed Final UI and Responsive Layout

--- src/pages/main/RecommendationPreview.jsx
+++ src/pages/main/RecommendationPreview.jsx
@@ -1,14 +1,14 @@
-/* eslint-disable */
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import useCurrentLocation from '@hooks/useCurrentLocation';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import Loading2 from '@components/layout/Loading2';
-import gsap from 'gsap';
+// import gsap from 'gsap';
 
 const apiKey = import.meta.env.VITE_REACT_APP_LOCATION_API_KEY;
 
 function RecommendationPreview() {
+  const navigate = useNavigate();
   const [locationData, setLocationData] = useState([]);
   const [locationReady, setLocationReady] = useState(false);
   const [contentTypeId, setContentTypeId] = useState('14');
@@ -32,7 +32,6 @@ function RecommendationPreview() {
           );
           setLocationData(response.data.response.body.items.item);
           setLoading(false);
-          // console.log(response.data);
         } catch (error) {
           console.error(
             '데이터를 원활하게 가져오는데 오류가 발생하였습니다.',
@@ -62,40 +61,37 @@ function RecommendationPreview() {
   //     tl.kill();
   //   }
   // },[])
-
+  // console.log(locationData);
+  
   return (
-    <div className="grow bg-white border-4 border-violet-200 p-4 rounded-2xl shadow-[0px_4px_10px_rgba(0,0,0,0.2),inset_0px_4px_10px_rgba(255,255,255,0.5)] shadow-violet-200 fade-in">
-      <div className="flex flex-col gap-4">
+    <div className="w-full xl:w-1/2 h-full bg-white border-4 border-violet-200 p-4 rounded-2xl shadow-[0px_4px_10px_rgba(0,0,0,0.2),inset_0px_4px_10px_rgba(255,255,255,0.5)] shadow-violet-200 fade-in">
+      <div className="flex flex-col gap-4 h-full">
         <div className="flex justify-center items-center gap-4 font-bold">
-          <h1><span className="text-xl text-primary">웨더메이트</span>가 추천하는 장소</h1>
-          <Link to="/location" className="text-base font-semibold text-white bg-indigo-500 h-10 text-pretty rounded-lg p-1 hover:bg-indigo-700 hover:shadow-[0_5px_30px_4px] hover:shadow-slate-400 duration-200 transition-all">
+          <h1><span className="text-xl text-blue-600">웨더메이트</span>가 추천하는 장소</h1>
+          <button onClick={() => navigate("/location")} className="text-base font-semibold text-white bg-indigo-500 h-full text-pretty rounded-lg p-1 hover:bg-indigo-700 hover:shadow-[0_5px_30px_4px] hover:shadow-slate-400 duration-200 transition-all">
             보러가기
-          </Link>
+          </button>
         </div>
-        <div>
-          {loading ? (
-            <Loading2 />
-          ) : (
-            <div className="flex gap-6 font-sans text-sm">
-              {locationData.map(item => (
-                <div key={item.contentid}>
-                  <Link to={`/location/${item.contentid}`}>
-                    <img
-                      src={item.firstimage}
-                      className="w-[100px] h-[100px] rounded-xl"
-                      alt="recommendation"
-                    />
-                  </Link>
-                  <p className="text-center text-sm mt-2">
-                    {item.title.length > 5
-                      ? `${item.title.slice(0, 10)}...`
-                      : item.title}
-                  </p>
-                </div>
-              ))}
-            </div>
-          )}
-        </div>
+        <hr className="border-slate-700"/>
+        {loading ? (
+          <Loading2 />
+        ) : (
+          <div className="flex grow gap-4 font-sans text-sm overflow-x-scroll">
+            {locationData.map(item => (
+              <Link key={item.contentid} to={`/location/${item.contentid}`} className="w-full h-full flex flex-col gap-2 text-center text-nowrap border-2 border-slate-300 p-2 rounded-xl">
+                <img
+                  src={item.firstimage}
+                  className="grow aspect-square min-w-[200px] max-h-[200px] rounded-lg"
+                  alt={item.title}
+                />
+                <hr className="border-slate-700"/>
+                <p className="grow flex justify-center items-center text-center text-sm font-bold overflow-hidden">
+                  {item.title.length > 10 ? `${item.title.slice(0,15)}...` : item.title}
+                </p>
+              </Link>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
