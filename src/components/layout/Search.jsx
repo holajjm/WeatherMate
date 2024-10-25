16071851069edb172e97206294330d02f7d86be8
holajm <holajjm@naver.com>
2024-10-25 16:09:55 +0900

    Edit Style - Edit Search Bar Style & UI

--- src/components/layout/Search.jsx
+++ src/components/layout/Search.jsx
@@ -15,25 +15,23 @@ function Search({ onClick }) {
   };
 
   return (
-    <form className="box-border">
-      <div className='flex items-center justify-end gap-2'>
-        <p className='text-nowrap font-bold text-lg xl:text-xl'>검색하기</p>
-        <input
-          className="w-full h-8 border rounded-md border-gray-300 focus:outline-none focus:border-blue-500 focus:ring-2 focus:ring-blue-200"
-          type="text"
-          value={keyword}
-          onChange={handleChange}
-        />
-        <Button 
-          className="bg-gray-500 p-1 border rounded flex justify-center items-center"
-          onClick={(e) => {
-            e.preventDefault();
-            onClick(keyword);
-          }}
-        >
-          <IoSearch className="text-2xl"/>
-        </Button>
-      </div>
+    <form className='w-full lg:w-1/2 flex items-center gap-4'>
+      <p className='text-nowrap font-bold text-lg xl:text-xl'>검색하기</p>
+      <input
+        className="w-full h-8 border rounded-md border-gray-300 focus:outline-none focus:border-blue-500 focus:ring-2 focus:ring-blue-200"
+        type="text"
+        value={keyword}
+        onChange={handleChange}
+      />
+      <Button 
+        className="bg-gray-500 p-1 border rounded flex justify-center items-center"
+        onClick={(e) => {
+          e.preventDefault();
+          onClick(keyword);
+        }}
+      >
+        <IoSearch className="text-2xl"/>
+      </Button>
     </form>
   );
 }
