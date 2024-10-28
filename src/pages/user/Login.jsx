7e333220ddde0968f79cd7b603566ea1bdbd76f6
holajm <holajjm@naver.com>
2024-10-28 19:03:18 +0900

    Edit UI & Style - Fixed Final Resposive Layout and Style

--- src/pages/user/Login.jsx
+++ src/pages/user/Login.jsx
@@ -1,9 +1,10 @@
-import useCustomAxios from '@hooks/useCustomAxios.mjs';
-import { useNavigate, Link, useLocation } from 'react-router-dom';
-import { useForm } from 'react-hook-form';
-import { memberState } from '@recoil/atom.mjs';
+import { useNavigate, useLocation } from 'react-router-dom';
 import { useSetRecoilState } from 'recoil';
+import { memberState } from '@recoil/atom.mjs';
+import { useForm } from 'react-hook-form';
+import useCustomAxios from '@hooks/useCustomAxios.mjs';
 import Submit from '@components/layout/Submit.jsx';
+import Button from '@components/layout/Button';
 
 function Login() {
   const location = useLocation();
@@ -24,10 +25,7 @@ function Login() {
 
   const onSubmit = async formData => {
     try {
-      const res = await axios.post(
-        '/users/login',
-        formData,
-      );
+      const res = await axios.post('/users/login', formData);
 
       setUser({
         _id: res.data.item._id,
@@ -50,26 +48,27 @@ function Login() {
       }
     }
   };
-  
-  return (
-    <>
-      <nav className="h-screen flex items-center justify-center bg-slate-100 p-16 max-w-full min-w-80">
-        <div className="bg-white text-center rounded-2xl p-8 w-full max-w-md">
-          <h2 className="text-xl font-semibold text-primary font-TTLaundryGothicB">
-            Weather Mate
-          </h2>
-          <p className="text-sm text-gray-700 font-medium pt-2 pb-2">
-            회원 서비스 이용을 위해 로그인 해주세요
-          </p>
 
-          <form onSubmit={handleSubmit(onSubmit)} className="mt-8">
-            <div className="flex flex-col space-y-4">
+  return (
+    <div className="h-screen bg-gray-100">
+      <div className="w-full flex justify-center p-8 md:px-20 xl:px-56 2xl:px-60 min-w-[375px] drop-shadow-md">
+        <nav className="bg-white flex flex-col w-full h-full lg:w-1/2 p-4 md:p-8 rounded-xl gap-4">
+          <div className="flex flex-col justify-center items-center gap-4">
+            <h2 className="font-black text-3xl text-sky-400 font-Ainmom">
+              Weather Mate
+            </h2>
+            <p className="text-gray-600">
+              회원 서비스 이용을 위해 로그인 해주세요
+            </p>
+          </div>
+
+            <form onSubmit={handleSubmit(onSubmit)} className="grow flex flex-col gap-2">
               <div>
                 <label htmlFor="email" className="sr-only">
                   이메일
                 </label>
                 <input
-                  className="py-2 px-4 rounded-lg bg-slate-100 border-gray-300 focus:ring-2 focus:ring-primary focus:border-transparent w-full"
+                  className="p-4 rounded-xl bg-slate-100 w-full focus:border-2 focus:border-sky-400"
                   type="email"
                   id="email"
                   placeholder="이메일을 입력하세요"
@@ -94,7 +93,7 @@ function Login() {
                   비밀번호
                 </label>
                 <input
-                  className="py-2 px-4 rounded-lg bg-slate-100 border-gray-300 focus:ring-2 focus:ring-primary focus:border-transparent w-full"
+                  className="p-4 rounded-xl bg-slate-100 border-gray-300 focus:ring-2 focus:ring-primary focus:border-transparent w-full"
                   type="password"
                   id="password"
                   placeholder="비밀번호를 입력하세요"
@@ -108,21 +107,24 @@ function Login() {
                   </p>
                 )}
               </div>
-
-              <Submit className="w-full bg-primary rounded-md p-2 hover:bg-primary_deep text-white font-bold">
-                로그인
-              </Submit>
-              <Link
-                className="mt-2 text-primary_deep hover:underline inline-block text-center"
-                to="/user/signup"
-              >
-                회원가입
-              </Link>
-            </div>
-          </form>
-        </div>
-      </nav>
-    </>
+              <div className="flex flex-col items-center gap-2">
+                <div className="flex w-full">
+                  <Submit
+                    text={'로그인'}
+                    className="grow p-4 border-2 border-slate-100 rounded-lg text-base font-semibold transition-all duration-200 text-white bg-sky-400 hover:bg-sky-500 hover:shadow-[0_5px_30px_4px] hover:shadow-slate-400"
+                  ></Submit>
+                  <Button
+                    text={'회원가입'}
+                    onClick={() => navigate('/user/signup')}
+                    className="grow p-4 border-2 border-slate-100 rounded-lg text-base font-semibold transition-all duration-200 text-white bg-indigo-500 hover:bg-indigo-700 hover:shadow-[0_5px_30px_4px] hover:shadow-slate-400"
+                  ></Button>
+                </div>
+                <img className="w-1/4 h-1/4" src="/logo.svg" />
+              </div>
+            </form>
+        </nav>
+      </div>
+    </div>
   );
 }
 
