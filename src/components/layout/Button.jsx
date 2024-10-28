4429fb070bc462023d57517eb5afb40d4d0bd654
holajm <holajjm@naver.com>
2024-10-28 17:16:53 +0900

    Edit Code & Refactor - Make Sure Public Button Component & Clean Up Code Logic

--- src/components/layout/Button.jsx
+++ src/components/layout/Button.jsx
@@ -1,33 +1,18 @@
 import PropTypes from 'prop-types';
 
 Button.propTypes = {
-  children: PropTypes.oneOfType([
-    PropTypes.string,
-    PropTypes.object
-  ]),
-  bgColor: PropTypes.string,
-  size: PropTypes.string,
+  text: PropTypes.string,
   type: PropTypes.string,
+  bgColor: PropTypes.string,
 };
 
-function Button({ children, type="button", bgColor='black', size='md', ...rest }){
+function Button({text,type,bgColor='black',...rest }){
   let btnColor = {
-    gray: `bg-gray-500`,
-    black: 'bg-black',
-    red: 'bg-red-500',
-    
+    sky: "bg-sky-400",
+    indigo: "bg-indigo-500",
   };
-  let btnSize = {
-    sm: 'py-1 px-2 text-sm',
-    md: 'py-1 px-4 text-base',
-    lg: 'py-2 px-6 text-lg',
-    fullsm: 'text-sm',
-    fullmd: 'text-md',
-
-
-  };
-
-  return <button type={ type } className={`${ btnColor[bgColor] } ${ btnSize[size] } text-white font-semibold ml-2 text-base hover:bg-black rounded`} { ...rest }>{ children }</button>
+  
+  return <button type={type} className={`${btnColor[bgColor]}` } { ...rest }>{text}</button>
 }
 
 export default Button;
