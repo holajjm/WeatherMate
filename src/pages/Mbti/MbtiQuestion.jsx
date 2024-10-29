46dfcb099060e40b185d1189c3615ee40a17cba9
holajm <holajjm@naver.com>
2024-10-29 18:07:42 +0900

    Edit UI - Fixed Final Responsive Layout and UI and Style

--- src/pages/Mbti/MbtiQuestion.jsx
+++ src/pages/Mbti/MbtiQuestion.jsx
@@ -1,18 +1,18 @@
-import React from 'react';
+import { useState } from 'react';
 import { createSearchParams, useNavigate } from 'react-router-dom';
 import MbtiQuestionData from '@assets/MbtiQuestionData';
-
-import DetailPageHeader from '@components/layout/DetailPageHeader';
+import mbtiStart from '@assets/mbti/mbtiimg/mbtiStart.png';
+import Button from '@components/layout/Button';
 
 function MbtiQuestion() {
-  const [questionNo, setQuestionNo] = React.useState(0);
-  const [totalScore, setTotalScore] = React.useState([
+  const navigate = useNavigate();
+  const [questionNo, setQuestionNo] = useState(0);
+  const [totalScore, setTotalScore] = useState([
     { id: 'EI', score: 0 },
     { id: 'SN', score: 0 },
     { id: 'TF', score: 0 },
     { id: 'JP', score: 0 },
   ]);
-  const navigate = useNavigate();
 
   //프로그래스바
   const progress = (questionNo + 1) / MbtiQuestionData.length;
@@ -52,45 +52,63 @@ function MbtiQuestion() {
 
   return (
     <>
-      <DetailPageHeader title={'날씨 성격 테스트'} />
-      <div className="min-w-[90vw] min-h-[700px] py-10 px-4 flex gap-10 flex-col items-center font-TTLaundryGothicB  lg:px-12 xl:px-60">
-        <h1 className="text-2xl font-bold">다음 상황에서 당신의 선택은?</h1>
-        <div className="w-full xl:w-[70vw] bg-gray-200 rounded-full ">
-          <div
-            className="bg-primary text-xs xl:text-s text-white text-center py-0.5 rounded-full"
-            style={{ width: `${progress * 100}%` }}
-          >
-            {`${questionNo + 1}/${MbtiQuestionData.length}`}
+      <div className="bg-gray-100 h-screen flex flex-col gap-4 font-TTLaundryGothicB p-4 md:px-20 xl:px-56 2xl:px-60 min-w-[375px]">
+        <div
+          style={{ backgroundImage: `url(${mbtiStart})` }}
+          className="h-full rounded-xl bg-cover bg-center flex items-center justify-center"
+        >
+          <div className="w-full lg:w-1/2 h-full sm:h-1/2 flex flex-col gap-4 bg-slate-100 p-6 rounded-xl">
+            <div className="flex flex-col gap-4">
+              <div className="flex justify-between">
+                {questionNo > 0 && (
+                  <Button
+                    text={"이전 문제 다시 풀기"}
+                    onClick={handleback}
+                    className="rounded-lg bg-white hover:bg-primary border-2 p-2 text-lg text-gray-700 font-bold"
+                  >
+                  </Button>
+                )}
+                <Button
+                  text={'Mbti 메인으로 가기'}
+                  onClick={() => navigate('/mbti')}
+                  className="rounded-lg bg-white hover:bg-red-300 border-2 p-2 text-lg text-gray-700 font-bold"
+                ></Button>
+              </div>
+              <h1 className="text-2xl text-center font-bold">다음 상황에서 당신의 선택은?</h1>
+              <div className="w-full bg-gray-200 rounded-full ">
+                <div
+                  className="bg-primary text-sm text-white text-center py-0.5 rounded-full"
+                  style={{ width: `${progress * 100}%` }}
+                >
+                  {`${questionNo + 1}/${MbtiQuestionData.length}`}
+                </div>
+              </div>
+
+              <div className="flex flex-col gap-8">
+                <div className="text-center border-4 text-xl text-gray-700 rounded-lg p-8 border-primary">
+                  {MbtiQuestionData[questionNo].title}
+                </div>
+                <div className="flex flex-col gap-4">
+                  <Button
+                    text={`${MbtiQuestionData[questionNo].answer1}`}
+                    onClick={() =>
+                      handleClickButton(1, MbtiQuestionData[questionNo].type)
+                    }
+                    className="w-full rounded-lg bg-white hover:bg-primary border-2 p-2 text-lg text-gray-700 font-bold "
+                  >
+                  </Button>
+                  <Button
+                    text={`${MbtiQuestionData[questionNo].answer2}`}
+                    onClick={() =>
+                      handleClickButton(0, MbtiQuestionData[questionNo].type)
+                    }
+                    className="w-full rounded-lg bg-white hover:bg-primary border-2 p-2 text-lg text-gray-700 font-bold text-nowrap"
+                  >
+                  </Button>
+                </div>
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="whitespace-pre-line flex flex-col gap-10 ">
-          <div className="min-w-[70vw] text-center xl:px-40 min-h-[172px] border-4 text-xl text-gray-700 rounded-lg p-10 border-primary">
-            {MbtiQuestionData[questionNo].title}
-          </div>
-          <button
-            onClick={() =>
-              handleClickButton(1, MbtiQuestionData[questionNo].type)
-            }
-            className="min-w-[360px] min-h-[76px] px-10 rounded-lg bg-white hover:bg-primary border-2 p-2 text-lg text-gray-700 font-bold "
-          >
-            {MbtiQuestionData[questionNo].answer1}
-          </button>
-          <button
-            onClick={() =>
-              handleClickButton(0, MbtiQuestionData[questionNo].type)
-            }
-            className="min-w-[360px] min-h-[76px] px-10 rounded-lg bg-white hover:bg-primary border-2 p-2 text-lg  text-gray-700 font-bold "
-          >
-            {MbtiQuestionData[questionNo].answer2}
-          </button>
-          {questionNo > 0 && (
-            <button
-              onClick={handleback}
-              className="min-w-[360px]  px-10 rounded-lg bg-white hover:bg-primary border-2 p-2 text-lg text-gray-700  font-bold "
-            >
-              이전 문제 다시 풀기
-            </button>
-          )}
         </div>
       </div>
     </>
