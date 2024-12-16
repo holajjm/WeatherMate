94063d6818d98ea87ab7cbcecdbe08db5feee733
holajm <holajjm@naver.com>
2024-12-16 17:12:51 +0900

    Remove:JS 파일 제거

--- src/pages/location/noResultMsg.jsx
+++
@@ -1,9 +0,0 @@
-function NoResultsMessage() {
-  return (
-    <div className="flex justify-center items-center h-full">
-      <p className="text-gray-500 text-xl">검색 결과가 없습니다.</p>
-    </div>
-  );
-}
-
-export default NoResultsMessage;
