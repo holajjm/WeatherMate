068a94b8a6d535d91ce109a16821e936370b3531
holajm <holajjm@naver.com>
2024-12-16 16:42:27 +0900

    Rename(config):파일명 TS로 변경

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: [
+      { find: '@', replacement: resolve(__dirname, 'src') },
+      { find: '@assets', replacement: resolve(__dirname, 'src/assets') },
+      { find: '@components', replacement: resolve(__dirname, 'src/components')},
+      { find: '@pages', replacement: resolve(__dirname, 'src/pages') },
+      { find: '@hooks', replacement: resolve(__dirname, 'src/hooks') },
+      { find: '@recoil', replacement: resolve(__dirname, 'src/recoil') },
+    ],
+  },
+});
