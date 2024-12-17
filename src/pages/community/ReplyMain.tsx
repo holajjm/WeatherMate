3a1f278988561858303f9186154a42ae817f2f6f
holajm <holajjm@naver.com>
2024-12-17 15:06:47 +0900

    Rename: TS 파일로 변경 / Refactor: 컴포넌트 분리

---
+++ src/pages/community/ReplyMain.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReplyList from './ReplyList'
+import ReplyNew from './ReplyNew'
+
+function ReplyMain() {
+  return (
+    <div className="p-2 bg-slate-200 rounded-lg drop-shadow-lg flex flex-col gap-2">
+      <ReplyNew />
+      <ReplyList />
+    </div>
+  )
+}
+
+export default ReplyMain
