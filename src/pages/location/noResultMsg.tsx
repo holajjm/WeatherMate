365239537c0b3d11649243cbc046fbe77fa03178
holajm <holajjm@naver.com>
2024-12-17 14:05:53 +0900

    Design: 레이아웃 및 버튼 공용 스타일 적용

---
+++ src/pages/location/noResultMsg.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NoResultsMessage() {
+  return (
+    <div className="flex justify-center items-center h-full">
+      <p className="text-gray-500 text-xl">검색 결과가 없습니다.</p>
+    </div>
+  );
+}
+
+export default NoResultsMessage;
