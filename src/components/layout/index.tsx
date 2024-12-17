8f5fe6c54832fd258d6870735971f1ace279d3be
holajm <holajjm@naver.com>
2024-12-17 15:28:53 +0900

    Rename: TS 파일로 변경

---
+++ src/components/layout/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Header from './Header';
+import Footer from '@components/layout/Footer';
+import NavigationBar from './NavigationBar';
+
+const Layout = () => {
+  return (
+    <div>
+      <Header />
+      <Outlet />
+      <Footer />
+      <NavigationBar />
+    </div>
+  );
+};
+
+export default Layout;
