873d33409b5d12c6e658311e0dded9b537151a2e
holajm <holajjm@naver.com>
2024-12-17 15:30:09 +0900

    Rename: TS 파일로 변경

---
+++ src/components/layout/DetailPageHeader.tsx
@@ -0,0 +1,19 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+DetailPageHeader.propTypes = {
+  title: PropTypes.string,
+};
+
+function DetailPageHeader(title:string) {
+  return (
+    <>
+      <div className=" px-5 p-5 flex items-center justify-center md:hidden xl:mx-60">
+        <h1 className="font-bold text-xl">{title}</h1>
+        <div className="w-5"></div>
+      </div>
+    </>
+  );
+}
+
+export default DetailPageHeader;
