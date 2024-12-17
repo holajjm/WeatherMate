786c8f20a2b466bfb0845cdddf2f670505f8faf3
holajm <holajjm@naver.com>
2024-12-17 13:16:43 +0900

    Design: 레이아웃 및 버튼 공용 스타일 적용:

---
+++ src/pages/user/MainLogin.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import Button from '@components/layout/Button';
+
+function MainLogin() {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const REST_API_KEY = import.meta.env.VITE_KAKAO_REST_API_KEY; //REST API KEY
+  const REDIRECT_URI = 'http://localhost:5173/auth/kakao'; //REDIRECT URI
+  // oauth 요청 URL
+  const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+
+  const handleLogin = () => {
+    window.location.href = kakaoURL;
+  };
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <div className="w-full flex justify-center p-8 md:px-20 xl:px-56 2xl:px-60 min-w-[375px] drop-shadow-md">
+        <nav className="bg-white flex flex-col justify-between w-full lg:w-1/2 p-4 md:p-8 rounded-xl gap-4">
+          <div className="flex flex-col justify-center items-center gap-4">
+            <h2 className="font-black text-3xl text-sky-400 font-Ainmom">
+              Weather Mate
+            </h2>
+            <p className="text-gray-600">
+              오늘의 날씨와 우리의 이야기를 나눠봐요
+            </p>
+          </div>
+          <img className="h-1/3 comment-float" src="/logo.svg" />
+          <div className="w-full grid grid-cols-2 gap-2 md:gap-4 comment-text">
+            <button
+              // text={'로그인'}
+              className="p-2 2xl:p-4 border-2 border-slate-100 rounded-lg text-sm 2xl:text-base font-semibold transition-all duration-200 text-white bg-sky-400 hover:bg-sky-500 hover:shadow-[0_5px_30px_4px] hover:shadow-slate-400"
+              onClick={() => navigate('/user/Login',{state: location.state})}
+            >로그인</button>
+            <button
+              // text={'회원가입'}
+              className="p-2 2xl:p-4 border-2 border-slate-100 rounded-lg text-sm 2xl:text-base font-semibold transition-all duration-200 text-white bg-indigo-500 hover:bg-indigo-700 hover:shadow-[0_5px_30px_4px] hover:shadow-slate-400"
+              onClick={() => navigate('/user/SignUp')}
+            >회원가입</button>
+            <button
+              // text={'카카오로 시작하기'}
+              className="p-2 2xl:p-4 border-2 border-[#FEE500] rounded-lg text-sm 2xl:text-base font-semibold transition-all duration-200 text-gray-500 text-nowrap bg-[#FEE500] hover:bg-yellow-400 hover:shadow-[0_5px_30px_4px] hover:shadow-slate-400 hover:text-white"
+              onClick={handleLogin}
+            >카카오로시작하기</button>
+            <button
+              // text={'메인으로 돌아가기'}
+              className="p-2 2xl:p-4 border-2 border-slate-100 rounded-lg text-sm 2xl:text-base font-semibold transition-all duration-200 text-gray-500 text-nowrap bg-slate-50 hover:bg-slate-400 hover:shadow-[0_5px_30px_4px] hover:shadow-slate-400 hover:text-white"
+              onClick={() => {
+                navigate('/main', { replace: true });
+              }}
+            >메인으로 돌아가기</button>
+          </div>
+        </nav>
+      </div>
+    </div>
+  );
+}
+
+export default MainLogin;
