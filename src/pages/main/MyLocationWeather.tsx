714fdf5cebe356d228a73bf51dbe2d4a2882b6a0
holajm <holajjm@naver.com>
2024-12-17 13:53:11 +0900

    Refactor: 세부 날씨 정보 영역 컴포넌트 분리 / Design: 레이아웃 및 버튼 공용 스타일 적용

---
+++ src/pages/main/MyLocationWeather.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import WeatherDetail from '@pages/main/WeatherDetail';
+
+const MyLocationWeather = () => {
+
+  const data = JSON.parse(sessionStorage.getItem("sessionWeather") as string)
+
+  return (
+    <div className="bg-white flex flex-col gap-4 px-2">
+      <header>
+        <h1 className="font-extrabold text-xl">
+          <span className="text-2xl text-blue-600">웨더메이트</span>가 알려주는
+          날씨 정보
+        </h1>
+        <h2 className="text-slate-400">오늘의 상세 날씨를 알려드려요!</h2>
+      </header>
+      <main className="flex flex-col gap-4 items-center justify-center h-full bg-contain bg-center bg-no-repeat border-slate-200">
+        {data && (
+          <>
+            <div className="w-full border-2 rounded-xl text-5xl font-bold text-center shadow-lg shadow-slate-200">
+              <h2 className="text-xl font-bold">{data.name}</h2>
+              {data.main.temp.toFixed(1)}°C
+              <p className="text-lg font-bold">{data.weather[0].description}</p>
+            </div>
+            <WeatherDetail />
+          </>
+        )}
+      </main>
+    </div>
+  );
+};
+export default MyLocationWeather;
