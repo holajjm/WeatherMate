2574a0d64dd65f102f83e520c93bc3be24919957
holajm <holajjm@naver.com>
2025-01-17 20:45:28 +0900

    Fix: 라우팅 파일 페이지별 정리

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,71 +1,76 @@
-import React from 'react';
-import { createBrowserRouter } from 'react-router-dom';
-import Layout from '@components/layout';
-import MainPage from '@pages/main/MainPage';
-import ErrorPage from '@pages/ErrorPage';
-import CommunityNew from '@pages/community/CommunityNew';
-import CommunityDetail from '@pages/community/CommunityDetail';
-import MbtiQuestion from '@pages/Mbti/MbtiQuestion';
-import MbtiResult from '@pages/Mbti/MbtiResult';
-import SignUp from '@pages/user/SignUp';
-import Login from '@pages/user/Login';
-import UserPage from '@pages/user/UserPage';
-import CommunityMain from '@pages/community/CommunityMain';
-import LocationDetailPage from '@pages/location/LocationDetailPage';
-import LocationMainPage from '@pages/location/LocationMainPage';
-import Setting from '@pages/user/Setting';
-import UserEdit from '@pages/user/UserEdit';
-import AllCitiesWeather from '@pages/main/AllCitiesWeather';
-import Oauth from '@pages/user/Oauth';
-import MbtiHome from '@pages/Mbti/MbtiHome';
-import ValidLogin from '@pages/user/ValidLogin';
-import PreviewMbti from '@pages/main/PreviewMbti';
-import ReplyMain from '@pages/community/ReplyMain';
-import CommunityEdit from '@pages/community/CommunityEdit';
+import React from "react";
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "@components/layout";
+import ErrorPage from "@pages/ErrorPage";
+// MainPage
+import MainHomePage from "@pages/main/MainHomePage";
+import MainAllCitiesWeather from "@pages/main/MainAllCitiesWeather";
+import MainPreviewMbti from "@pages/main/MainPreviewMbti";
+// Community
+import CommunityNew from "@pages/community/CommunityNew";
+import CommunityDetail from "@pages/community/CommunityDetail";
+import CommunityMain from "@pages/community/CommunityMain";
+import ReplyMain from "@pages/community/ReplyMain";
+import CommunityEdit from "@pages/community/CommunityEdit";
+// Location
+import LocationDetailPage from "@pages/location/LocationDetailPage";
+import LocationMainPage from "@pages/location/LocationMainPage";
+// Mbti
+import MbtiQuestion from "@pages/Mbti/MbtiQuestion";
+import MbtiResult from "@pages/Mbti/MbtiResult";
+import MbtiHome from "@pages/Mbti/MbtiHome";
+// User
+import UserLogin from "@pages/user/UserLogin";
+import UserValidLogin from "@pages/user/UserValidLogin";
+import UserOAuth from "@pages/user/UserOAuth";
+import UserSetting from "@pages/user/UserSetting";
+import UserSignUp from "@pages/user/UserSignUp";
+import UserPage from "@pages/user/UserPage";
+import UserEdit from "@pages/user/UserEdit";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     errorElement: <ErrorPage />,
     children: [
       {
-        path: '/',
+        path: "/",
         element: <Layout />,
         children: [
           {
             index: true,
-            element: <MainPage />,
+            element: <MainHomePage />,
+          },
+
+          //Main
+          {
+            path: "main",
+            element: <MainHomePage />,
           },
           {
-            path: 'main',
-            element: <MainPage />,
+            path: "allcity",
+            element: <MainAllCitiesWeather />,
           },
           {
-            path: "mainlogin",
-            element: <ValidLogin />
+            path: "category",
+            element: <MainPreviewMbti />,
           },
+
+          // Community
           {
-            path: 'allcity',
-            element: <AllCitiesWeather />,
-          },
-          {
-            path: 'category',
-            element: <PreviewMbti />,
-          },
-          {
-            path: 'community',
+            path: "community",
             element: <CommunityMain />,
           },
           {
-            path: 'community/new',
+            path: "community/new",
             element: <CommunityNew />,
           },
           {
-            path: 'community/edit',
+            path: "community/edit",
             element: <CommunityEdit />,
           },
           {
-            path: 'community/:_id',
+            path: "community/:_id",
             element: <CommunityDetail />,
             children: [
               {
@@ -74,53 +79,59 @@ const router = createBrowserRouter([
               },
             ],
           },
+
+          // Location
           {
-            path: 'location',
+            path: "location",
             element: <LocationMainPage />,
           },
           {
-            path: 'location/:id',
+            path: "location/:id",
             element: <LocationDetailPage />,
           },
+
+          // Mbti
           {
-            path: 'mbti',
+            path: "mbti",
             element: <MbtiHome />,
           },
           {
-            path: 'mbti/question',
+            path: "mbti/question",
             element: <MbtiQuestion />,
           },
           {
-            path: 'mbti/result',
+            path: "mbti/result",
             element: <MbtiResult />,
           },
+
+          // User
           {
-            path: 'user/mypage',
+            path: "user/login",
+            element: <UserLogin />,
+          },
+          {
+            path: "mainlogin",
+            element: <UserValidLogin />,
+          },
+          {
+            path: "auth/kakao",
+            element: <UserOAuth />,
+          },
+          {
+            path: "user/setting",
+            element: <UserSetting />,
+          },
+          {
+            path: "user/signup",
+            element: <UserSignUp />,
+          },
+          {
+            path: "user/mypage",
             element: <UserPage />,
           },
           {
-            path: 'user/signup',
-            element: <SignUp />,
-          },
-          {
-            path: 'user/login',
-            element: <Login />,
-          },
-          {
-            path: 'user/validlogin',
-            element: <ValidLogin />,
-          },
-          {
-            path: 'user/setting',
-            element: <Setting />,
-          },
-          {
-            path: 'user/edit',
+            path: "user/edit",
             element: <UserEdit />,
-          },
-          {
-            path: 'auth/kakao',
-            element: <Oauth />,
           },
         ],
       },
