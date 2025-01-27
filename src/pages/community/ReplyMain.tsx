8338851230df22d55502ce826e244501c7628228
holajm <holajjm@naver.com>
2025-01-27 18:12:02 +0900

    Refactor: 댓글 컴포넌트 병합에 따른 JSX 문법 수정

--- src/pages/community/ReplyMain.tsx
+++ src/pages/community/ReplyMain.tsx
@@ -1,13 +1,28 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import useCustomAxios from "@hooks/useCustomAxios.mjs";
+import { useQuery } from "@tanstack/react-query";
+import { ReplyData } from "type";
 
-import ReplyList from "./ReplyList";
 import ReplyNew from "./ReplyNew";
+import ReplyItem from "./ReplyItem";
 
 function ReplyMain() {
+  const { _id } = useParams();
+  const axios = useCustomAxios();
+  const { data } = useQuery({
+    queryKey: ["posts", _id, "replies"],
+    queryFn: () => axios.get(`/posts/${_id}/replies`),
+  });
   return (
     <div className="p-2 bg-slate-200 rounded-lg drop-shadow-lg flex flex-col gap-2">
       <ReplyNew />
-      <ReplyList />
+      <div className="grid gap-2">
+        {data?.data?.item &&
+          data?.data?.item.map((e: ReplyData) => (
+            <ReplyItem key={e._id} item={e} />
+          ))}
+      </div>
     </div>
   );
 }
