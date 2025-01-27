6c95b47306be5300cb7f61668d5a90a0d92e96db
holajm <holajjm@naver.com>
2025-01-27 18:05:05 +0900

    Design: 게시글 편집 컴포넌트 스타일 부여

--- src/pages/community/CommunityEdit.tsx
+++ src/pages/community/CommunityEdit.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function CommunityEdit() {
-  return <div>CommunityEdit</div>;
+  return <div className="max-w-[600px] min-w-[320px] m-auto h-screen flex flex-col gap-4 overflow-y-scroll scrollbar-hide p-2 bg-slate-50">CommunityEdit</div>;
 }
 
 export default CommunityEdit;
