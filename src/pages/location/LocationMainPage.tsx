fc85d5a2dfac463e332934a0f7961af7e5c72eda
holajm <holajjm@naver.com>
2025-02-17 14:05:12 +0900

    Refactor: 스크롤 최상단 위치 커스텀 훅 적용

--- src/pages/location/LocationMainPage.tsx
+++ src/pages/location/LocationMainPage.tsx
@@ -2,10 +2,12 @@ import React from "react";
 
 import Location from "@pages/location/Location";
 import usePageTitle from "@hooks/usePageTitle";
+import useScrollTop from "@hooks/useScrollTop";
 import ToTheTopButton from "@components/layout/ToTheTopButton";
 
 function LocationMainPage() {
   usePageTitle("Location");
+  useScrollTop();
   return (
     <div className="max-w-[600px] min-w-[320px] m-auto">
       <ToTheTopButton />
