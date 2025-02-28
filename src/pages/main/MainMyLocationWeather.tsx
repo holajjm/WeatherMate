5622e083601cdb8e54db3de451548ae604647b66
holajm <holajjm@naver.com>
2025-02-28 13:20:40 +0900

    Design: 텍스트 및 요소 사이즈 축소

--- src/pages/main/MainMyLocationWeather.tsx
+++ src/pages/main/MainMyLocationWeather.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+
 import MainWeatherDetail from "@pages/main/MainWeatherDetail";
 import MainLocationWeatherSkeleton from "@components/skeleton/MainLocationWeatherSkeleton";
 
@@ -8,17 +9,17 @@ const MainMyLocationWeather = () => {
   return (
     <div className="bg-white flex flex-col gap-4 px-2">
       <header>
-        <h1 className="font-extrabold text-xl">
-          <span className="text-2xl text-blue-600">웨더메이트</span>가 알려주는
+        <h1 className="font-black text-lg text-[#2D2D2D]">
+          <span className="text-xl text-blue-600">웨더메이트</span>가 알려주는
           날씨 정보
         </h1>
-        <h2 className="text-slate-400">오늘의 상세 날씨를 알려드려요!</h2>
+        <h2 className="text-slate-400 text-base">오늘의 상세 날씨를 알려드려요!</h2>
       </header>
       <main className="flex flex-col gap-4 items-center justify-center h-full bg-contain bg-center bg-no-repeat border-slate-200">
         {data ? (
           <>
-            <div className="w-full border-2 rounded-xl text-5xl font-bold text-center shadow-lg shadow-slate-200">
-              <h2 className="text-xl font-bold">{data.name}</h2>
+            <div className="w-full border-2 rounded-xl text-4xl font-bold text-center shadow-lg shadow-slate-200">
+              <h2 className="text-lg font-bold">{data.name}</h2>
               {data.main.temp.toFixed(1)}°C
               <p className="text-lg font-bold">{data.weather[0].description}</p>
             </div>
