a0b82eb0e310430c61247fd9584c7295cdfb4627
holajm <holajjm@naver.com>
2025-02-28 13:16:42 +0900

    Design: 네비게이션 바 위치 상단으로 변경 및 스타일 수정

--- src/components/layout/NavigationBar.tsx
+++ src/components/layout/NavigationBar.tsx
@@ -5,63 +5,62 @@ import { GoHomeFill } from "react-icons/go";
 import { IoMdChatboxes } from "react-icons/io";
 import { MdLocationOn } from "react-icons/md";
 import { FaUserCircle } from "react-icons/fa";
-import { BiSolidCategory } from "react-icons/bi";
+// import { BiSolidCategory } from "react-icons/bi";
 
 function NavigationBar() {
   const location = useLocation();
   return (
-    <footer className="max-w-[600px] min-w-[320px] m-auto py-2 flex text-white h-16 items-center justify-around gap-4 font-bold bg-blue-200 sticky bottom-0 z-20 shadow-inner font-UhBeeKangJa">
-      <Link
+    <footer className="max-w-[600px] min-w-[320px] m-auto py-2 border-b-[1px] border-slate-300 flex text-white h-18 items-center gap-4 font-bold bg-white sticky top-0 z-20 shadow-inner font-UhBeeKangJa">
+      {/* <Link
         to="/category"
-        className={`w-20 flex flex-col gap-1 items-center p-1 ${location.pathname === "/category" ? "text-indigo-600 border-2 rounded-2xl bg-white shadow-inner shadow-slate-500" : "text-indigo-500 hover:text-white"}`}
+        className={`w-20 flex flex-col gap-1 items-center p-1 ${location.pathname === "/category" ? "text-indigo-500" : "text-slate-400 hover:text-indigo-500"}`}
       >
-        <BiSolidCategory className="text-2xl" />
+        <BiSolidCategory />
         <p className="text-nowrap text-sm">카테고리</p>
-      </Link>
-
+      </Link> */}
       <Link
-        to="/community"
-        className={`w-20 flex flex-col gap-1 items-center p-1 ${
-          location.pathname === "/community"
-            ? "text-indigo-600 border-2 rounded-2xl bg-white shadow-inner shadow-slate-500"
-            : "text-indigo-500 hover:text-white"
+        to="/main"
+        className={`w-full flex flex-col gap-1 items-center p-1 ${
+          location.pathname === "/main" || location.pathname === "/"
+            ? "text-indigo-500"
+            : "text-slate-400 hover:text-indigo-500"
         }`}
       >
-        <IoMdChatboxes className="text-2xl" />
+        {/* <GoHomeFill /> */}
+        <p className="text-nowrap text-sm">홈</p>
+      </Link>
+      <Link
+        to="/community"
+        className={`w-full flex flex-col gap-1 items-center p-1 ${
+          location.pathname === "/community"
+            ? "text-indigo-500"
+            : "text-slate-400 hover:text-indigo-500"
+        }`}
+      >
+        {/* <IoMdChatboxes /> */}
         <p className="text-nowrap text-sm">커뮤니티</p>
       </Link>
 
       <Link
-        to="/main"
-        className={`w-20 flex flex-col gap-1 items-center p-1 ${
-          location.pathname === "/main" || location.pathname === "/"
-            ? "text-indigo-600 border-2 rounded-2xl bg-white shadow-inner shadow-slate-500"
-            : "text-indigo-500 hover:text-white"
-        }`}
-      >
-        <GoHomeFill className="text-2xl" />
-        <p className="text-nowrap text-sm">홈</p>
-      </Link>
-      <Link
         to="/location"
-        className={`w-20 flex flex-col gap-1 items-center p-1 ${
+        className={`w-full flex flex-col gap-1 items-center p-1 ${
           location.pathname === "/location"
-            ? "text-indigo-600 border-2 rounded-2xl bg-white shadow-inner shadow-slate-500"
-            : "text-indigo-500 hover:text-white"
+            ? "text-indigo-500"
+            : "text-slate-400 hover:text-indigo-500"
         }`}
       >
-        <MdLocationOn className="text-2xl" />
+        {/* <MdLocationOn /> */}
         <p className="text-nowrap text-sm">장소추천</p>
       </Link>
       <Link
         to="/user/mypage"
-        className={`w-20 flex flex-col gap-1 items-center p-1 ${
+        className={`w-full flex flex-col gap-1 items-center p-1 ${
           location.pathname === "/user/mypage"
-            ? "text-indigo-600 border-2 rounded-2xl bg-white shadow-inner shadow-slate-500"
-            : "text-indigo-500 hover:text-white"
+            ? "text-indigo-500"
+            : "text-slate-400 hover:text-indigo-500"
         }`}
       >
-        <FaUserCircle className="text-2xl" />
+        {/* <FaUserCircle /> */}
         <p className="text-nowrap text-sm">마이</p>
       </Link>
     </footer>
