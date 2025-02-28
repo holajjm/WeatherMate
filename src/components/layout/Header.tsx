cd0dbc16f9d1517c32ff06f6c7e7082ecf9efdf0
holajm <holajjm@naver.com>
2025-02-28 13:10:56 +0900

    Feat: 카테고리 메뉴 상단바 햄버거 메뉴로 전환 & Design: 상단바 높이 및 타이틀 폰트 사이즈 축소

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,5 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
+
+import { AnimatePresence, motion } from "framer-motion";
+
+import MainCategory from "@pages/main/MainCategory";
+import { CgMenuLeftAlt } from "react-icons/cg";
+import { IoIosArrowDropleft } from "react-icons/io";
 
 function Header() {
   const location = useLocation();
@@ -10,17 +16,46 @@ function Header() {
       window.location.reload();
     }
   };
+  const [open, setOpen] = useState<boolean>(false);
+  const handleCategoryOpen = () => {
+    setOpen(!open);
+  };
 
   return (
-    <div className="max-w-[600px] min-w-[320px] h-16 m-auto sticky top-0 z-30 flex items-center justify-center bg-blue-200 shadow-[0px_3px_4px_0px_rgba(0,0,0,0.7)] shadow-slate-300">
+    <div className="max-w-[600px] min-w-[320px] h-14 m-auto sticky top-0 z-50 flex items-center justify-center bg-blue-200">
+      {!open ? (
+        <CgMenuLeftAlt
+          className="text-2xl absolute left-4"
+          onClick={handleCategoryOpen}
+        />
+      ) : (
+        <IoIosArrowDropleft
+          className="text-2xl absolute left-4"
+          onClick={handleCategoryOpen}
+        />
+      )}
+      <AnimatePresence>
+        {open && <motion.div
+          initial={{ x: -100, opacity: 0 }}
+          animate={{ opacity: 1, x: 0 }}
+          exit={{ opacity: 0, x: -100 }}
+          transition={{
+            ease: "easeInOut",
+            duration: 0.8,
+          }}
+          className={`w-2/3 h-full absolute top-14 left-0 bg-black`}
+        >
+          <MainCategory />
+        </motion.div>}
+      </AnimatePresence>
       <Link to="/" className="flex justify-center">
         <img
           src="/weatherMateLogo.svg"
           alt="weathermate logo"
-          className="w-20"
+          className="w-10"
           onClick={handleRefresh}
         />
-        <p className="text-4xl text-orange-400 font-extrabold font-UhBeeKangJa">
+        <p className="text-2xl text-orange-400 font-extrabold font-UhBeeKangJa">
           WeatherMate
         </p>
       </Link>
