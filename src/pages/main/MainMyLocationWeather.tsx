9d9f4a59647566d8b804cc534ca3fc471ce37742
holajm <holajjm@naver.com>
2025-03-20 22:32:38 +0900

    Refactor: 시멘틱 태그 적용 & Style: 코드 정리

--- src/pages/main/MainMyLocationWeather.tsx
+++ src/pages/main/MainMyLocationWeather.tsx
@@ -7,15 +7,17 @@ const MainMyLocationWeather = () => {
   const data = JSON.parse(sessionStorage.getItem("sessionWeather") as string);
 
   return (
-    <div className="bg-white flex flex-col gap-4 px-2">
+    <article className="bg-white flex flex-col gap-4 px-2">
       <header>
         <h1 className="font-black text-lg text-[#2D2D2D]">
           <span className="text-xl text-blue-600">웨더메이트</span>가 알려주는
           날씨 정보
         </h1>
-        <h2 className="text-slate-400 text-base">오늘의 상세 날씨를 알려드려요!</h2>
+        <h2 className="text-slate-400 text-base">
+          오늘의 상세 날씨를 알려드려요!
+        </h2>
       </header>
-      <main className="flex flex-col gap-4 items-center justify-center h-full bg-contain bg-center bg-no-repeat border-slate-200">
+      <section className="flex flex-col gap-4 items-center justify-center h-full bg-contain bg-center bg-no-repeat border-slate-200">
         {data ? (
           <>
             <div className="w-full border-2 rounded-xl text-4xl font-bold text-center shadow-lg shadow-slate-200">
@@ -28,8 +30,8 @@ const MainMyLocationWeather = () => {
         ) : (
           <MainLocationWeatherSkeleton />
         )}
-      </main>
-    </div>
+      </section>
+    </article>
   );
 };
 
--- src/pages/main/MainWeatherDetail.tsx
+++ src/pages/main/MainWeatherDetail.tsx
@@ -22,7 +22,7 @@ function MainWeatherDetail() {
   return (
     <>
       {data && (
-        <div className="grow flex flex-col w-full h-full justify-center items-center gap-2 font-TTLaundryGothicB text-sm">
+        <article className="grow flex flex-col w-full h-full justify-center items-center gap-2 font-TTLaundryGothicB text-sm">
           <section className="w-full flex gap-2 justify-between">
             <div className="bg-white w-full h-full text-center text-nowrap flex flex-col gap-1 items-center justify-center p-1 border-[1px] shadow-lg shadow-slate-200 rounded-lg">
               <img src="sunrise.svg" className="w-8" />
@@ -69,19 +69,19 @@ function MainWeatherDetail() {
               </p>
             </div>
             <div className="bg-white w-full h-full text-center text-nowrap flex flex-col gap-1 items-center justify-center p-1 border-[1px] shadow-lg shadow-slate-200 rounded-lg">
-              <img src="windspeed.svg" className="w-8" />
+              <img src="WeatherIcon6.svg" className="w-8" />
               <p className="text-slate-600">풍속</p>
               <hr className="border-[1px] border-slate-400 w-3/4" />
               <p className="text-slate-700">{data.wind.speed}m/s</p>
             </div>
             <div className="bg-white w-full h-full text-center text-nowrap flex flex-col gap-1 items-center justify-center p-1 border-[1px] shadow-lg shadow-slate-200 rounded-lg">
-              <img src="uvi.svg" className="w-8" />
+              <img src="WeatherIcon1.svg" className="w-8" />
               <p className="text-slate-600">자외선</p>
               <hr className="border-[1px] border-slate-400 w-3/4" />
               <p className="text-slate-700">{getWindStatus(data.wind.speed)}</p>
             </div>
           </section>
-        </div>
+        </article>
       )}
     </>
   );
