bc110ed3a85ba9b28d7b27f893e5ad8d71b24892
holajm <holajjm@naver.com>
2025-03-26 16:14:55 +0900

    Design: 폰트 및 사이즈 변경

--- src/pages/user/UserValidLogin.tsx
+++ src/pages/user/UserValidLogin.tsx
@@ -25,10 +25,10 @@ function UserValidLogin() {
       <div className="p-4 flex justify-center drop-shadow-md">
         <div className="bg-white flex flex-col w-full p-4 rounded-xl gap-4">
           <div className="flex flex-col justify-center items-center gap-4">
-            <h2 className="text-3xl font-black text-sky-400 font-UhBeeKangJa">
+            <h2 className="text-xl font-black text-sky-400 font-UhBeeKangJa">
               로그인 후 이용할 수 있어요!
             </h2>
-            <p className="text-slate-600 text-xl font-SSRONETHandwritten">
+            <p className="text-slate-600 text-lg font-SSRONETHandwritten">
               원활한 서비스 사용을 위해 로그인을 해주세요!
             </p>
             <img className="w-1/3" src="/mainlogin.svg" />
@@ -36,7 +36,7 @@ function UserValidLogin() {
           <div className="w-full grid grid-cols-2 gap-2">
             <button
               // text={'로그인'}
-              className="p-4 border-2 border-slate-100 rounded-lg font-UhBeeKangJa transition-all duration-200 text-white bg-sky-400 hover:shadow-[0_4px_8px_1px] hover:shadow-slate-400"
+              className="p-4 border-2 border-slate-100 rounded-lg font-SSRONETHandwritten font-bold transition-all duration-200 text-white bg-sky-400 hover:shadow-[0_4px_8px_1px] hover:shadow-slate-400"
               onClick={() =>
                 navigate("/user/Login", { state: location.pathname })
               }
@@ -45,21 +45,21 @@ function UserValidLogin() {
             </button>
             <button
               // text={'회원가입'}
-              className="p-4 border-2 border-slate-100 rounded-lg font-UhBeeKangJa transition-all duration-200 text-white bg-indigo-500 hover:shadow-[0_4px_8px_1px] hover:shadow-slate-400"
+              className="p-4 border-2 border-slate-100 rounded-lg font-SSRONETHandwritten font-bold transition-all duration-200 text-white bg-indigo-500 hover:shadow-[0_4px_8px_1px] hover:shadow-slate-400"
               onClick={() => navigate("/user/SignUp")}
             >
               회원가입
             </button>
             <button
               // text={'카카오로 시작하기'}
-              className="p-4 border-2 border-slate-100 rounded-lg font-UhBeeKangJa transition-all duration-200 text-gray-500 text-nowrap bg-[#FEE500] hover:shadow-[0_4px_8px_1px] hover:shadow-slate-400"
+              className="p-4 border-2 border-slate-100 rounded-lg font-SSRONETHandwritten font-bold transition-all duration-200 text-gray-500 text-nowrap bg-[#FEE500] hover:shadow-[0_4px_8px_1px] hover:shadow-slate-400"
               onClick={handleLogin}
             >
               카카오로 시작하기
             </button>
             <button
               // text={'메인으로 돌아가기'}
-              className="p-4 border-2 border-slate-100 rounded-lg font-UhBeeKangJa transition-all duration-200 text-gray-500 text-nowrap bg-slate-200 hover:shadow-[0_4px_8px_1px] hover:shadow-slate-400"
+              className="p-4 border-2 border-slate-100 rounded-lg font-SSRONETHandwritten font-bold transition-all duration-200 text-gray-500 text-nowrap bg-slate-200 hover:shadow-[0_4px_8px_1px] hover:shadow-slate-400"
               onClick={() => navigate("/")}
             >
               메인으로 돌아가기
