260d529368969cb18d095c5dc6f8d342d5325aab
holajm <holajjm@naver.com>
2025-04-03 17:04:14 +0900

    Design: 가로 높이 설정

--- src/pages/main/MainWeatherDetail.tsx
+++ src/pages/main/MainWeatherDetail.tsx
@@ -24,68 +24,68 @@ function MainWeatherDetail() {
       {data && (
         <article className="grow flex flex-col w-full h-full justify-center items-center gap-1 font-TTLaundryGothicB text-sm">
           <section className="w-full flex gap-1 justify-between">
-            <div className="bg-white w-full h-full text-center text-nowrap flex flex-col gap-1 items-center justify-center p-1 border-[1px] shadow-lg shadow-slate-200 rounded-lg">
+            <div className="bg-white w-full h-24 text-center text-nowrap flex flex-col gap-1 items-center justify-center p-1 border-[1px] shadow-lg shadow-slate-200 rounded-lg">
               <img src="sunrise.svg" className="w-8" />
               <p className="text-slate-600">일출</p>
-              <hr className="border-[1px] border-slate-400 w-3/4" />
+              <hr className="border-[1px] border-slate-400 w-5/6" />
               <p className="text-slate-700">{unixTime(data.sys.sunrise)}</p>
             </div>
-            <div className="bg-white w-full h-full text-center text-nowrap flex flex-col gap-1 items-center justify-center p-1 border-[1px] shadow-lg shadow-slate-200 rounded-lg">
+            <div className="bg-white w-full h-24 text-center text-nowrap flex flex-col gap-1 items-center justify-center p-1 border-[1px] shadow-lg shadow-slate-200 rounded-lg">
               <img src="sunset.svg" className="w-8" />
               <p className="text-slate-600">일몰</p>
-              <hr className="border-[1px] border-slate-400 w-3/4" />
+              <hr className="border-[1px] border-slate-400 w-5/6" />
               <p className="text-slate-700">{unixTime(data.sys.sunset)}</p>
             </div>
-            <div className="bg-white w-full h-full text-center text-nowrap flex flex-col gap-1 items-center justify-center p-1 border-[1px] shadow-lg shadow-slate-200 rounded-lg">
+            <div className="bg-white w-full h-24 text-center text-nowrap flex flex-col gap-1 items-center justify-center p-1 border-[1px] shadow-lg shadow-slate-200 rounded-lg">
               <img src="hightemperature.svg" className="w-8" />
               <p className="text-slate-600">최고 온도</p>
-              <hr className="border-[1px] border-slate-400 w-3/4" />
+              <hr className="border-[1px] border-slate-400 w-5/6" />
               <p className="text-slate-700">
                 {data.main.temp_max.toFixed(1)}°C
               </p>
             </div>
           </section>
           <section className="w-full flex gap-1 justify-between">
-            <div className="bg-white w-full h-full text-center text-nowrap flex flex-col gap-1 items-center justify-center p-1 border-[1px] shadow-lg shadow-slate-200 rounded-lg">
+            <div className="bg-white w-full h-24 text-center text-nowrap flex flex-col gap-1 items-center justify-center p-1 border-[1px] shadow-lg shadow-slate-200 rounded-lg">
               <img src="lowtemperature.svg" className="w-8" />
               <p className="text-slate-600">최저 온도</p>
-              <hr className="border-[1px] border-slate-400 w-3/4" />
+              <hr className="border-[1px] border-slate-400 w-5/6" />
               <p className="text-slate-700">
                 {data.main.temp_min.toFixed(1)}°C
               </p>
             </div>
-            <div className="bg-white w-full h-full text-center text-nowrap flex flex-col gap-1 items-center justify-center p-1 border-[1px] shadow-lg shadow-slate-200 rounded-lg">
+            <div className="bg-white w-full h-24 text-center text-nowrap flex flex-col gap-1 items-center justify-center p-1 border-[1px] shadow-lg shadow-slate-200 rounded-lg">
               <img src="humidity.svg" className="w-8" />
               <p className="text-slate-600">습도</p>
-              <hr className="border-[1px] border-slate-400 w-3/4" />
+              <hr className="border-[1px] border-slate-400 w-5/6" />
               <p className="text-slate-700">{data.main.humidity}%</p>
             </div>
-            <div className="bg-white w-full h-full text-center text-nowrap flex flex-col gap-1 items-center justify-center p-1 border-[1px] shadow-lg shadow-slate-200 rounded-lg">
+            <div className="bg-white w-full h-24 text-center text-nowrap flex flex-col gap-1 items-center justify-center p-1 border-[1px] shadow-lg shadow-slate-200 rounded-lg">
               <img src="feelslike.svg" className="w-8" />
               <p className="text-slate-600">체감온도</p>
-              <hr className="border-[1px] border-slate-400 w-3/4" />
+              <hr className="border-[1px] border-slate-400 w-5/6" />
               <p className="text-slate-700">
                 {data.main.feels_like.toFixed(1)}°C
               </p>
             </div>
           </section>
           <section className="w-full flex gap-1 justify-between">
-            <div className="bg-white w-full h-full text-center text-nowrap flex flex-col gap-1 items-center justify-center p-1 border-[1px] shadow-lg shadow-slate-200 rounded-lg">
+            <div className="bg-white w-full h-24 text-center text-nowrap flex flex-col gap-1 items-center justify-center p-1 border-[1px] shadow-lg shadow-slate-200 rounded-lg">
               <img src="WeatherIconWind.svg" className="w-8" />
               <p className="text-slate-600">풍속</p>
-              <hr className="border-[1px] border-slate-400 w-3/4" />
+              <hr className="border-[1px] border-slate-400 w-5/6" />
               <p className="text-slate-700">{data.wind.speed}m/s</p>
             </div>
-            <div className="bg-white w-full h-full text-center text-nowrap flex flex-col gap-1 items-center justify-center p-1 border-[1px] shadow-lg shadow-slate-200 rounded-lg">
+            <div className="bg-white w-full h-24 text-center text-nowrap flex flex-col gap-1 items-center justify-center p-1 border-[1px] shadow-lg shadow-slate-200 rounded-lg">
               <img src="WeatherIconSun.svg" className="w-8" />
               <p className="text-slate-600">자외선</p>
-              <hr className="border-[1px] border-slate-400 w-3/4" />
+              <hr className="border-[1px] border-slate-400 w-5/6" />
               <p className="text-slate-700">{getWindStatus(data.wind.speed)}</p>
             </div>
-            <div className="bg-white w-full h-full text-center text-nowrap flex flex-col gap-1 items-center justify-center p-1 border-[1px] shadow-lg shadow-slate-200 rounded-lg">
+            <div className="bg-white w-full h-24 text-center text-nowrap flex flex-col gap-1 items-center justify-center p-1 border-[1px] shadow-lg shadow-slate-200 rounded-lg">
               <img src="WeatherIconRain.svg" className="w-8" />
               <p className="text-slate-600">강수량</p>
-              <hr className="border-[1px] border-slate-400 w-3/4" />
+              <hr className="border-[1px] border-slate-400 w-5/6" />
               <p className="text-slate-700">
                 {data?.rain ? data?.rain?.["1h"] : "00"}mm
               </p>
