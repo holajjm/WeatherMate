1aa4830c766e1031f3811ad79a8e767dd0cfddf7
holajm <holajjm@naver.com>
2025-04-03 17:47:21 +0900

    Refactor: 메인 페이지 라우팅 url 수정

--- src/components/layout/NavigationBar.tsx
+++ src/components/layout/NavigationBar.tsx
@@ -6,7 +6,7 @@ function NavigationBar() {
   return (
     <nav className="max-w-[600px] min-w-[320px] m-auto border-b-[1px] border-slate-300 flex text-white text-lg h-18 items-center gap-4 font-bold bg-white sticky top-0 z-20 shadow-inner font-SSRONETHandwritten">
       <Link
-        to="/main"
+        to="/"
         className={`w-full flex flex-col gap-1 items-center p-1 ${
           location.pathname === "/main" || location.pathname === "/"
             ? "text-indigo-500"
