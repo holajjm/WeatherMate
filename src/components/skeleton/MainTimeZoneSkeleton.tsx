4487679461112c7f5a7fd2ab82a630f48a9939b0
holajm <holajjm@naver.com>
2025-04-03 17:37:33 +0900

    Design: 스켈레톤 UI 가로 사이즈 설정

--- src/components/skeleton/MainTimeZoneSkeleton.tsx
+++ src/components/skeleton/MainTimeZoneSkeleton.tsx
@@ -3,13 +3,13 @@ import React from "react";
 function MainTimeZoneSkeleton() {
   return (
     <div className="w-full py-1 flex gap-1 overflow-x-scroll scrollbar-hide">
-      <div className="h-24 w-14 p-2 bg-slate-300 rounded-lg"></div>
-      <div className="h-24 w-14 p-2 bg-slate-300 rounded-lg"></div>
-      <div className="h-24 w-14 p-2 bg-slate-300 rounded-lg"></div>
-      <div className="h-24 w-14 p-2 bg-slate-300 rounded-lg"></div>
-      <div className="h-24 w-14 p-2 bg-slate-300 rounded-lg"></div>
-      <div className="h-24 w-14 p-2 bg-slate-300 rounded-lg"></div>
-      <div className="h-24 w-14 p-2 bg-slate-300 rounded-lg"></div>
+      <div className="h-24 w-full min-w-14 p-2 bg-slate-300 rounded-lg"></div>
+      <div className="h-24 w-full min-w-14 p-2 bg-slate-300 rounded-lg"></div>
+      <div className="h-24 w-full min-w-14 p-2 bg-slate-300 rounded-lg"></div>
+      <div className="h-24 w-full min-w-14 p-2 bg-slate-300 rounded-lg"></div>
+      <div className="h-24 w-full min-w-14 p-2 bg-slate-300 rounded-lg"></div>
+      <div className="h-24 w-full min-w-14 p-2 bg-slate-300 rounded-lg"></div>
+      <div className="h-24 w-full min-w-14 p-2 bg-slate-300 rounded-lg"></div>
     </div>
   );
 }
