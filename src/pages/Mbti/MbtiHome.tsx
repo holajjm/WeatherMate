7f47fde87d19f149c00222d10c589930fafad07c
holajm <holajjm@naver.com>
2025-04-11 13:05:32 +0900

    Refactor: 이미지 LCP 개선 속성 적용 및 버튼 공용 컴포넌트 적용 & Design: 전체 사이즈 축소

--- src/pages/Mbti/MbtiHome.tsx
+++ src/pages/Mbti/MbtiHome.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { useRecoilState } from "recoil";
-import { memberState } from "../../recoil/atom";
+import { memberState } from "@recoil/atom";
+
 import usePageTitle from "@hooks/usePageTitle";
+import Button from "@components/layout/Button";
+import useScrollTop from "@hooks/useScrollTop";
 
 import mbtiStart from "@assets/mbti/mbtiimg/mbtiStart.png";
-import useScrollTop from "@hooks/useScrollTop";
-// import Button from '@components/layout/Button';
 
 function MbtiHome() {
   usePageTitle("MBTI");
@@ -20,42 +21,47 @@ function MbtiHome() {
       style={{ backgroundImage: `url(${mbtiStart})` }}
       className="max-w-[600px] min-w-[320px] m-auto min-h-screen p-2 bg-cover bg-center"
     >
-      <div className="h-full rounded-xl flex flex-col gap-4 items-center justify-center font-SSRONETHandwritten text-nowrap">
-        <div className="p-20 flex flex-col gap-10 text-center items-center justift-center">
-          <p className="text-4xl font-bold font-UhBeeKangJa">
+      <div className="h-full flex flex-col gap-2 items-center justify-center text-nowrap">
+        <div className="w-full pt-10 flex flex-col gap-2 text-center items-center justift-center">
+          <h1 className="text-xl font-bold font-Pretendard">
             날씨 성격 테스트
-          </p>
-          <img className="w-1/2" src="/mainlogin.svg" />
-          <div className="text-2xl font-bold text-slate-600">
-            <p>날씨별 상황에 따라</p>
-            <p>내 행동을 골라보자!</p>
-            <br />
+          </h1>
+          <img
+            className=""
+            src="/mainlogin.webp"
+            width={120}
+            loading="lazy"
+            // fetchPriority="high"
+            decoding="async"
+          />
+          <div className="text-base font-bold text-slate-600 opacity-80 font-SSRONETHandwritten">
+            <p>날씨별 상황에 따라 내 행동을 골라보자!</p>
             <p>나는 어떤 날씨와 어울릴까?</p>
           </div>
-        </div>
-        <div className="w-full flex gap-4">
-          <button
-            // text={'테스트 시작하기'}
-            onClick={() => {
-              if (!user?.name) {
-                confirm("로그인이 필요합니다. 로그인 하시겠습니까?")
-                  ? navigate("/mainlogin", { state: location.pathname })
-                  : null;
-              } else {
-                navigate("question");
-              }
-            }}
-            className="w-full p-4 border-2 border-slate-100 rounded-lg font-UhBeeKangJa font-bold text-lg  transition-all duration-200 text-white bg-sky-400 hover:shadow-[0_4px_8px_1px] hover:shadow-slate-400"
-          >
-            테스트 시작하기
-          </button>
-          <button
-            // text={'메인으로 돌아가기'}
-            onClick={() => navigate("/")}
-            className="w-full p-4 border-2 border-slate-100 rounded-lg font-UhBeeKangJa font-bold text-lg  transition-all duration-200 text-gray-500 text-nowrap bg-slate-200 hover:shadow-[0_4px_8px_1px] hover:shadow-slate-400"
-          >
-            메인으로 돌아가기
-          </button>
+          <div className="w-full flex gap-4">
+            <Button
+              text={"테스트 시작하기"}
+              textColor="white"
+              bgColor="sky"
+              width="full"
+              onClick={() => {
+                if (!user?.name) {
+                  confirm("로그인이 필요합니다. 로그인 하시겠습니까?")
+                    ? navigate("/mainlogin", { state: location.pathname })
+                    : null;
+                } else {
+                  navigate("question");
+                }
+              }}
+            ></Button>
+            <Button
+              text={"메인으로 돌아가기"}
+              textColor="gray"
+              bgColor="gray"
+              width="full"
+              onClick={() => navigate("/")}
+            ></Button>
+          </div>
         </div>
       </div>
     </div>
