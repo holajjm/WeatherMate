a7062615634c1be62f2ac355a3aa2e0428d53c1e
holajm <holajjm@naver.com>
2025-04-14 17:52:18 +0900

    Fix: 메인 페이지 레이아웃 수정에 따른 스켈레톤 UI 수정

--- src/components/skeleton/MainComentSkeleton.tsx
+++ src/components/skeleton/MainComentSkeleton.tsx
@@ -2,10 +2,7 @@ import React from "react";
 
 function MainComentSkeleton() {
   return (
-    <div className="w-full flex flex-col gap-2 p-2 bg-slate-200 rounded-lg">
-      <div className="h-8 bg-slate-300 rounded-lg"></div>
-      <div className="h-32 bg-slate-300 rounded-lg"></div>
-    </div>
+    <div className="w-full h-40 flex flex-col gap-2 p-2 bg-slate-200 rounded-lg"></div>
   );
 }
 
