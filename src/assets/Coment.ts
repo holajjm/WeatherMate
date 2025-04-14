7bea9e48434939733c282097293017fc9cb1c4bb
holajm <holajjm@naver.com>
2025-04-14 17:50:26 +0900

    Refactor: 코멘트 데이터 필드 분리

--- src/assets/Coment.ts
+++ src/assets/Coment.ts
@@ -1,47 +1,50 @@
 export const Coment = [
   {
     temperature: 4,
-    recommendation:
-      "패딩 안 입으면 눈사람 되는거야!☃️ \n( 패딩, 두꺼운코트 추천!)",
+    recommendation: "패딩 안 입으면 눈사람 되는거야!☃️",
+    clothes: "패딩, 두꺼운코트 등",
     CLOTHES_IMG: "/clothes-1.webp",
   },
   {
     temperature: 8,
-    recommendation: "추워요! \n보온성 있는 옷 필수! \n( 울 코트 ,히트택 추천!)",
+    recommendation: "추워요! 보온성 있는 옷 필수!",
+    clothes: "울 코트 ,히트택 등",
     CLOTHES_IMG: "/clothes-2.webp",
   },
   {
     temperature: 12,
-    recommendation:
-      "방한을 위해 얇게 껴 입어요! \n ( 두꺼운 자켓 , 니트 추천!)",
+    recommendation: "방한을 위해 얇게 껴 입어요!",
+    clothes: "두꺼운 자켓 , 니트 등",
     CLOTHES_IMG: "/clothes-3.webp",
   },
   {
     temperature: 16,
-    recommendation:
-      "좋은 날씨에요!!\n 예쁜 옷을 입어봐요!! \n 오늘의 의상: ( 자켓 , 가디건 추천!)",
+    recommendation: "좋은 날씨에요!! 예쁜 옷을 입어봐요!!",
+    clothes: "자켓 , 가디건 등",
     CLOTHES_IMG: "/clothes-4.webp",
   },
   {
     temperature: 19,
-    recommendation: "편하기 입기 딱 좋은 날 \n( 맨투맨 , 후드 추천!)",
+    recommendation: "편하기 입기 딱 좋은 날",
+    clothes: "맨투맨 , 후드 등",
     CLOTHES_IMG: "/clothes-5.webp",
   },
   {
     temperature: 22,
-    recommendation:
-      "도시락 만들어서 소풍 갈래요? :) \n( 블라우스, 면바지 추천!)",
+    recommendation: "도시락 만들어서 소풍 갈래요? :)",
+    clothes: "블라우스, 면바지 등",
     CLOTHES_IMG: "/clothes-6.webp",
   },
   {
     temperature: 27,
-    recommendation: "땀이 삐질 삐질 날 수도 있어요! \n( 반팔 , 얇은셔츠 추천!)",
+    recommendation: "땀이 삐질 삐질 날 수도 있어요!",
+    clothes: "반팔 , 얇은셔츠 등",
     CLOTHES_IMG: "/clothes-7.webp",
   },
   {
     temperature: 30,
-    recommendation:
-      "무척 더워요..\n 시원한 옷차림을 추천해요!! \n( 민소매 , 반바지 추천!)",
+    recommendation: "무척 더워요..시원한 옷차림을 추천해요!!",
+    clothes: "민소매 , 반바지 등",
     CLOTHES_IMG: "/clothes-8.webp",
   },
 ];
