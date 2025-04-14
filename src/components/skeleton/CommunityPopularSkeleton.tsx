4acc17c5fac4c307005065bf0f5eb4a9ce4727e9
holajm <holajjm@naver.com>
2025-04-14 19:39:18 +0900

    Feat: 인기 게시글 목록 및 장소 추천 아이템 목록 스켈레톤 UI 추가

---
+++ src/components/skeleton/CommunityPopularSkeleton.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+function CommunityPopularSkeleton() {
+  return (
+    <div className="flex gap-2">
+      <div className="h-[104px]">
+        <div className="bg-slate-300 rounded-full w-20 h-20"></div>
+        <div className="bg-slate-300 h-6"></div>
+      </div>
+      <div className="h-[104px]">
+        <div className="bg-slate-300 rounded-full w-20 h-20"></div>
+        <div className="bg-slate-300 h-6"></div>
+      </div>
+      <div className="h-[104px]">
+        <div className="bg-slate-300 rounded-full w-20 h-20"></div>
+        <div className="bg-slate-300 h-6"></div>
+      </div>
+      <div className="h-[104px]">
+        <div className="bg-slate-300 rounded-full w-20 h-20"></div>
+        <div className="bg-slate-300 h-6"></div>
+      </div>
+      <div className="h-[104px]">
+        <div className="bg-slate-300 rounded-full w-20 h-20"></div>
+        <div className="bg-slate-300 h-6"></div>
+      </div>
+      <div className="h-[104px]">
+        <div className="bg-slate-300 rounded-full w-20 h-20"></div>
+        <div className="bg-slate-300 h-6"></div>
+      </div>
+      <div className="h-[104px]">
+        <div className="bg-slate-300 rounded-full w-20 h-20"></div>
+        <div className="bg-slate-300 h-6"></div>
+      </div>
+    </div>
+  );
+}
+
+export default CommunityPopularSkeleton;
---
+++ src/components/skeleton/LocationItemSkeleton.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+function LocationItemSkeleton() {
+  return (
+    <div className="grid grid-cols-2 gap-1">
+      <div className="w-full h-72 flex flex-col gap-1 bg-slate-300 rounded-lg p-1">
+        <div className="w-full h-40 bg-slate-200"></div>
+        <div className="h-4 bg-slate-200 rounded-lg"></div>
+        <div className="h-7 bg-slate-200 rounded-lg"></div>
+        <div className="h-5 bg-slate-200 rounded-lg"></div>
+        <div className="h-4 bg-slate-200 rounded-lg"></div>
+      </div>
+      <div className="w-full h-72 flex flex-col gap-1 bg-slate-300 rounded-lg p-1">
+        <div className="w-full h-40 bg-slate-200"></div>
+        <div className="h-4 bg-slate-200 rounded-lg"></div>
+        <div className="h-7 bg-slate-200 rounded-lg"></div>
+        <div className="h-5 bg-slate-200 rounded-lg"></div>
+        <div className="h-4 bg-slate-200 rounded-lg"></div>
+      </div><div className="w-full h-72 flex flex-col gap-1 bg-slate-300 rounded-lg p-1">
+        <div className="w-full h-40 bg-slate-200"></div>
+        <div className="h-4 bg-slate-200 rounded-lg"></div>
+        <div className="h-7 bg-slate-200 rounded-lg"></div>
+        <div className="h-5 bg-slate-200 rounded-lg"></div>
+        <div className="h-4 bg-slate-200 rounded-lg"></div>
+      </div><div className="w-full h-72 flex flex-col gap-1 bg-slate-300 rounded-lg p-1">
+        <div className="w-full h-40 bg-slate-200"></div>
+        <div className="h-4 bg-slate-200 rounded-lg"></div>
+        <div className="h-7 bg-slate-200 rounded-lg"></div>
+        <div className="h-5 bg-slate-200 rounded-lg"></div>
+        <div className="h-4 bg-slate-200 rounded-lg"></div>
+      </div><div className="w-full h-72 flex flex-col gap-1 bg-slate-300 rounded-lg p-1">
+        <div className="w-full h-40 bg-slate-200"></div>
+        <div className="h-4 bg-slate-200 rounded-lg"></div>
+        <div className="h-7 bg-slate-200 rounded-lg"></div>
+        <div className="h-5 bg-slate-200 rounded-lg"></div>
+        <div className="h-4 bg-slate-200 rounded-lg"></div>
+      </div><div className="w-full h-72 flex flex-col gap-1 bg-slate-300 rounded-lg p-1">
+        <div className="w-full h-40 bg-slate-200"></div>
+        <div className="h-4 bg-slate-200 rounded-lg"></div>
+        <div className="h-7 bg-slate-200 rounded-lg"></div>
+        <div className="h-5 bg-slate-200 rounded-lg"></div>
+        <div className="h-4 bg-slate-200 rounded-lg"></div>
+      </div>
+    </div>
+  );
+}
+
+export default LocationItemSkeleton;
