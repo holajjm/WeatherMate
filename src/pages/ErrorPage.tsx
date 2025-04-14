33ba8bd6c01fe0eea6152e9bc4f8cd7093a32cfb
holajm <holajjm@naver.com>
2025-04-14 19:40:00 +0900

    Fix: 이미지 포맷 및 스타일 일부 수정

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -9,11 +9,11 @@ function ErrorPage() {
       <Header />
       <nav className="max-w-[600px] min-w-[320px] m-auto h-screen p-2 bg-slate-50">
         <div className="h-1/2 bg-white rounded-xl drop-shadow-lg flex flex-col gap-8 items-center justify-center text-center">
-          <img className="w-60" src="/error.svg" alt="Error" />
+          <img className="w-60" src="/error.webp" alt="Error" />
           <h1 className="text-2xl font-semibold font-TTLaundryGothicB">
             Page not Found
           </h1>
-          <p className="ttext-base text-slate-600">
+          <p className="text-base text-slate-600">
             서버와의 통신이 원활하지 않아
             <br />
             데이터를 불러올 수 없습니다
