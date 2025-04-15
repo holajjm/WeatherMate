b63a5b6bf7cab5ee4f9ab6bbf7512d8796988146
holajm <holajjm@naver.com>
2025-04-15 13:15:19 +0900

    Fix: 버튼 공용 컴포넌트 동적 클래스 분석 오류 수정

--- src/components/layout/Button.tsx
+++ src/components/layout/Button.tsx
@@ -9,26 +9,27 @@ interface ButtonProps {
 }
 
 const buttonColor = {
-  sky: "sky-400",
-  blue: "blue-600",
-  indigo: "indigo-500",
-  kakao: "[#FEE500]",
-  gray: "slate-300",
-  amber: "amber-200",
-  red: "red-500",
-  white: "white",
-  black: "[#2D2D2D]",
+  sky: "bg-sky-400",
+  blue: "bg-blue-600",
+  indigo: "bg-indigo-500",
+  kakao: "bg-[#FEE500]",
+  gray: "bg-slate-300",
+  amber: "bg-amber-200",
+  red: "bg-red-500",
+  lightRed: "bg-red-300",
+  white: "bg-white",
+  black: "bg-[#2D2D2D]",
 };
 const fontColor = {
-  white: "white",
-  gray: "gray-500",
-  black: "[#2D2D2D]",
-  amber: "amber-500",
+  white: "text-white",
+  gray: "text-gray-500",
+  black: "text-[#2D2D2D]",
+  amber: "text-amber-500",
 };
 function Button({ text, textColor, bgColor, width, onClick }: ButtonProps) {
   return (
     <button
-      className={`bg-${buttonColor[bgColor]} text-${fontColor[textColor]} w-${width} h-10 text-sm p-2 box-border border-[1px] flex items-center justify-center rounded-lg font-Pretendard font-bold transition-all duration-200 text-nowrap border-slate-100 shadow-sm shadow-slate-300`}
+      className={`${buttonColor[bgColor]} ${fontColor[textColor]} w-${width} h-10 text-sm p-2 box-border border-[1px] flex items-center justify-center rounded-lg font-Pretendard font-bold transition-all duration-200 text-nowrap border-slate-100 shadow-sm shadow-slate-300`}
       onClick={onClick}
     >
       {text}
