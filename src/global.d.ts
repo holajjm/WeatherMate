fb16c73f61e01d1819bef8d5ad626f066760e378
holajm <holajjm@naver.com>
2025-04-15 13:01:39 +0900

    Chore: fetchpriority 속성 타입 오류 해결

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,3 +1,9 @@
+import React from "react";
+declare module "react" {
+  interface ImgHTMLAttributes<T> extends HTMLAttributes<T> {
+    fetchpriority?: "high" | "low" | "auto";
+  }
+}
 interface Window {
   KaKao: any;
 }
