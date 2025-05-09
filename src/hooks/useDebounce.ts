8474741f0d98c729065c13e2a56362587ce376bc
holajm <holajjm@naver.com>
2025-05-09 15:47:10 +0900

    Style: 코드 정리

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -1,21 +1,17 @@
-/* eslint-disable */
 import { useState, useEffect } from "react";
 
-// 검색할 때 딜레이 강제시키는 함수
-export const useDebounce = (value:string, delay:number) => {
-
+export const useDebounce = (value: string, delay: number) => {
   const [debouncedValue, setDebouncedValue] = useState(value);
 
   useEffect(() => {
-
     const handler = setTimeout(() => {
-      setDebouncedValue(value)
-    }, delay)
+      setDebouncedValue(value);
+    }, delay);
 
     return () => {
-      clearTimeout(handler)
-    }
-  }, [value, delay])
+      clearTimeout(handler);
+    };
+  }, [value, delay]);
 
-  return debouncedValue
-}
+  return debouncedValue;
+};
