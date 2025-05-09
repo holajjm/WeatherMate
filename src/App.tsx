a5fb93c1d7e19f0ffc594f27cff4469dbcdee600
holajm <holajjm@naver.com>
2025-05-09 15:21:48 +0900

    Feat: 토스트 컨테이너 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import React, { Suspense, useEffect } from 'react';
-import { RouterProvider } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { RecoilRoot } from 'recoil';
-import Loading from '@components/layout/Loading';
-import router from './routes';
+import React, { Suspense, useEffect } from "react";
+import { RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { RecoilRoot } from "recoil";
+import { ToastContainer } from "react-toastify";
+
+import Loading from "@components/layout/Loading";
+import router from "./routes";
 // import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const { Kakao } = window as any;
@@ -12,7 +14,7 @@ const queryClient = new QueryClient();
 function App() {
   useEffect(() => {
     if (!Kakao.isInitialized()) {
-      Kakao.init('44ca17bb4cb74c64db42d774cc78f8af');
+      Kakao.init("44ca17bb4cb74c64db42d774cc78f8af");
     }
   }, []);
 
@@ -21,11 +23,17 @@ function App() {
       <QueryClientProvider client={queryClient}>
         <RecoilRoot>
           <Suspense fallback={<Loading />}>
+            <ToastContainer
+              position="top-right"
+              limit={2}
+              closeButton={true}
+              autoClose={5000}
+            />
             <RouterProvider router={router} />
           </Suspense>
         </RecoilRoot>
       </QueryClientProvider>
-        {/* <ReactQueryDevtools initialIsOpen={false} /> */}
+      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
     </>
   );
 }
