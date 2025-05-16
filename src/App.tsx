6dce958d207b01caf704218fa7baeb44b5dface4
holajm <holajjm@naver.com>
2025-05-16 17:57:11 +0900

    Refactor: QueryClient main.tsx로 위치 이동

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { Suspense, useEffect } from "react";
 import { RouterProvider } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RecoilRoot } from "recoil";
+
 import { ToastContainer } from "react-toastify";
 
 import Loading from "@components/layout/Loading";
@@ -9,7 +9,6 @@ import router from "./routes";
 // import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const { Kakao } = window as any;
-const queryClient = new QueryClient();
 
 function App() {
   useEffect(() => {
@@ -20,19 +19,17 @@ function App() {
 
   return (
     <>
-      <QueryClientProvider client={queryClient}>
-        <RecoilRoot>
-          <Suspense fallback={<Loading />}>
-            <ToastContainer
-              position="top-right"
-              limit={2}
-              closeButton={true}
-              autoClose={5000}
-            />
-            <RouterProvider router={router} />
-          </Suspense>
-        </RecoilRoot>
-      </QueryClientProvider>
+      <RecoilRoot>
+        <Suspense fallback={<Loading />}>
+          <ToastContainer
+            position="top-right"
+            limit={2}
+            closeButton={true}
+            autoClose={5000}
+          />
+          <RouterProvider router={router} />
+        </Suspense>
+      </RecoilRoot>
       {/* <ReactQueryDevtools initialIsOpen={false} /> */}
     </>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 import App from "./App.js";
-import "./index.css";
 import EnvCheck from "@hooks/EnvCheck.js";
+import "./index.css";
 
 EnvCheck();
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <App />,
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>,
 );
