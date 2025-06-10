b8bba5a86999e43c0b1b299f94bb55039985da4b
holajm <holajjm@naver.com>
2025-06-10 15:09:33 +0900

    Feat: 디바운스를 통한 검색 기능 활성화 및 UI 업데이트 개선, Refactor: 타입 선언 인덱스 시그니쳐로 전환

--- src/pages/location/Location.tsx
+++ src/pages/location/Location.tsx
@@ -13,41 +13,25 @@ import LocationKeywords from "@pages/location/LocationKeyword";
 import LocationItem from "@pages/location/LocationItem";
 import LocationItemSkeleton from "@components/skeleton/LocationItemSkeleton";
 
-interface InitialData {
-  addr1: string;
-  addr2: string;
-  areacode: string;
-  booktour: string;
-  cat1: string;
-  cat2: string;
-  cat3: string;
-  contentid: string;
-  contenttypeid: string;
-  cpyrhtDivCd: string;
-  createdtime: string;
-  dist: string;
-  firstimage: string;
-  firstimage2: string;
-  mapx: string;
-  mapy: string;
-  mlevel: string;
-  modifiedtime: string;
-  sigungucode: string;
-  tel: string;
-  title: string;
-}
-interface Data {
+type InitialData = {
+  [key: string]: string;
+};
+interface PageData {
   items: { item: InitialData[] };
   numOfRows: number;
   pageNo: number;
   totalCount: number;
 }
-
-interface QueryData {
-  pageParams: Array<number | undefined>;
-  pages: Data[];
-}
-
+const options = [
+  { id: "12", label: "전체", img_src: "all.webp" },
+  { id: "14", label: "문화", img_src: "communityplace.webp" },
+  { id: "15", label: "행사", img_src: "festival.webp" },
+  { id: "25", label: "여행지", img_src: "travel.webp" },
+  { id: "28", label: "레포츠", img_src: "reports.webp" },
+  { id: "32", label: "숙박", img_src: "hotel.webp" },
+  { id: "38", label: "쇼핑", img_src: "shopping.webp" },
+  { id: "39", label: "음식점", img_src: "food.webp" },
+];
 function Location() {
   const locationAPIKEY = import.meta.env.VITE_REACT_APP_LOCATION_API_KEY;
   const navigate = useNavigate();
@@ -55,15 +39,20 @@ function Location() {
   const [contentID, setContentID] = useState<string>("12");
   const radius = "100000";
 
-  //사용자 위치 정보에 따른 기본 데이터 호출 로직
+  //초기 데이터 호출 로직
   const getLocationData = async (page: number) => {
     const response = await axios.get(
       `http://apis.data.go.kr/B551011/KorService1/locationBasedList1?serviceKey=${locationAPIKEY}&pageNo=${page}&numOfRows=8&mapX=${longitude}&mapY=${latitude}&radius=${radius}&MobileApp=AppTest&MobileOS=ETC&contentTypeId=${contentID}&_type=json `,
     );
     return response?.data?.response?.body;
   };
-  //무한 스크롤에 따른 데이터 호출 구현
-  const { data, isLoading, fetchNextPage } = useInfiniteQuery({
+
+  //무한 스크롤 로직
+  const {
+    data: InitialData,
+    isLoading,
+    fetchNextPage,
+  } = useInfiniteQuery({
     queryKey: [
       "InfiniteData",
       locationAPIKEY,
@@ -74,79 +63,74 @@ function Location() {
     ],
     queryFn: ({ pageParam = 0 }: { pageParam?: number }) =>
       getLocationData(pageParam),
-    getNextPageParam: (lastPage: Data) => {
+    getNextPageParam: (lastPage: PageData) => {
       // console.log(lastPage);
       return lastPage?.pageNo + 1;
     },
     initialPageParam: 0,
   });
+  // console.log(InitialData);
+
   const mergedItems: InitialData[] | undefined =
-    data && data?.pages?.length === 1
-      ? data?.pages[0]?.items?.item
-      : data?.pages?.flatMap(page => page?.items?.item);
+    InitialData && InitialData?.pages?.length === 1
+      ? InitialData?.pages[0]?.items?.item
+      : InitialData?.pages?.flatMap(page => page?.items?.item);
   // console.log(mergedItems);
-  // console.log(data);
 
   const { ref, inView } = useInView();
-  // console.log(inView);
   useEffect(() => {
     if (inView) {
       fetchNextPage();
     }
   }, [inView]);
 
-  //검색 로직 구현
+  //검색 로직
   const [keyword, setKeyword] = useState("");
-  const [searchKeyword, setSearchKeyword] = useState<string>("");
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setKeyword(e.target.value);
   };
-  const debounceSearchKeyword = useDebounce(keyword, 0);
-  useEffect(() => {
-    setSearchKeyword(debounceSearchKeyword);
-  }, [debounceSearchKeyword]);
 
-  const fetchLocationSearchData = async () => {
+  const [searchKeyword, setSearchKeyword] = useState<string>("");
+  const debounceSearch = useDebounce(keyword, 300);
+  const [result, setResult] = useState([]);
+  // console.log(result);
+
+  useEffect(() => {
+    getFilteredData(debounceSearch);
+  }, [debounceSearch]);
+
+  const getFilteredData = async (searchKeyword: string) => {
     try {
       const response = await axios.get(
         `https://apis.data.go.kr/B551011/KorService1/searchKeyword1?MobileOS=ETC&MobileApp=testweb&serviceKey=${locationAPIKEY}&keyword=${searchKeyword}&_type=json&contentTypeId=${contentID}`,
       );
+      setResult(response?.data?.response?.body?.items?.item);
+      return response;
     } catch (error) {
-      console.error(
-        "데이터를 원활하게 가져오는데 오류가 발생하였습니다.",
-        error,
-      );
+      console.error(error);
     }
   };
-
   const handleClick = () => {
     if (searchKeyword) {
-      fetchLocationSearchData();
-      setKeyword("");
-    }
-  };
-  const handleSubmit = () => {
-    if (searchKeyword) {
-      fetchLocationSearchData();
-      setKeyword("");
-    }
-  };
-  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === "Enter") {
-      handleSubmit();
+      getFilteredData(searchKeyword);
+      setSearchKeyword("");
     }
   };
 
-  const options = [
-    { id: "12", label: "전체", img_src: "all.webp" },
-    { id: "14", label: "문화", img_src: "communityplace.webp" },
-    { id: "15", label: "행사", img_src: "festival.webp" },
-    { id: "25", label: "여행지", img_src: "travel.webp" },
-    { id: "28", label: "레포츠", img_src: "reports.webp" },
-    { id: "32", label: "숙박", img_src: "hotel.webp" },
-    { id: "38", label: "쇼핑", img_src: "shopping.webp" },
-    { id: "39", label: "음식점", img_src: "food.webp" },
-  ];
+  const searchItemList = result?.map((e: InitialData) => (
+    <motion.section
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{
+        ease: "easeInOut",
+        duration: 1,
+      }}
+      key={e?.modifiedtime}
+      className="flex flex-col justify-between p-2 rounded-lg shadow-lg border-2 border-slate-200 hover:border-blue-400 duration-200"
+    >
+      <LocationItem item={e} />
+    </motion.section>
+  ));
 
   const locationItemList = mergedItems?.map((e: InitialData, i: number) => (
     <motion.section
@@ -171,7 +155,6 @@ function Location() {
           type="text"
           value={keyword}
           onChange={handleChange}
-          onKeyDown={handleKeyDown}
         />
         <Button
           text="검색하기"
@@ -209,7 +192,7 @@ function Location() {
       ) : (
         <>
           <main className="grid grid-cols-2 gap-1 relative">
-            {locationItemList}
+            {searchItemList?.length ? searchItemList : locationItemList}
           </main>
           <p ref={ref} className="w-full text-center">
             더 불러오기
