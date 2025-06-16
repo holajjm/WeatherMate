0ef1efaafaf74c00aca29bab6770e3a27791f2a6
holajm <holajjm@naver.com>
2025-06-16 13:45:22 +0900

    Chore: 날씨 데이터 전역 타입 변경

--- src/type.ts
+++ src/type.ts
@@ -88,6 +88,42 @@ export interface WeatherImage {
   Dust: string;
   overcastClouds: string;
 }
+export interface AllCityData {
+  clouds: { all: number };
+  coord: { lat: number; lon: number };
+  dt: number;
+  id: number;
+  main: {
+    feels_like: number;
+    grnd_level: number;
+    humidity: number;
+    pressure: number;
+    sea_level: number;
+    temp: number;
+    temp_max: number;
+    temp_min: number;
+  };
+  name: string;
+  sys: {
+    country: string;
+    sunrise: number;
+    sunset: number;
+    timezone: number;
+  };
+  visibility: number;
+  weather: [
+    {
+      id: number;
+      main: string;
+      description: string;
+      icon: string;
+    },
+  ];
+  wind: {
+    deg: number;
+    speed: number;
+  };
+}
 
 //Mbti Data Type
 export interface MBTIResult {
