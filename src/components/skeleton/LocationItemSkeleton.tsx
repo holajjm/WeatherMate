211c4613ddf991ae68e93fb5d0e76d5bead97e14
holajm <holajjm@naver.com>
2025-06-16 14:08:23 +0900

    Design: Location 스켈레톤 UI shimmer 애니메이션 적용

--- src/components/skeleton/LocationItemSkeleton.tsx
+++ src/components/skeleton/LocationItemSkeleton.tsx
@@ -3,43 +3,293 @@ import React from "react";
 function LocationItemSkeleton() {
   return (
     <div className="grid grid-cols-2 gap-1">
-      <div className="w-full h-72 flex flex-col gap-1 bg-slate-300 rounded-lg p-1">
-        <div className="w-full h-40 bg-slate-200"></div>
-        <div className="h-4 bg-slate-200 rounded-lg"></div>
-        <div className="h-7 bg-slate-200 rounded-lg"></div>
-        <div className="h-5 bg-slate-200 rounded-lg"></div>
-        <div className="h-4 bg-slate-200 rounded-lg"></div>
+      <div className="w-full h-72 flex flex-col gap-1 bg-slate-300 rounded-md p-1">
+        <div className="relative overflow-hidden w-full h-2/3 bg-slate-200 rounded-md">
+          <div
+            className="absolute top-0 left-0 h-full w-[40%]
+          animate-shimmer
+          bg-gradient-to-r from-transparent via-white/70 to-transparent"
+          ></div>
+        </div>
+        <div className="w-full h-1/2 flex flex-col gap-1">
+          <div className="w-full h-full flex items-center justify-between">
+            <div className="relative overflow-hidden w-1/4 h-full bg-slate-200 rounded-md">
+              <div
+                className="absolute top-0 left-0 h-full w-[40%]
+          animate-shimmer
+          bg-gradient-to-r from-transparent via-white/70 to-transparent"
+              ></div>
+            </div>
+            <div className="relative overflow-hidden w-1/4 h-full bg-slate-200 rounded-md">
+              <div
+                className="absolute top-0 left-0 h-full w-[40%]
+          animate-shimmer
+          bg-gradient-to-r from-transparent via-white/70 to-transparent"
+              ></div>
+            </div>
+          </div>
+          <div className="relative overflow-hidden w-1/2 h-full bg-slate-200 rounded-md">
+            <div
+              className="absolute top-0 left-0 h-full w-[40%]
+          animate-shimmer
+          bg-gradient-to-r from-transparent via-white/70 to-transparent"
+            ></div>
+          </div>
+          <div className="relative overflow-hidden w-3/4 h-full bg-slate-200 rounded-md">
+            <div
+              className="absolute top-0 left-0 h-full w-[40%]
+          animate-shimmer
+          bg-gradient-to-r from-transparent via-white/70 to-transparent"
+            ></div>
+          </div>
+          <div className="relative overflow-hidden h-full bg-slate-200 rounded-md">
+            <div
+              className="absolute top-0 left-0 h-full w-[40%]
+          animate-shimmer
+          bg-gradient-to-r from-transparent via-white/70 to-transparent"
+            ></div>
+          </div>
+        </div>
       </div>
-      <div className="w-full h-72 flex flex-col gap-1 bg-slate-300 rounded-lg p-1">
-        <div className="w-full h-40 bg-slate-200"></div>
-        <div className="h-4 bg-slate-200 rounded-lg"></div>
-        <div className="h-7 bg-slate-200 rounded-lg"></div>
-        <div className="h-5 bg-slate-200 rounded-lg"></div>
-        <div className="h-4 bg-slate-200 rounded-lg"></div>
-      </div><div className="w-full h-72 flex flex-col gap-1 bg-slate-300 rounded-lg p-1">
-        <div className="w-full h-40 bg-slate-200"></div>
-        <div className="h-4 bg-slate-200 rounded-lg"></div>
-        <div className="h-7 bg-slate-200 rounded-lg"></div>
-        <div className="h-5 bg-slate-200 rounded-lg"></div>
-        <div className="h-4 bg-slate-200 rounded-lg"></div>
-      </div><div className="w-full h-72 flex flex-col gap-1 bg-slate-300 rounded-lg p-1">
-        <div className="w-full h-40 bg-slate-200"></div>
-        <div className="h-4 bg-slate-200 rounded-lg"></div>
-        <div className="h-7 bg-slate-200 rounded-lg"></div>
-        <div className="h-5 bg-slate-200 rounded-lg"></div>
-        <div className="h-4 bg-slate-200 rounded-lg"></div>
-      </div><div className="w-full h-72 flex flex-col gap-1 bg-slate-300 rounded-lg p-1">
-        <div className="w-full h-40 bg-slate-200"></div>
-        <div className="h-4 bg-slate-200 rounded-lg"></div>
-        <div className="h-7 bg-slate-200 rounded-lg"></div>
-        <div className="h-5 bg-slate-200 rounded-lg"></div>
-        <div className="h-4 bg-slate-200 rounded-lg"></div>
-      </div><div className="w-full h-72 flex flex-col gap-1 bg-slate-300 rounded-lg p-1">
-        <div className="w-full h-40 bg-slate-200"></div>
-        <div className="h-4 bg-slate-200 rounded-lg"></div>
-        <div className="h-7 bg-slate-200 rounded-lg"></div>
-        <div className="h-5 bg-slate-200 rounded-lg"></div>
-        <div className="h-4 bg-slate-200 rounded-lg"></div>
+      <div className="w-full h-72 flex flex-col gap-1 bg-slate-300 rounded-md p-1">
+        <div className="relative overflow-hidden w-full h-2/3 bg-slate-200 rounded-md">
+          <div
+            className="absolute top-0 left-0 h-full w-[40%]
+          animate-shimmer
+          bg-gradient-to-r from-transparent via-white/70 to-transparent"
+          ></div>
+        </div>
+        <div className="w-full h-1/2 flex flex-col gap-1">
+          <div className="w-full h-full flex items-center justify-between">
+            <div className="relative overflow-hidden w-1/4 h-full bg-slate-200 rounded-md">
+              <div
+                className="absolute top-0 left-0 h-full w-[40%]
+          animate-shimmer
+          bg-gradient-to-r from-transparent via-white/70 to-transparent"
+              ></div>
+            </div>
+            <div className="relative overflow-hidden w-1/4 h-full bg-slate-200 rounded-md">
+              <div
+                className="absolute top-0 left-0 h-full w-[40%]
+          animate-shimmer
+          bg-gradient-to-r from-transparent via-white/70 to-transparent"
+              ></div>
+            </div>
+          </div>
+          <div className="relative overflow-hidden w-1/2 h-full bg-slate-200 rounded-md">
+            <div
+              className="absolute top-0 left-0 h-full w-[40%]
+          animate-shimmer
+          bg-gradient-to-r from-transparent via-white/70 to-transparent"
+            ></div>
+          </div>
+          <div className="relative overflow-hidden w-3/4 h-full bg-slate-200 rounded-md">
+            <div
+              className="absolute top-0 left-0 h-full w-[40%]
+          animate-shimmer
+          bg-gradient-to-r from-transparent via-white/70 to-transparent"
+            ></div>
+          </div>
+          <div className="relative overflow-hidden h-full bg-slate-200 rounded-md">
+            <div
+              className="absolute top-0 left-0 h-full w-[40%]
+          animate-shimmer
+          bg-gradient-to-r from-transparent via-white/70 to-transparent"
+            ></div>
+          </div>
+        </div>
+      </div>
+      <div className="w-full h-72 flex flex-col gap-1 bg-slate-300 rounded-md p-1">
+        <div className="relative overflow-hidden w-full h-2/3 bg-slate-200 rounded-md">
+          <div
+            className="absolute top-0 left-0 h-full w-[40%]
+          animate-shimmer
+          bg-gradient-to-r from-transparent via-white/70 to-transparent"
+          ></div>
+        </div>
+        <div className="w-full h-1/2 flex flex-col gap-1">
+          <div className="w-full h-full flex items-center justify-between">
+            <div className="relative overflow-hidden w-1/4 h-full bg-slate-200 rounded-md">
+              <div
+                className="absolute top-0 left-0 h-full w-[40%]
+          animate-shimmer
+          bg-gradient-to-r from-transparent via-white/70 to-transparent"
+              ></div>
+            </div>
+            <div className="relative overflow-hidden w-1/4 h-full bg-slate-200 rounded-md">
+              <div
+                className="absolute top-0 left-0 h-full w-[40%]
+          animate-shimmer
+          bg-gradient-to-r from-transparent via-white/70 to-transparent"
+              ></div>
+            </div>
+          </div>
+          <div className="relative overflow-hidden w-1/2 h-full bg-slate-200 rounded-md">
+            <div
+              className="absolute top-0 left-0 h-full w-[40%]
+          animate-shimmer
+          bg-gradient-to-r from-transparent via-white/70 to-transparent"
+            ></div>
+          </div>
+          <div className="relative overflow-hidden w-3/4 h-full bg-slate-200 rounded-md">
+            <div
+              className="absolute top-0 left-0 h-full w-[40%]
+          animate-shimmer
+          bg-gradient-to-r from-transparent via-white/70 to-transparent"
+            ></div>
+          </div>
+          <div className="relative overflow-hidden h-full bg-slate-200 rounded-md">
+            <div
+              className="absolute top-0 left-0 h-full w-[40%]
+          animate-shimmer
+          bg-gradient-to-r from-transparent via-white/70 to-transparent"
+            ></div>
+          </div>
+        </div>
+      </div>
+      <div className="w-full h-72 flex flex-col gap-1 bg-slate-300 rounded-md p-1">
+        <div className="relative overflow-hidden w-full h-2/3 bg-slate-200 rounded-md">
+          <div
+            className="absolute top-0 left-0 h-full w-[40%]
+          animate-shimmer
+          bg-gradient-to-r from-transparent via-white/70 to-transparent"
+          ></div>
+        </div>
+        <div className="w-full h-1/2 flex flex-col gap-1">
+          <div className="w-full h-full flex items-center justify-between">
+            <div className="relative overflow-hidden w-1/4 h-full bg-slate-200 rounded-md">
+              <div
+                className="absolute top-0 left-0 h-full w-[40%]
+          animate-shimmer
+          bg-gradient-to-r from-transparent via-white/70 to-transparent"
+              ></div>
+            </div>
+            <div className="relative overflow-hidden w-1/4 h-full bg-slate-200 rounded-md">
+              <div
+                className="absolute top-0 left-0 h-full w-[40%]
+          animate-shimmer
+          bg-gradient-to-r from-transparent via-white/70 to-transparent"
+              ></div>
+            </div>
+          </div>
+          <div className="relative overflow-hidden w-1/2 h-full bg-slate-200 rounded-md">
+            <div
+              className="absolute top-0 left-0 h-full w-[40%]
+          animate-shimmer
+          bg-gradient-to-r from-transparent via-white/70 to-transparent"
+            ></div>
+          </div>
+          <div className="relative overflow-hidden w-3/4 h-full bg-slate-200 rounded-md">
+            <div
+              className="absolute top-0 left-0 h-full w-[40%]
+          animate-shimmer
+          bg-gradient-to-r from-transparent via-white/70 to-transparent"
+            ></div>
+          </div>
+          <div className="relative overflow-hidden h-full bg-slate-200 rounded-md">
+            <div
+              className="absolute top-0 left-0 h-full w-[40%]
+          animate-shimmer
+          bg-gradient-to-r from-transparent via-white/70 to-transparent"
+            ></div>
+          </div>
+        </div>
+      </div>
+      <div className="w-full h-72 flex flex-col gap-1 bg-slate-300 rounded-md p-1">
+        <div className="relative overflow-hidden w-full h-2/3 bg-slate-200 rounded-md">
+          <div
+            className="absolute top-0 left-0 h-full w-[40%]
+          animate-shimmer
+          bg-gradient-to-r from-transparent via-white/70 to-transparent"
+          ></div>
+        </div>
+        <div className="w-full h-1/2 flex flex-col gap-1">
+          <div className="w-full h-full flex items-center justify-between">
+            <div className="relative overflow-hidden w-1/4 h-full bg-slate-200 rounded-md">
+              <div
+                className="absolute top-0 left-0 h-full w-[40%]
+          animate-shimmer
+          bg-gradient-to-r from-transparent via-white/70 to-transparent"
+              ></div>
+            </div>
+            <div className="relative overflow-hidden w-1/4 h-full bg-slate-200 rounded-md">
+              <div
+                className="absolute top-0 left-0 h-full w-[40%]
+          animate-shimmer
+          bg-gradient-to-r from-transparent via-white/70 to-transparent"
+              ></div>
+            </div>
+          </div>
+          <div className="relative overflow-hidden w-1/2 h-full bg-slate-200 rounded-md">
+            <div
+              className="absolute top-0 left-0 h-full w-[40%]
+          animate-shimmer
+          bg-gradient-to-r from-transparent via-white/70 to-transparent"
+            ></div>
+          </div>
+          <div className="relative overflow-hidden w-3/4 h-full bg-slate-200 rounded-md">
+            <div
+              className="absolute top-0 left-0 h-full w-[40%]
+          animate-shimmer
+          bg-gradient-to-r from-transparent via-white/70 to-transparent"
+            ></div>
+          </div>
+          <div className="relative overflow-hidden h-full bg-slate-200 rounded-md">
+            <div
+              className="absolute top-0 left-0 h-full w-[40%]
+          animate-shimmer
+          bg-gradient-to-r from-transparent via-white/70 to-transparent"
+            ></div>
+          </div>
+        </div>
+      </div>
+      <div className="w-full h-72 flex flex-col gap-1 bg-slate-300 rounded-md p-1">
+        <div className="relative overflow-hidden w-full h-2/3 bg-slate-200 rounded-md">
+          <div
+            className="absolute top-0 left-0 h-full w-[40%]
+          animate-shimmer
+          bg-gradient-to-r from-transparent via-white/70 to-transparent"
+          ></div>
+        </div>
+        <div className="w-full h-1/2 flex flex-col gap-1">
+          <div className="w-full h-full flex items-center justify-between">
+            <div className="relative overflow-hidden w-1/4 h-full bg-slate-200 rounded-md">
+              <div
+                className="absolute top-0 left-0 h-full w-[40%]
+          animate-shimmer
+          bg-gradient-to-r from-transparent via-white/70 to-transparent"
+              ></div>
+            </div>
+            <div className="relative overflow-hidden w-1/4 h-full bg-slate-200 rounded-md">
+              <div
+                className="absolute top-0 left-0 h-full w-[40%]
+          animate-shimmer
+          bg-gradient-to-r from-transparent via-white/70 to-transparent"
+              ></div>
+            </div>
+          </div>
+          <div className="relative overflow-hidden w-1/2 h-full bg-slate-200 rounded-md">
+            <div
+              className="absolute top-0 left-0 h-full w-[40%]
+          animate-shimmer
+          bg-gradient-to-r from-transparent via-white/70 to-transparent"
+            ></div>
+          </div>
+          <div className="relative overflow-hidden w-3/4 h-full bg-slate-200 rounded-md">
+            <div
+              className="absolute top-0 left-0 h-full w-[40%]
+          animate-shimmer
+          bg-gradient-to-r from-transparent via-white/70 to-transparent"
+            ></div>
+          </div>
+          <div className="relative overflow-hidden h-full bg-slate-200 rounded-md">
+            <div
+              className="absolute top-0 left-0 h-full w-[40%]
+          animate-shimmer
+          bg-gradient-to-r from-transparent via-white/70 to-transparent"
+            ></div>
+          </div>
+        </div>
       </div>
     </div>
   );
