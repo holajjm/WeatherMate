8a08a806b8986420986740a37c029fed4292a248
holajm <holajjm@naver.com>
2025-06-16 13:44:57 +0900

    Feat: 전국 날씨 UI 스켈레톤 UI 제작

---
+++ src/components/skeleton/MainAllWeatherSkeleton.tsx
@@ -0,0 +1,73 @@
+import React from "react";
+
+function MainAllWeatherSkeleton() {
+  return (
+    <>
+      <div className="relative overflow-hidden bg-slate-200 w-full h-44 rounded-lg">
+        <div
+          className="absolute top-0 left-0 h-full w-[40%]
+          animate-shimmer
+          bg-gradient-to-r from-transparent via-white/70 to-transparent"
+        ></div>
+      </div>
+      <div className="relative overflow-hidden bg-slate-200 w-full h-44 rounded-lg">
+        <div
+          className="absolute top-0 left-0 h-full w-[40%]
+          animate-shimmer
+          bg-gradient-to-r from-transparent via-white/70 to-transparent"
+        ></div>
+      </div>
+      <div className="relative overflow-hidden bg-slate-200 w-full h-44 rounded-lg">
+        <div
+          className="absolute top-0 left-0 h-full w-[40%]
+          animate-shimmer
+          bg-gradient-to-r from-transparent via-white/70 to-transparent"
+        ></div>
+      </div>
+      <div className="relative overflow-hidden bg-slate-200 w-full h-44 rounded-lg">
+        <div
+          className="absolute top-0 left-0 h-full w-[40%]
+          animate-shimmer
+          bg-gradient-to-r from-transparent via-white/70 to-transparent"
+        ></div>
+      </div>
+      <div className="relative overflow-hidden bg-slate-200 w-full h-44 rounded-lg">
+        <div
+          className="absolute top-0 left-0 h-full w-[40%]
+          animate-shimmer
+          bg-gradient-to-r from-transparent via-white/70 to-transparent"
+        ></div>
+      </div>
+      <div className="relative overflow-hidden bg-slate-200 w-full h-44 rounded-lg">
+        <div
+          className="absolute top-0 left-0 h-full w-[40%]
+          animate-shimmer
+          bg-gradient-to-r from-transparent via-white/70 to-transparent"
+        ></div>
+      </div>
+      <div className="relative overflow-hidden bg-slate-200 w-full h-44 rounded-lg">
+        <div
+          className="absolute top-0 left-0 h-full w-[40%]
+          animate-shimmer
+          bg-gradient-to-r from-transparent via-white/70 to-transparent"
+        ></div>
+      </div>
+      <div className="relative overflow-hidden bg-slate-200 w-full h-44 rounded-lg">
+        <div
+          className="absolute top-0 left-0 h-full w-[40%]
+          animate-shimmer
+          bg-gradient-to-r from-transparent via-white/70 to-transparent"
+        ></div>
+      </div>
+      <div className="relative overflow-hidden bg-slate-200 w-full h-44 rounded-lg">
+        <div
+          className="absolute top-0 left-0 h-full w-[40%]
+          animate-shimmer
+          bg-gradient-to-r from-transparent via-white/70 to-transparent"
+        ></div>
+      </div>
+    </>
+  );
+}
+
+export default MainAllWeatherSkeleton;
