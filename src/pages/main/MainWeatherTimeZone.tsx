bda0fd73368ecdd6ebb56da59a367c2d3e2fcdd8
holajm <holajjm@naver.com>
2025-06-16 13:04:31 +0900

    Design: 제목과 내용 간격 수정

--- src/pages/main/MainWeatherTimeZone.tsx
+++ src/pages/main/MainWeatherTimeZone.tsx
@@ -67,7 +67,7 @@ function MainWeatherTimeZone() {
     return `${ampm} ${hour}`;
   };
   return (
-    <section className="w-full p-2 bg-slate-50 flex flex-col gap-2">
+    <section className="w-full p-2 bg-slate-50 flex flex-col gap-1">
       <h2 className="text-slate-400 text-base">
         시간대별 날씨정보(3시간 기준)
       </h2>
