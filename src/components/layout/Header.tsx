be06f94a915baeae8740a13f81f3764a4a9bdd54
holajm <holajjm@naver.com>
2025-06-18 16:53:52 +0900

    Refactor: nav 바 레이아웃 추가, 메인 로고 이미지 초기 사이즈 설정

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -6,6 +6,7 @@ import { AnimatePresence, motion } from "framer-motion";
 import HeaderCategory from "@components/layout/HeaderCategory";
 import { CgMenuLeftAlt } from "react-icons/cg";
 import { IoIosArrowDropleft } from "react-icons/io";
+import NavigationBar from "./NavigationBar";
 
 function Header() {
   const location = useLocation();
@@ -28,18 +29,19 @@ function Header() {
   }, [location.pathname]);
 
   return (
-    <header className="max-w-[600px] min-w-[180px] h-14 m-auto sticky top-0 z-50 flex items-center justify-center bg-blue-200">
+    <header className="max-w-[600px] min-w-[180px] h-24 m-auto sticky top-0 z-50 flex flex-col items-center justify-center bg-blue-200">
       {!open ? (
         <CgMenuLeftAlt
-          className="text-2xl absolute left-4"
+          className="text-2xl absolute top-5 left-4"
           onClick={handleCategoryOpen}
         />
       ) : (
         <IoIosArrowDropleft
-          className="text-2xl absolute left-4"
+          className="text-2xl absolute top-5 left-4"
           onClick={handleCategoryOpen}
         />
       )}
+
       <AnimatePresence>
         {open && (
           <motion.div
@@ -50,23 +52,27 @@ function Header() {
               ease: "easeInOut",
               duration: 0.8,
             }}
-            className={`w-1/4 sm:w-1/2 h-full absolute top-14 left-0 bg-black`}
+            className={`w-1/4 sm:w-1/2 h-full absolute top-24 left-0 bg-black`}
           >
             <HeaderCategory />
           </motion.div>
         )}
       </AnimatePresence>
-      <Link to="/" className="flex justify-center">
+
+      <Link to="/" className="h-full flex items-center justify-center">
         <img
           src="/weatherMateLogo.webp"
           alt="weathermate logo"
-          className="w-10"
+          width={40}
+          height={32}
+          className="w-10 h-8"
           onClick={handleRefresh}
         />
-        <p className="text-2xl text-orange-400 font-PretendardBold">
+        <p className="text-2xl text-blue-900 font-PretendardBold">
           WeatherMate
         </p>
       </Link>
+      <NavigationBar />
     </header>
   );
 }
