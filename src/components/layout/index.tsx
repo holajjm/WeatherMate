2efe8f68fa62ea215e0c2e9fd4ee7b0748fc3ed3
holajm <holajjm@naver.com>
2025-06-18 16:53:17 +0900

    Refactor: nav 바 위치 변경

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -3,13 +3,11 @@ import { Outlet } from "react-router-dom";
 
 import Header from "./Header";
 import Footer from "@components/layout/Footer";
-import NavigationBar from "./NavigationBar";
 
 const Layout = () => {
   return (
     <div>
       <Header />
-      <NavigationBar />
       <Outlet />
       <Footer />
     </div>
