8869db52fe37a79976a9f0d2af04009b5f89dc5e
holajm <holajjm@naver.com>
2025-07-16 17:38:33 +0900

    Design: 글자 크키 및 폰트 설정 변경

--- src/components/layout/NavigationBar.tsx
+++ src/components/layout/NavigationBar.tsx
@@ -4,46 +4,46 @@ import { Link, useLocation } from "react-router-dom";
 function NavigationBar() {
   const location = useLocation();
   return (
-    <nav className="max-w-[600px] min-w-[320px] w-full m-auto border-b-[1px] border-slate-300 flex text-white text-lg h-18 items-center gap-4 font-bold bg-white sticky top-0 z-20 shadow-inner font-SSRONETHandwritten">
+    <nav className="max-w-[600px] min-w-[320px] w-full m-auto border-b-[1px] border-slate-300 flex text-white text-lg h-18 items-center gap-4 font-bold bg-white sticky top-0 z-20 shadow-inner">
       <Link
         to="/"
         className={`w-full flex flex-col gap-1 items-center p-1 ${
           location.pathname === "/main" || location.pathname === "/"
-            ? "text-indigo-500"
-            : "text-slate-600 hover:text-indigo-500"
+            ? "text-blue_middle"
+            : "text-slate-600 hover:text-blue_middle"
         }`}
       >
-        <p className="text-nowrap text-base">홈</p>
+        <p className="text-nowrap text-sm">홈</p>
       </Link>
       <Link
         to="/community"
         className={`w-full flex flex-col gap-1 items-center p-1 ${
           location.pathname === "/community"
-            ? "text-indigo-500"
-            : "text-slate-600 hover:text-indigo-500"
+            ? "text-blue_middle"
+            : "text-slate-600 hover:text-blue_middle"
         }`}
       >
-        <p className="text-nowrap text-base">커뮤니티</p>
+        <p className="text-nowrap text-sm">커뮤니티</p>
       </Link>
       <Link
         to="/location"
         className={`w-full flex flex-col gap-1 items-center p-1 ${
           location.pathname === "/location"
-            ? "text-indigo-500"
-            : "text-slate-600 hover:text-indigo-500"
+            ? "text-blue_middle"
+            : "text-slate-600 hover:text-blue_middle"
         }`}
       >
-        <p className="text-nowrap text-base">장소추천</p>
+        <p className="text-nowrap text-sm">장소추천</p>
       </Link>
       <Link
         to="/user/mypage"
         className={`w-full flex flex-col gap-1 items-center p-1 ${
           location.pathname === "/user/mypage"
-            ? "text-indigo-500"
-            : "text-slate-600 hover:text-indigo-500"
+            ? "text-blue_middle"
+            : "text-slate-600 hover:text-blue_middle"
         }`}
       >
-        <p className="text-nowrap text-base">마이페이지</p>
+        <p className="text-nowrap text-sm">마이페이지</p>
       </Link>
     </nav>
   );
