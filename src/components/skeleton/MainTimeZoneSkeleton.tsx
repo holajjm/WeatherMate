637165d091b057d06c1eceee63f95f49a0af3dfc
holajm <holajjm@naver.com>
2025-07-18 18:25:57 +0900

    Fix: 스켈레톤 UI 개수 수정

--- src/components/skeleton/MainTimeZoneSkeleton.tsx
+++ src/components/skeleton/MainTimeZoneSkeleton.tsx
@@ -52,6 +52,13 @@ function MainTimeZoneSkeleton() {
           bg-gradient-to-r from-transparent via-white/70 to-transparent"
         ></div>
       </div>
+      <div className="h-24 relative overflow-hidden bg-slate-200 rounded-md w-full min-w-14 p-2">
+        <div
+          className="absolute top-0 left-0 h-full w-[40%]
+          animate-shimmer
+          bg-gradient-to-r from-transparent via-white/70 to-transparent"
+        ></div>
+      </div>
     </div>
   );
 }
