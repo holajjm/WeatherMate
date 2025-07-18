c156fc33a67cda50354fa96bbc33693954fed6a4
holajm <holajjm@naver.com>
2025-07-18 18:46:21 +0900

    Refactor: 컴포넌트 설계 및 패턴 재조정

--- src/pages/main/MainWeatherTimeZone.tsx
+++ src/pages/main/MainWeatherTimeZone.tsx
@@ -1,59 +1,45 @@
 import React from "react";
-import { useQuery } from "@tanstack/react-query";
-
-import { useCoordsStore } from "@store/store";
-import { UnixTime } from "@hooks/UnixTime";
 
 import MainTimeZoneSkeleton from "@components/skeleton/MainTimeZoneSkeleton";
+import { useWeatherTimeQuery } from "@features/weather/useWeatherTimeQuery";
+import { UnixTime } from "@hooks/UnixTime";
+import ErrorPage from "@pages/ErrorPage";
+
 import type { WeatherTime } from "types/WeatherType";
 
 function MainWeatherTimeZone() {
-  const { latitude, longitude } = useCoordsStore(state => state);
-  const { data } = useQuery({
-    queryKey: ["timeWeather", latitude, longitude],
-    queryFn: async () => {
-      const response = await fetch(
-        `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${import.meta.env.VITE_REACT_APP_WEATHER_API_KEY}&units=metric&cnt=8`,
-      );
-      const data = await response.json();
-      return data;
-    },
-    staleTime: 1000 * 60 * 60,
-    refetchInterval: 1000 * 60 * 60,
-    refetchIntervalInBackground: true,
-    enabled: !!latitude && !!longitude,
-  });
-  // console.log(data);
+  const { data: timeWeather, isError, isFetching } = useWeatherTimeQuery();
+  if (isError || timeWeather?.cod !== "200") return <ErrorPage />;
 
   return (
     <section className="w-full p-2 bg-slate-50 flex flex-col gap-1">
-      <h2 className="text-slate-600 text-base">
+      <h2 className="text-slate-600 text-base flex justify-between">
         시간대별 날씨정보(3시간 기준)
       </h2>
-      {data?.cod !== "200" ? (
+      {isFetching ? (
         <MainTimeZoneSkeleton />
       ) : (
         <div className="w-full py-1 flex gap-1 rounded-lg overflow-x-scroll scrollbar-hide">
-          {data &&
-            data?.list?.map((item: WeatherTime) => (
-              <div
-                key={item.dt}
-                className="h-24 w-full min-w-14 p-1 box-border border-[1px] shadow-md shadow-slate-300 bg-white rounded-lg flex flex-col items-center justify-center text-nowrap"
-              >
-                <p className="text-xs text-center text-slate-600">
-                  {UnixTime(item.dt, "")}
-                </p>
-                <img
-                  className="w-10 h-10"
-                  src={`https://openweathermap.org/img/wn/${item.weather[0].icon}.png`}
-                  alt="Weather Icon"
-                  fetchpriority="high"
-                />
-                <p className="text-center text-sm font-semibold">
-                  {item.main.temp.toFixed(1)}°C
-                </p>
-              </div>
-            ))}
+          {timeWeather?.list?.map((item: WeatherTime) => (
+            <div
+              key={item.dt}
+              className="h-24 w-full min-w-14 p-1 box-border border-[1px] shadow-md shadow-slate-300 bg-white rounded-lg flex flex-col items-center justify-center text-nowrap"
+            >
+              <p className="text-xs text-center text-slate-600">
+                {UnixTime(item.dt, "")}
+              </p>
+              <img
+                className="w-10 h-10"
+                src={`https://openweathermap.org/img/wn/${item.weather[0].icon}.png`}
+                alt="Weather Icon"
+                {...{ fetchpriority: "high" }}
+                decoding="async"
+              />
+              <p className="text-center text-sm font-semibold">
+                {item.main.temp.toFixed(1)}°C
+              </p>
+            </div>
+          ))}
         </div>
       )}
     </section>
