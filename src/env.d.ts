603e187f7674efe76d44a1a6b83f9e8aa438adaf
holajm <holajjm@naver.com>
2025-08-06 16:21:32 +0900

    Chore: 환경 변수 타입 정리

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,14 +2,20 @@ interface ImportMeta {
   readonly env:ImportMetaEnv
 }
 interface ImportMetaEnv {
-  readonly VITE_REACT_APP_WEATHER_API_KEY: string,
-  readonly VITE_REACT_APP_WEATHER_API_KEY2: string,
+  // Weather
+  readonly VITE_REACT_APP_WEATHER_MAIN_KEY: string,
+  readonly VITE_REACT_APP_WEATHER_SERVE_KEY: string,
+  // KAKAO
+  readonly VITE_KAKAO_LOGIN_API_KEY: string,
+  // Location
   readonly VITE_REACT_APP_LOCATION_API_KEY: string,
+  // Server
+  readonly VITE_API_SERVER: string,
+  // Production
+  readonly VITE_APP_PRODUCTION: string,
+  
   readonly VITE_REACT_APP_KAKAO_NATIVE_API_KEY: string,
   readonly VITE_REACT_APP_KAKAO_REST_API_KEY: string,
   readonly VITE_REACT_APP_KAKAO_JAVASCRIPT_KEY: string,
   readonly VITE_REACT_APP_KAKAO_ADMIN_KEY: string,
-  readonly VITE_API_SERVER: string,
-  readonly VITE_APP_TITLE: string,
-  readonly VITE_KAKAO_REST_API_KEY: string,
 }
