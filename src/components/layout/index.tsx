b188be37f79d115a8b3294a3b54afcebcc35d066
holajm <holajjm@naver.com>
2025-08-06 17:37:37 +0900

    Fix: alias 적용

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 
-import Header from "./Header";
+import Header from "@components/layout/Header";
 import Footer from "@components/layout/Footer";
 
 const Layout = () => {
