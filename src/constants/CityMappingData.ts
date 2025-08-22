aa610e4a9aded1c3d767211590a8876bba7ebc26
holajm <holajjm@naver.com>
2025-08-22 16:58:36 +0900

    Feat: 전국 지역 데이터 매핑

---
+++ src/constants/CityMappingData.ts
@@ -0,0 +1,14 @@
+import type { Cities } from "types/WeatherType";
+
+export const citysData: Cities = {
+  Seoul: "서울",
+  "Gyeonggi-do": "경기도",
+  "Gangwon-do": "강원도",
+  "North Chungcheong": "충청북도",
+  "Chungcheongnam-do": "충청남도",
+  "Jeollabuk-do": "전라북도",
+  "Jeollanam-do": "전라남도",
+  "Gyeongsangbuk-do": "경상북도",
+  "Gyeongsangnam-do": "경상남도",
+  "Jeju-do": "제주도",
+};
