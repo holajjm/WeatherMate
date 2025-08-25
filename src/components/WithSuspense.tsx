edc8d51b91c6739e0fd0c8ffcfbde65cc0dab9ab
holajm <holajjm@naver.com>
2025-08-25 17:44:50 +0900

    Fix: fallback UI 수정

--- src/components/WithSuspense.tsx
+++ src/components/WithSuspense.tsx
@@ -1,7 +1,9 @@
 import React, { Suspense } from "react";
 
+import Loading from "@components/layout/Loading";
+
 function WithSuspense(Component: React.ReactNode) {
-  return <Suspense fallback={<>Loading...</>}>{Component}</Suspense>;
+  return <Suspense fallback={<Loading />}>{Component}</Suspense>;
 }
 
 export default WithSuspense;
