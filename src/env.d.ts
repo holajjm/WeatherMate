f94ddd8d46dab6cfef960dd412d7b9e3cbed78f7
holajm <holajjm@naver.com>
2025-08-25 17:48:05 +0900

    Chore: 환경변수 복구

--- src/env.d.ts
+++ src/env.d.ts
@@ -7,10 +7,16 @@ interface ImportMetaEnv {
   readonly VITE_REACT_APP_WEATHER_SERVE_KEY: string;
   // KAKAO
   readonly VITE_KAKAO_LOGIN_API_KEY: string;
+  readonly VITE_REACT_APP_KAKAO_JS_KEY: string;
   // Location
   readonly VITE_REACT_APP_LOCATION_API_KEY: string;
   // Server
   readonly VITE_API_SERVER: string;
   // Production
   readonly VITE_APP_PRODUCTION: string;
+  // ----------------------------------------
+  readonly VITE_REACT_APP_KAKAO_NATIVE_API_KEY: string,
+  readonly VITE_REACT_APP_KAKAO_REST_API_KEY: string,
+  readonly VITE_REACT_APP_KAKAO_JAVASCRIPT_KEY: string,
+  readonly VITE_REACT_APP_KAKAO_ADMIN_KEY: string,
 }
