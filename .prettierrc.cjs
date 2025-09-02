f45abaf167f5d7cca4b2181364eb6b311348f232
holajm <holajjm@naver.com>
2025-09-02 17:42:13 +0900

    Chore: prettier 속성 주석 추가

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,11 +1,21 @@
 module.exports = {
+  // 홀따옴표
   "singleQuote": false,
-  "semi": true,
-  "useTabs": false,
-  "tabWidth": 2,
-  "trailingComma": 'all',
+  // 한 줄 최대 글자 수
   "printWidth": 80,
+  // 자동 세미 콜론
+  "semi": true,
+  // 후행 쉼표
+  "trailingComma": 'none',
+  // 화살표 함수 괄호 사용
   "arrowParens": 'avoid',
+  // 탭 사용
+  "useTabs": false,
+  // 탭 너비
+  "tabWidth": 2,
+  // 객체 리터럴 여백
+  "bracketSpacing": true,
+  // 개행 문자 형식
   "endOfLine": "auto",
   "plugins": ["prettier-plugin-tailwindcss"]
 };
