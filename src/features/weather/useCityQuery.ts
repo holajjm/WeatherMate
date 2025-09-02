ea51ffa83b688182f87856dabebdb086be3fc789
holajm <holajjm@naver.com>
2025-09-02 17:43:17 +0900

    Refactor: 오픈 API 지원 종료로 인한 API 로직 수정

--- src/features/weather/useCityQuery.ts
+++ src/features/weather/useCityQuery.ts
@@ -2,25 +2,29 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
 import { ENV } from "@constants/env";
+import { citiesData } from "@constants/CityMappingData";
 
 export default function useCityQuery() {
-  const params = {
-    id: "1835847,1841610,1843125,1845106,1845105,1845789,1845788,1841597,1902028,1846265",
-    appid: ENV.WEATHER_SERVE,
-    lang: "kr",
-    units: "metric",
-  };
+  // const params = {
+  //   id: "1835847,1841610,1843125,1845106,1845105,1845789,1845788,1841597,1902028,1846265",
+  //   appid: ENV.WEATHER_SERVE,
+  //   lang: "kr",
+  //   units: "metric",
+  // };
   return useQuery({
     queryKey: ["AllCityData"],
     queryFn: async () => {
-      const res = await axios.get(
-        "https://api.openweathermap.org/data/2.5/group",
-        {
-          params,
-        },
+      const weatherData = await Promise.all(
+        Object.keys(citiesData).map(async (city) => {
+          const res = await axios.get(
+            `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${ENV.WEATHER_SERVE}`,
+          );
+          return res?.data;
+        })
       );
-      // console.log(res);
-      return res?.data?.list;
+      // console.log(weatherData);
+      
+      return weatherData;
     },
     refetchInterval: 1000 * 60 * 30,
   });
