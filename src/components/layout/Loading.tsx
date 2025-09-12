2b7f9f55e9320c37a35eab802a5a099ecc232570
holajm <holajjm@naver.com>
2025-09-12 16:51:38 +0900

    Feat: 로딩 UI 문구 동적 동작 추가

--- src/components/layout/Loading.tsx
+++ src/components/layout/Loading.tsx
@@ -1,26 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function Loading() {
+  const [dots, setDots] = useState<string>("");
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDots(prev => {
+        if (prev === "") return ".";
+        if (prev === ".") return "..";
+        if (prev === "..") return "...";
+        return "";
+      });
+    }, 300);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <div
-      className={`
-        relative m-auto flex h-screen w-full min-w-[320px] max-w-[600px]
-        items-center justify-center bg-white
-      `}
-    >
-      <div
-        className={`
-          inset-0 flex h-full w-full flex-col items-center justify-center
-          bg-white text-center
-        `}
-      >
-        <img
-          src="/Loading_Cloud.gif"
-          alt="로딩중..."
-          className="mx-auto w-16"
-        />
-        <p className="text-2xl text-gray-600">로딩중...</p>
-      </div>
+    <div className="flex flex-col items-center">
+      <img
+        src="/Loading_Spin.gif"
+        alt="로딩중..."
+        className="w-12 rounded-full"
+      />
+      <p className="text-body text-toss-gray">Loading{dots}</p>
     </div>
   );
 }
