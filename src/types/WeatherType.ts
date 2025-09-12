51a986643ebe8bbf11465aa72a473a05172805c3
holajm <holajjm@naver.com>
2025-09-12 17:04:24 +0900

    Fix: 불필요 타입 필드 정리

--- src/types/WeatherType.ts
+++ src/types/WeatherType.ts
@@ -1,17 +1,6 @@
 //Weather Data Type
 export interface WeatherImage {
   [key: string]: string;
-  // Clear: string;
-  // Clouds: string;
-  // Rain: string;
-  // Drizzle: string;
-  // Thunderstorm: string;
-  // Snow: string;
-  // Haze: string;
-  // Mist: string;
-  // Smoke: string;
-  // Dust: string;
-  // overcastClouds: string;
 }
 
 export interface WeatherCondition {
@@ -106,75 +95,10 @@ export interface TimeWeather {
 export interface Cities {
   [key: string]: string;
 }
-// export interface AllCityData {
-//   clouds: { all: number };
-//   coord: { lat: number; lon: number };
-//   dt: number;
-//   id: number;
-//   main: {
-//     feels_like: number;
-//     grnd_level: number;
-//     humidity: number;
-//     pressure: number;
-//     sea_level: number;
-//     temp: number;
-//     temp_max: number;
-//     temp_min: number;
-//   };
-//   name: string;
-//   sys: {
-//     country: string;
-//     sunrise: number;
-//     sunset: number;
-//     timezone: number;
-//   };
-//   visibility: number;
-//   weather: [
-//     {
-//       id: number;
-//       main: string;
-//       description: string;
-//       icon: string;
-//     },
-//   ];
-//   wind: {
-//     deg: number;
-//     speed: number;
-//   };
-// }
-// export interface WeatherTime {
-//   clouds: { all: number };
-//   dt: number;
-//   dt_txt: string;
-//   main: {
-//     feels_like: number;
-//     grnd_level: number;
-//     humidity: number;
-//     pressure: number;
-//     sea_level: number;
-//     temp: number;
-//     temp_max: number;
-//     temp_min: number;
-//   };
-//   pop: number;
-//   rain?: {
-//     "3h": number;
-//   };
-//   sys: {
-//     pod: string;
-//   };
-//   visibility: number;
-//   weather: [
-//     {
-//       id: number;
-//       main: string;
-//       description: string;
-//       icon: string;
-//     },
-//   ];
-//   wind: {
-//     deg: number;
-//     gust: number;
-//     speed: number;
-//   };
-// }
+export interface WeatherDetailItem {
+  id: number;
+  title: string;
+  icon: string;
+  value: string;
+  alt: string;
+}
