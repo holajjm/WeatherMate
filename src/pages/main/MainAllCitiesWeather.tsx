552dc8c2ab30a48ff0c76c9d20695f777c0f8bf6
holajm <holajjm@naver.com>
2025-09-12 16:56:01 +0900

    Refactor: 슬라이드 모션 적용, map 메서드를 통한 유지보수, 가독성 향상

--- src/pages/main/MainAllCitiesWeather.tsx
+++ src/pages/main/MainAllCitiesWeather.tsx
@@ -1,6 +1,6 @@
 import React, { memo } from "react";
 
-import { citiesData, citiesDesc } from "@constants/CityMappingData";
+import { citiesData } from "@constants/CityMappingData";
 import MainAllWeatherSkeleton from "@components/skeleton/MainAllWeatherSkeleton";
 import useCityQuery from "@features/weather/useCityQuery";
 import useScrollTop from "@hooks/useScrollTop";
@@ -15,57 +15,67 @@ import "swiper/css/navigation";
 
 function MainAllCitiesWeather() {
   useScrollTop();
-  const { data: cityWeather } = useCityQuery();
+  const { data: cityWeather, isFetching } = useCityQuery();
   // console.log(cityWeather);
-  
+
   return (
     <>
-      <Swiper
-        direction="vertical"
-        spaceBetween={100}
-        centeredSlides={true}
-        autoplay={{
-          delay: 2000,
-          disableOnInteraction: false,
-        }}
-        // pagination={{
-        //   clickable: true,
-        // }}
-        // navigation={true}
-        modules={[Autoplay, Pagination, Navigation]}
-        className="h-12 w-full pb-2"
-      >
-        {cityWeather?.length ? (
-          <>
-            {cityWeather.map((item: AllCityData, i: number) => {
-              return (
-                <SwiperSlide
-                  key={item?.id}
-                  className="flex w-full items-center justify-around gap-2 rounded-xl bg-blue-300 p-2 text-center font-bold text-[#2d2d2d] shadow-md shadow-slate-300"
-                >
-                  <p className="grow">{citiesData[item.name as keyof Cities]}</p>
-                  <div className="flex items-center justify-center gap-4">
-                    <img
-                      src={`http://openweathermap.org/img/wn/${item.weather[0].icon}.png`}
-                      alt="Weather Icon"
-                      className="h-10 w-10 rounded-xl"
-                      width={40}
-                      height={40}
-                    />
-                    <p className="font-normal">
-                      {citiesDesc[item?.weather[0]?.description as keyof Cities]}
+      {!isFetching && (
+        <Swiper
+          // direction="vertical"
+          spaceBetween={100}
+          centeredSlides={true}
+          autoplay={{
+            delay: 2000,
+            disableOnInteraction: false
+            // pauseOnMouseEnter: true
+          }}
+          loop={true}
+          // pagination={{
+          //   clickable: true,
+          // }}
+          // navigation={true}
+          modules={[Autoplay, Pagination, Navigation]}
+          className="h-36 w-1/6 text-center text-caption font-bold text-toss-black"
+        >
+          {cityWeather?.length ? (
+            <>
+              {cityWeather.map((item: AllCityData, i: number) => {
+                return (
+                  <SwiperSlide
+                    key={item?.id}
+                    className="flex w-full flex-col items-center justify-around gap-1 rounded-xl border-[1px] bg-white p-2 shadow-lg shadow-slate-200"
+                  >
+                    <p className="text-body text-toss-gray">
+                      {citiesData[item.name as keyof Cities]}
                     </p>
-                    <p className="">{String(item.main.temp).slice(0, 2)}°C</p>
-                    <p className="font-normal">{item?.main?.humidity}%</p>
-                  </div>
-                </SwiperSlide>
-              );
-            })}
-          </>
-        ) : (
-          <MainAllWeatherSkeleton />
-        )}
-      </Swiper>
+                    <div className="flex w-full flex-col items-center justify-center gap-1">
+                      <img
+                        src={`http://openweathermap.org/img/wn/${item.weather[0].icon}.png`}
+                        alt="Weather Icon"
+                        className="bg-toss-lightblue w-10 rounded-button"
+                        width={40}
+                        height={40}
+                      />
+                      <p className="font-normal">
+                        {item?.weather[0]?.description as keyof Cities}
+                      </p>
+                      <div className="flex w-full items-center justify-around text-caption font-bold">
+                        <p className="">
+                          {String(item.main.temp).slice(0, 2)}°C
+                        </p>
+                        <p className="">{item?.main?.humidity}%</p>
+                      </div>
+                    </div>
+                  </SwiperSlide>
+                );
+              })}
+            </>
+          ) : (
+            <MainAllWeatherSkeleton />
+          )}
+        </Swiper>
+      )}
     </>
   );
 }
