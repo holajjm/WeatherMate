61e750a796504764f77cef2d87311b3ced45d21b
holajm <holajjm@naver.com>
2025-09-16 17:25:34 +0900

    Design: 전체 UI 토스 개편

--- src/pages/user/UserEdit.tsx
+++ src/pages/user/UserEdit.tsx
@@ -7,6 +7,7 @@ import useCustomAxios from "@hooks/useCustomAxios";
 
 import { toast } from "react-toastify";
 import type { UserMainData } from "types/UserType";
+import Button from "@components/layout/Button";
 
 function UserEdit() {
   const [user, setUser] = useRecoilState<UserMainData>(memberState);
@@ -16,7 +17,7 @@ function UserEdit() {
   const [password, setPassword] = useState<string>("");
   const axios = useCustomAxios();
   const navigate = useNavigate();
-  console.log(user);
+  // console.log(user);
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -25,7 +26,7 @@ function UserEdit() {
         name,
         phone,
         email,
-        password,
+        password
       };
       let updated = false;
       console.log(updatedData);
@@ -59,7 +60,7 @@ function UserEdit() {
 
       setUser(prevUser => ({
         ...prevUser,
-        ...updatedData,
+        ...updatedData
       }));
 
       // 변경된 부분만 초기화
@@ -82,59 +83,87 @@ function UserEdit() {
   };
 
   return (
-    <nav className="m-auto h-screen min-w-[320px] max-w-[600px] bg-slate-50 p-2">
-      <div className="flex w-full flex-col gap-4 rounded-xl bg-white p-4 drop-shadow-lg">
-        <h1 className="font-UhBeeKangJa m-auto h-16 p-4 text-2xl font-bold">
-          회원 정보 수정
-        </h1>
-        <form onSubmit={handleSubmit} className="flex flex-col gap-2">
-          <input
-            id="name"
-            type="text"
-            placeholder="수정할 이름을 입력하세요"
-            value={name}
-            onChange={e => setName(e.target.value)}
-            className="w-full rounded-xl border-gray-300 bg-slate-100 p-4 focus:border-transparent focus:ring-2 focus:ring-primary"
-          />
-          <input
-            id="phone"
-            type="text"
-            placeholder="수정할 번호을 입력하세요"
-            value={phone}
-            onChange={e => setPhone(e.target.value)}
-            className="w-full rounded-xl border-gray-300 bg-slate-100 p-4 focus:border-transparent focus:ring-2 focus:ring-primary"
-          />
-          <input
-            id="email"
-            type="email"
-            placeholder="새로운 이메일을 입력하세요"
-            value={email}
-            onChange={e => setEmail(e.target.value)}
-            className="w-full rounded-xl border-gray-300 bg-slate-100 p-4 focus:border-transparent focus:ring-2 focus:ring-primary"
-          />
-          <input
-            id="password"
-            type="password"
-            placeholder="새로운 비밀번호 입력하세요"
-            value={password}
-            onChange={e => setPassword(e.target.value)}
-            className="w-full rounded-xl border-gray-300 bg-slate-100 p-4 focus:border-transparent focus:ring-2 focus:ring-primary"
-          />
-          <div className="flex gap-2">
-            <button
-              onClick={() => navigate("/user/mypage")}
-              type="button"
-              className="font-UhBeeKangJa w-full text-nowrap rounded-lg border-2 border-slate-100 bg-slate-200 p-4 text-gray-500 transition-all duration-200 hover:shadow-[0_4px_8px_1px] hover:shadow-slate-400"
-            >
-              취소하기
-            </button>
-            <button
-              type="submit"
-              className="font-UhBeeKangJa w-full text-nowrap rounded-lg border-2 border-slate-100 bg-sky-400 p-4 text-white transition-all duration-200 hover:shadow-[0_4px_8px_1px] hover:shadow-slate-400"
-            >
-              수정하기
-            </button>
+    <nav className="m-auto h-screen min-w-[320px] max-w-[600px] bg-slate-50">
+      <div className="flex w-full flex-col gap-4 bg-white p-4 drop-shadow-sm">
+        <form
+          onSubmit={handleSubmit}
+          className="flex flex-col gap-4 bg-white text-body drop-shadow-sm"
+        >
+          <div className="flex flex-col gap-2">
+            <div className="relative flex w-full items-center justify-center">
+              <svg
+                className="absolute left-0 top-0 h-5 w-5 cursor-pointer hover:scale-110"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                onClick={() => window.history.back()}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M15 19l-7-7 7-7"
+                />
+              </svg>
+              <h1 className="text-subtitle font-bold">회원 정보 수정</h1>
+            </div>
+            <div className="flex items-center justify-between">
+              <label
+                htmlFor="name"
+                className="flex w-1/6 items-center justify-start text-body text-toss-gray"
+              >
+                닉네임
+              </label>
+              <input
+                id="name"
+                type="text"
+                placeholder="수정할 닉네임을 입력하세요"
+                value={name}
+                onChange={e => setName(e.target.value)}
+                className="w-3/4 rounded-button border-gray-300 bg-slate-100 p-3 focus:border-transparent focus:ring-2 focus:ring-primary"
+              />
+            </div>
+            <div className="flex items-center justify-between">
+              <label
+                htmlFor="name"
+                className="flex w-1/6 items-center justify-start text-body text-toss-gray"
+              >
+                이메일
+              </label>
+              <input
+                id="email"
+                type="email"
+                placeholder="새로운 이메일을 입력하세요"
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+                className="w-3/4 rounded-button border-gray-300 bg-slate-100 p-3 focus:border-transparent focus:ring-2 focus:ring-primary"
+              />
+            </div>
+            <div className="flex items-center justify-between">
+              <label
+                htmlFor="name"
+                className="flex w-1/6 items-center justify-start text-body text-toss-gray"
+              >
+                비밀번호
+              </label>
+              <input
+                id="password"
+                type="password"
+                placeholder="새로운 비밀번호 입력하세요"
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+                className="w-3/4 rounded-button border-gray-300 bg-slate-100 p-3 focus:border-transparent focus:ring-2 focus:ring-primary"
+              />
+            </div>
           </div>
+          <Button
+            text={"수정하기"}
+            bgColor="blue"
+            textColor="white"
+            width="full"
+            height="12"
+            onClick={() => {}}
+          ></Button>
         </form>
       </div>
     </nav>
