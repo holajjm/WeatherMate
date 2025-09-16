34d39ba1f1d56eccf51660ddfbf3f5a298b163ba
holajm <holajjm@naver.com>
2025-09-16 17:35:41 +0900

    Refactor: 예외처리UI 추가

--- src/pages/user/UserBookmark.tsx
+++ src/pages/user/UserBookmark.tsx
@@ -10,14 +10,13 @@ import type { UserBookmarkData } from "types/UserType";
 function UserBookMark() {
   const [bookmarks, setBookmarks] = useState([]);
   const [bookmarkData, setBookmarkData] = useState<UserBookmarkData[]>([]);
-  const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
   // console.log(bookmarkData);
 
   const fetchData = async (contentId: string) => {
     try {
       const response = await axios.get(
-        `https://apis.data.go.kr/B551011/KorService1/detailCommon1?MobileOS=ETC&MobileApp=test&_type=json&contentId=${contentId}&serviceKey=Tni56ZINiQ1IRiydSoRdwSLjhCAXtB2FKJPCTEQPxyyr0%2FJvNjWymNpCJQzOtAmEEr1jyhFa2zejQamJnkB9Uw%3D%3D&defaultYN=Y&firstImageYN=Y`,
+        `https://apis.data.go.kr/B551011/KorService1/detailCommon1?MobileOS=ETC&MobileApp=test&_type=json&contentId=${contentId}&serviceKey=Tni56ZINiQ1IRiydSoRdwSLjhCAXtB2FKJPCTEQPxyyr0%2FJvNjWymNpCJQzOtAmEEr1jyhFa2zejQamJnkB9Uw%3D%3D&defaultYN=Y&firstImageYN=Y`
       );
       return response.data.response.body.items.item;
     } catch (error) {
@@ -34,10 +33,9 @@ function UserBookMark() {
   useEffect(() => {
     const fetchDataForBookmarks = async () => {
       const bookmarkDataArray = await Promise.all(
-        bookmarks.map(bookmark => fetchData(bookmark)),
+        bookmarks.map(bookmark => fetchData(bookmark))
       );
       setBookmarkData(bookmarkDataArray.flat());
-      setLoading(false);
     };
     fetchDataForBookmarks();
   }, [bookmarks]);
@@ -49,25 +47,24 @@ function UserBookMark() {
   const removeBookmark = (contentId: string) => {
     if (confirm("북마크에서 삭제하시겠습니까?")) {
       const updatedBookmarks = bookmarks.filter(
-        bookmark => bookmark !== contentId,
+        bookmark => bookmark !== contentId
       );
       setBookmarks(updatedBookmarks);
       localStorage.setItem("bookmarks", JSON.stringify(updatedBookmarks));
       // 새로운 북마크 데이터 배열 생성
       const updatedBookmarkData = bookmarkData.filter(
-        item => item.contentid !== contentId,
+        item => item.contentid !== contentId
       );
       console.log(updatedBookmarkData);
       setBookmarkData(updatedBookmarkData);
     }
     return;
   };
+  // console.log(bookmarkData);
 
   return (
-    <div className="h-96 overflow-y-scroll rounded-xl bg-slate-100 p-4 scrollbar-hide">
-      {loading ? (
-        <Loading />
-      ) : (
+    <div className="min-h-60 overflow-scroll rounded-button scrollbar-hide">
+      {bookmarkData?.length ? (
         <div className="grid grid-cols-1 gap-2 sm:grid-cols-2">
           {bookmarkData.map((item, index) => (
             <div key={index} className="flex">
@@ -80,7 +77,7 @@ function UserBookMark() {
                 </div>
 
                 <div className="flex flex-col gap-1">
-                  <p className="font-TTLaundryGothicB text-center text-base font-normal">
+                  <p className="text-center text-base font-normal">
                     {item.title}
                   </p>
                   <div className="flex gap-1">
@@ -89,6 +86,7 @@ function UserBookMark() {
                       textColor="gray"
                       bgColor="gray"
                       width="full"
+                      height="10"
                       onClick={() => moveToBookMarkPage(item.contentid)}
                     ></Button>
                     <Button
@@ -96,6 +94,7 @@ function UserBookMark() {
                       textColor="white"
                       bgColor="lightRed"
                       width="full"
+                      height="10"
                       onClick={() => removeBookmark(item.contentid)}
                     ></Button>
                   </div>
@@ -104,6 +103,10 @@ function UserBookMark() {
             </div>
           ))}
         </div>
+      ) : (
+        <p className="flex h-60 items-center justify-center text-toss-gray">
+          저장한 장소가 없습니다.
+        </p>
       )}
     </div>
   );
