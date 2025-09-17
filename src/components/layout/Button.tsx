56c463654ec134d48fe50e3b1f198b8f75ab4034
holajm <holajjm@naver.com>
2025-09-17 15:53:32 +0900

    Design: 색상 필드값 변경

--- src/components/layout/Button.tsx
+++ src/components/layout/Button.tsx
@@ -10,25 +10,34 @@ interface ButtonProps {
 }
 
 const buttonColor = {
-  sky: "bg-sky-400",
+  white: "bg-white",
+  black: "bg-toss-black",
   blue: "bg-toss-blue",
-  indigo: "bg-indigo-500",
-  kakao: "bg-[#FEE500]",
-  gray: "bg-slate-300",
-  gray_light: "bg-slate-200",
-  amber: "bg-amber-200",
+  lightblue: "bg-toss-lightblue",
+  gray: "bg-toss-gray",
+  lightgray: "bg-toss-lightgray",
   red: "bg-red-500",
   lightRed: "bg-red-300",
-  white: "bg-white",
-  black: "bg-[#2D2D2D]",
+
+  sky: "bg-sky-400",
+  indigo: "bg-indigo-500",
+  kakao: "bg-[#FEE500]",
+  amber: "bg-amber-200"
 };
 const fontColor = {
   white: "text-white",
   gray: "text-toss-gray",
   black: "text-toss-black",
-  amber: "text-amber-500",
+  amber: "text-amber-500"
 };
-function Button({ text, textColor, bgColor, width,height, onClick }: ButtonProps) {
+function Button({
+  text,
+  textColor,
+  bgColor,
+  width,
+  height,
+  onClick
+}: ButtonProps) {
   return (
     <button
       className={`${buttonColor[bgColor]} ${fontColor[textColor]} w-${width} h-${height} box-border flex items-center justify-center text-nowrap rounded-button p-2 text-caption font-bold transition-all duration-200 hover:scale-[1.03]`}
