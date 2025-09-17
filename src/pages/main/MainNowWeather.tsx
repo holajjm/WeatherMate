24719e77df5fdaa7e45c3056520d633cc356f2d2
holajm <holajjm@naver.com>
2025-09-17 15:51:54 +0900

    Refactor: 이미지 예외처리 로직 추가

--- src/pages/main/MainNowWeather.tsx
+++ src/pages/main/MainNowWeather.tsx
@@ -9,7 +9,6 @@ import ErrorPage from "@pages/ErrorPage";
 
 import type { WeatherImage } from "types/WeatherType";
 
-
 function MainNowWeather() {
   const { data: weatherData, isError, isFetching } = useWeatherQuery();
   if (isError) return <ErrorPage />;
@@ -27,21 +26,28 @@ function MainNowWeather() {
       <div className="flex gap-2">
         {!isFetching ? (
           <div className="relative flex h-36 w-1/2 gap-2">
-            <img
-              className="absolute h-full w-full rounded-button object-fill"
-              src={realImage ? realImage : "./Loading_Cloud.gif"}
-              alt="today"
-              width={580}
-              height={160}
-              {...{ fetchpriority: "high" }}
-              decoding="async"
-            />
+            {realImage && (
+              <img
+                className="absolute h-full w-full rounded-button object-fill"
+                // src={realImage ? realImage : "./Loading_Spin.gif"}
+                src={realImage}
+                alt="today"
+                width={580}
+                height={160}
+                {...{ fetchpriority: "high" }}
+                decoding="async"
+              />
+            )}
             <div
-              className={`flex w-full flex-col justify-center gap-2 rounded-button text-center font-bold shadow-md shadow-slate-500 ${["Clouds", "Rain", "Drizzle", "Thunderstorm", "Snow"].includes(weatherData?.weather[0].main as string) ? "text-white" : "text-[#2d2d2d]"} z-20`}
+              className={`flex w-full flex-col justify-center gap-2 rounded-button text-center font-bold shadow-md shadow-slate-500 ${["Clouds", "Rain", "Drizzle", "Thunderstorm", "Snow"].includes(weatherData?.weather[0].main as string) ? "text-white" : "text-toss-black"} z-20`}
             >
               <p className="text-caption">{weatherData?.name}</p>
-              <p className="text-title">{weatherData?.main.temp.toFixed(1)}°C</p>
-              <p className="text-subtitle">{weatherData?.weather[0].description}</p>
+              <p className="text-title">
+                {weatherData?.main.temp.toFixed(1)}°C
+              </p>
+              <p className="text-subtitle">
+                {weatherData?.weather[0].description}
+              </p>
             </div>
           </div>
         ) : (
