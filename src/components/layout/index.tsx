c5510345fcc7b246a4e0ab056ed73438d47f4b23
holajm <holajjm@naver.com>
2025-09-17 18:37:12 +0900

    Refactor: 네비게이션 바 위치 수정

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -3,6 +3,7 @@ import { Outlet } from "react-router-dom";
 
 import Header from "@components/layout/Header";
 import Footer from "@components/layout/Footer";
+import NavigationBar from "@components/layout/NavigationBar";
 
 const Layout = () => {
   return (
@@ -10,6 +11,7 @@ const Layout = () => {
       <Header />
       <Outlet />
       <Footer />
+      <NavigationBar />
     </div>
   );
 };
