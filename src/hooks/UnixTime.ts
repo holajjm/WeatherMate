71e7c893bb75c530058735cd118ca1649bc37a0c
holajm <holajjm@naver.com>
2025-09-18 15:07:09 +0900

    Fix: 시간대 표시 영문 변환

--- src/hooks/UnixTime.ts
+++ src/hooks/UnixTime.ts
@@ -4,7 +4,7 @@ export const UnixTime = (time: number | undefined, type: string) => {
       .toString()
       .padStart(2, "0");
     const min = new Date(time * 1000).getMinutes().toString().padStart(2, "0");
-    const ampm = new Date(time * 1000).getHours() >= 12 ? "오후" : "오전";
-    return type !== "withMin" ? `${ampm} ${hour}시` : `${ampm}${hour}:${min}`;
+    const ampm = new Date(time * 1000).getHours() >= 12 ? "PM" : "AM";
+    return type !== "withMin" ? `${ampm} ${hour}` : `${ampm}${hour}:${min}`;
   }
 };
