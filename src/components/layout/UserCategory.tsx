1414d8ee1d330fec08a38f86536c4daec303314f
holajm <holajjm@naver.com>
2025-09-18 16:36:03 +0900

    Feat: 로그아웃 로직 추가 & Refactor: 유저 정보 존재에 따른 유효성 검사 추가

--- src/components/layout/UserCategory.tsx
+++ src/components/layout/UserCategory.tsx
@@ -1,15 +1,27 @@
 import React from "react";
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 
 import useScrollTop from "@hooks/useScrollTop";
 import { motion } from "framer-motion";
 
 import { MdLocationOn } from "react-icons/md";
 import { FaUserCircle } from "react-icons/fa";
+import { useRecoilState, useSetRecoilState } from "recoil";
+import { memberState } from "@recoil/atom";
 
 function UserCategory() {
-  const location = useLocation();
   useScrollTop();
+  const location = useLocation();
+  const navigate = useNavigate();
+  const [user] = useRecoilState(memberState);
+  const setUser = useSetRecoilState(memberState);
+  console.log(user);
+  const handleLogout = () => {
+    if (confirm("로그아웃 할까요?")) {
+      setUser(null);
+      navigate("/");
+    }
+  };
   return (
     <div className="flex h-screen items-start justify-center bg-slate-100 p-4 text-lg drop-shadow-[-5px_5px_3px_rgba(0,0,0,0.3)]">
       <div className="grid w-full grid-cols-2 gap-2">
@@ -24,7 +36,7 @@ function UserCategory() {
           <Link
             to={"/user/mypage"}
             state={{ from: location.pathname }}
-            className="flex h-full w-full aspect-square flex-col items-center justify-center gap-2 rounded-button bg-white p-2 text-caption font-medium drop-shadow-md transition-all duration-200 hover:scale-105 hover:bg-toss-blue hover:text-white hover:shadow-lg"
+            className="flex aspect-square h-full w-full flex-col items-center justify-center gap-2 rounded-button bg-white p-2 text-caption font-medium drop-shadow-md transition-all duration-200 hover:scale-105 hover:bg-toss-blue hover:text-white hover:shadow-lg"
           >
             <FaUserCircle className="text-2xl" />
             <span>마이페이지</span>
@@ -39,14 +51,24 @@ function UserCategory() {
             duration: 0.75
           }}
         >
-          <Link
-            to={"/location"}
-            state={{ from: location.pathname }}
-            className="flex h-full w-full aspect-square flex-col items-center justify-center gap-2 rounded-button bg-white p-2 text-caption font-medium drop-shadow-md transition-all duration-200 hover:scale-105 hover:bg-toss-lightred hover:text-white hover:shadow-lg"
-          >
-            <MdLocationOn className="text-2xl" />
-            <span>로그아웃</span>
-          </Link>
+          {user ? (
+            <button
+              onClick={handleLogout}
+              className="flex aspect-square h-full w-full flex-col items-center justify-center gap-2 rounded-button bg-white p-2 text-caption font-medium drop-shadow-md transition-all duration-200 hover:scale-105 hover:bg-toss-lightred hover:text-white hover:shadow-lg"
+            >
+              <MdLocationOn className="text-2xl" />
+              <span>로그아웃</span>
+            </button>
+          ) : (
+            <Link
+              to={"/mainlogin"}
+              state={{ from: location.pathname }}
+              className="flex aspect-square h-full w-full flex-col items-center justify-center gap-2 rounded-button bg-white p-2 text-caption font-medium drop-shadow-md transition-all duration-200 hover:scale-105 hover:bg-toss-blue hover:text-white hover:shadow-lg"
+            >
+              <MdLocationOn className="text-2xl" />
+              <span>로그인</span>
+            </Link>
+          )}
         </motion.div>
       </div>
     </div>
