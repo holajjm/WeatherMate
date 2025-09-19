e06c6fb9ee0dafb3c8ad6f38175238e101f88a2b
holajm <holajjm@naver.com>
2025-09-19 18:09:16 +0900

    Refactor: 최상단 이동 버튼 전역적 적용

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -4,6 +4,7 @@ import { Outlet } from "react-router-dom";
 import Header from "@components/layout/Header";
 import Footer from "@components/layout/Footer";
 import NavigationBar from "@components/layout/NavigationBar";
+import ToTheTopButton from "./ToTheTopButton";
 
 const Layout = () => {
   return (
@@ -11,6 +12,7 @@ const Layout = () => {
       <Header />
       <Outlet />
       <Footer />
+      <ToTheTopButton />
       <NavigationBar />
     </div>
   );
