6455e0400dceb26d9f51ebb75e838fa3ecc90093
holajm <holajjm@naver.com>
2025-09-25 17:08:18 +0900

    Feat: 커뮤니티 게시글 목록 조회 로직 분리

---
+++ src/features/community/usePostsQuery.ts
@@ -0,0 +1,21 @@
+import { useQuery } from "@tanstack/react-query";
+
+import useCustomAxios from "@hooks/useCustomAxios";
+
+export function usePostsQuery() {
+  const axios = useCustomAxios();
+  return useQuery({
+    queryKey: ["posts"],
+    queryFn: () =>
+      axios.get("/posts", {
+        params: {
+          type: "community"
+        }
+      }),
+    select: data => data?.data?.item,
+    staleTime: 1000 * 60 * 30,
+    refetchOnWindowFocus: true,
+    refetchInterval: 1000 * 60 * 30,
+    refetchOnMount: "always"
+  });
+}
