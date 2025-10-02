65304031711f6ffd69f8352a98aeb552c5efc248
holajm <holajjm@naver.com>
2025-10-02 15:40:34 +0900

    Feat: 게시글 개별 컴포넌트에 좋아요 UI 및 추가,취소 로직 생성

--- src/pages/community/CommunityItem.tsx
+++ src/pages/community/CommunityItem.tsx
@@ -1,103 +1,141 @@
-import React from "react";
+import React, { useEffect} from "react";
 import { useNavigate } from "react-router-dom";
 
 import { ENV } from "@constants/env";
+import { useLikesQuery } from "@features/community/useLikesQuery";
+import { useLikesDelete } from "@features/community/useLikesDelete";
+import { useLikesCreate } from "@features/community/useLikesCreate";
 import { TimeDiff } from "@hooks/TimeDiff";
+import { useLikesStore } from "@store/store";
 
 import { IoChatbubbleEllipsesOutline } from "react-icons/io5";
-import type { ExpandCommunityData } from "types/CommunityType";
+import { GoHeart } from "react-icons/go";
+import { GoHeartFill } from "react-icons/go";
+import type { ExpandCommunityData, LikesData } from "types/CommunityType";
 
 function CommunityItem({ item }: { item: ExpandCommunityData }) {
   const navigate = useNavigate();
+  const { likes, setLikes } = useLikesStore();
+
+  // 좋아요 전체 목록 조회
+  const { data: getLikePosts } = useLikesQuery();
+  // console.log(getLikePosts);
+
+  useEffect(() => {
+    getLikePosts?.forEach((e: LikesData) => {
+      setLikes({ realId: e?.post?._id, likeId: e?._id });
+    });
+  }, [getLikePosts]);
+
+  // 좋아요 누르기
+  const { mutate: postLike } = useLikesCreate({
+    id: item._id,
+    content: item.content
+  });
+
+  // 좋아요 취소
+  const { mutate: deleteLike } = useLikesDelete();
+  const handleDeleteLike = () => {
+    const likeObj = likes.find(e => e.realId === item._id);
+    if (likeObj) deleteLike(likeObj.likeId);
+  };
 
   return (
-    <div
-      className="box-border flex h-[25rem] w-full cursor-pointer flex-col gap-1 rounded-button bg-white drop-shadow-lg"
-      onClick={() => navigate(`/community/${item._id}`)}
-    >
-      <header className="h-12 p-2">
-        <section className="flex items-center gap-3">
-          <div className="flex h-10 w-10 items-center justify-center rounded-full border-2">
-            <img
-              src={
-                item?.user.profile
-                  ? `${ENV.API_SERVER}/files/07-WeatherMate/${item.user.profile}`
-                  : "/NullUser.webp"
-              }
-              alt="profile"
-              className="h-10 w-10 rounded-full object-contain"
-              width={48}
-              height={48}
-              {...{ fetchpriority: "high" }}
-              decoding="async"
-            />
-          </div>
-
-          <div className="flex grow items-center">
-            <div className="grow">
-              <h1 className="text-sm font-bold text-black">
-                {item.user?.name}
-              </h1>
-              <p className="text-caption text-toss-gray">조회수 {item.views}</p>
-            </div>
-            {item.title && (
+    <div className="box-border flex h-[25rem] w-full cursor-pointer flex-col gap-1 rounded-button bg-white drop-shadow-lg">
+      <div className="grow" onClick={() => navigate(`/community/${item._id}`)}>
+        <header className="h-12 p-2">
+          <section className="flex items-center gap-3">
+            <div className="flex h-10 w-10 items-center justify-center rounded-full border-2">
               <img
-                className="h-8 w-8 rounded-button"
                 src={
-                  [
-                    "Sun",
-                    "Cloud",
-                    "Rain",
-                    "Foggy",
-                    "Snow",
-                    "Thunder",
-                    "Wind",
-                  ].includes(item?.title)
-                    ? `./WeatherIcon/WeatherIcon${item?.title}.webp`
-                    : "./MBTIImage/MBTIMain.webp"
+                  item?.user.profile
+                    ? `${ENV.API_SERVER}/files/07-WeatherMate/${item.user.profile}`
+                    : "/NullUser.webp"
                 }
-                alt="weatherIcon"
+                alt="profile"
+                className="h-10 w-10 rounded-full object-contain"
                 width={48}
                 height={48}
                 {...{ fetchpriority: "high" }}
                 decoding="async"
               />
-            )}
+            </div>
+
+            <div className="flex grow items-center">
+              <div className="grow">
+                <h1 className="text-sm font-bold text-black">
+                  {item.user?.name}
+                </h1>
+                <p className="text-caption text-toss-gray">
+                  조회수 {item.views}
+                </p>
+              </div>
+              {item.title && (
+                <img
+                  className="h-8 w-8 rounded-button"
+                  src={
+                    [
+                      "Sun",
+                      "Cloud",
+                      "Rain",
+                      "Foggy",
+                      "Snow",
+                      "Thunder",
+                      "Wind"
+                    ].includes(item?.title)
+                      ? `./WeatherIcon/WeatherIcon${item?.title}.webp`
+                      : "./MBTIImage/MBTIMain.webp"
+                  }
+                  alt="weatherIcon"
+                  width={48}
+                  height={48}
+                  {...{ fetchpriority: "high" }}
+                  decoding="async"
+                />
+              )}
+            </div>
+          </section>
+        </header>
+
+        <section className="flex h-60 grow flex-col gap-1">
+          <div className="h-full">
+            <img
+              className="h-full w-full border-y-[1px] border-toss-lightgray object-cover"
+              src={
+                item.extra
+                  ? `${ENV.API_SERVER}/files/07-WeatherMate/${item.extra.image}`
+                  : item?.image
+                    ? `${ENV.API_SERVER}/files/07-WeatherMate/${item?.image}`
+                    : `/ReadyForImage.webp`
+              }
+              alt="DetailImage"
+              width={300}
+              height={240}
+              {...{ fetchpriority: "high" }}
+              decoding="async"
+            />
           </div>
         </section>
-      </header>
 
-      <section className="flex h-60 grow flex-col gap-1">
-        <div className="h-full">
-          <img
-            className="h-full w-full object-cover border-y-[1px] border-toss-lightgray"
-            src={
-              item.extra
-                ? `${ENV.API_SERVER}/files/07-WeatherMate/${item.extra.image}`
-                : item?.image
-                  ? `${ENV.API_SERVER}/files/07-WeatherMate/${item?.image}`
-                  : `/ReadyForImage.webp`
-            }
-            alt="DetailImage"
-            width={300}
-            height={240}
-            {...{ fetchpriority: "high" }}
-            decoding="async"
-          />
-        </div>
-      </section>
-
-      <section>
-        <p className="h-12 grow overflow-scroll px-2 text-caption text-black scrollbar-hide">
-          {item.content}
-        </p>
-      </section>
+        <section>
+          <p className="h-12 grow overflow-scroll px-2 text-caption text-black scrollbar-hide">
+            {item.content}
+          </p>
+        </section>
+      </div>
 
       <footer className="flex items-center justify-between gap-4 p-2 text-amber-400">
         <p className="text-caption font-medium text-toss-gray">
           {TimeDiff(item.createdAt)}
         </p>
         <section className="flex items-center justify-center gap-1">
+          <button>
+            {likes.some(e => e.realId === item._id) ? (
+              <GoHeartFill onClick={handleDeleteLike} />
+            ) : (
+              <GoHeart onClick={() => postLike(item._id)} />
+            )}
+          </button>
           <p className="flex items-center">
             <IoChatbubbleEllipsesOutline className="text-body" />
           </p>
